9bccedd406cd045aa0612bf1462f7d2721b2cb16
Sergey Zubov <zubov@spb.gs.ru>
2012-07-15 17:53:57 +0400

    now HasNestedType work for reference typedefs

--- NestedTypeCheck.h
+++ NestedTypeCheck.h
@@ -12,7 +12,7 @@ namespace stingray
 	template < typename T > \
 	class HasNestedType_##NestedType_ \
 	{ \
-		template < typename U > static YesType deduce(IntToType<sizeof(typename U::NestedType_*)>*); \
+		template < typename U > static YesType deduce(IntToType<sizeof(typename Dereference<typename U::NestedType_>::ValueT*)>*); \
 		template < typename U > static NoType deduce(...); \
 		\
 	public: \
