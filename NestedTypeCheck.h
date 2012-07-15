ff24dea07fb1de40f237b5a10159c8885b5c63e6
Sergey Zubov <zubov@spb.gs.ru>
2012-07-15 17:44:29 +0400

    rewrited HasNestedType

--- NestedTypeCheck.h
+++ NestedTypeCheck.h
@@ -1,42 +1,28 @@
-#ifndef __GS_DVRLIB_TOOLKIT_NESTEDTYPECHECK_H__
-#define __GS_DVRLIB_TOOLKIT_NESTEDTYPECHECK_H__
+#ifndef STINGRAY_TOOLKIT_NESTEDTYPECHECK_H
+#define STINGRAY_TOOLKIT_NESTEDTYPECHECK_H
 
 
-#include <stingray/toolkit/TypeList.h>
+#include <stingray/toolkit/MetaProgramming.h>
+
 
 namespace stingray
 {
+
+#define TOOLKIT_DECLARE_NESTED_TYPE_CHECK(NestedType_) \
+	template < typename T > \
+	class HasNestedType_##NestedType_ \
+	{ \
+		template < typename U > static YesType deduce(IntToType<sizeof(typename U::NestedType_*)>*); \
+		template < typename U > static NoType deduce(...); \
+		\
+	public: \
+		static const bool Value = sizeof(deduce<T>(0)) == sizeof(YesType); \
+	}
 
 	namespace Detail
 	{
 		struct DoesNotHaveAnyNestedTypes
 		{ static const bool Value = false; };
-	}
-
-#define TOOLKIT_DECLARE_NESTED_TYPE_CHECK(NestedType_) \
-	template < typename T > \
-	struct HasNestedType_##NestedType_ \
-	{ \
-		template < typename Type > \
-		class Impl \
-		{ \
-			struct BaseMixin { struct NestedType_{}; }; \
-			struct Base : public Type, public BaseMixin { Base(); }; \
-			\
-			template <typename V, V t>    class Helper{}; \
-			\
-			template <typename U> static NoType deduce(U*, typename U::NestedType_* = 0); \
-			static YesType deduce(...); \
-			\
-		public: \
-			static const bool Value = (sizeof(YesType) == sizeof(deduce((Base*)(0)))); \
-		};\
-		static const bool Value = \
-			If	< \
-					IsClass<T>::Value, \
-					Impl<T>, \
-					stingray::Detail::DoesNotHaveAnyNestedTypes \
-				>::ValueT::Value; \
 	}
 
 #define TOOLKIT_DECLARE_METHOD_CHECK(Method_) \
