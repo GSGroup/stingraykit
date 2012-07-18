92ea91945bfbe8402e5dfccf1afabd0a58c01948
Борис Сазонов <sazonov@spb.gs.ru>
2012-07-18 22:48:09 +0400

    Extracted DiscontinuityRepairer to a separate class and added bandwidth-based discontinuity repairing

---
+++ DiscontinuityRepairer.cpp
@@ -0,0 +1,11 @@
+#include <stingray/toolkit/DiscontinuityRepairer.h>
+
+namespace stingray {
+
+
+	TOOLKIT_DEFINE_NAMED_LOGGER(DiscontinuityRepairer);
+
+
+}
+
+
---
+++ DiscontinuityRepairer.h
@@ -0,0 +1,100 @@
+#ifndef __GS_STINGRAY_TOOLKIT_DISCONTINUITYREPAIRER_H__
+#define __GS_STINGRAY_TOOLKIT_DISCONTINUITYREPAIRER_H__
+
+#include <stingray/log/Logger.h>
+#include <stingray/timer/ElapsedTime.h>
+#include <stingray/timer/Time.h>
+#include <stingray/toolkit/optional.h>
+
+namespace stingray {
+
+
+	struct DiscontinuityRepairer // repairs discontinuities in both PTS and PCR
+	{
+	private:
+		static NamedLogger	s_logger;
+		ElapsedTime			_timeSinceLastIndex;
+		optional<u64>		_firstIndex, _lastIndex;
+		bool				_paused, _allowDiscontinuity;
+
+	public:
+		DiscontinuityRepairer() : _paused(false), _allowDiscontinuity(false)
+		{}
+
+		optional<TimeDuration> GetTimeSinceLast()
+		{ return _firstIndex ? optional<TimeDuration>(TimeDuration(_timeSinceLastIndex.ElapsedMilliseconds())) : optional<TimeDuration>(); }
+
+		void Pause(bool paused)
+		{
+			if (paused == _paused)
+				return;
+
+			s_logger.Warning() << "Pausing: " << Backtrace().Get();
+
+			if (paused)
+			{
+				_timeSinceLastIndex.Restart();
+			}
+			else
+			{
+				_timeSinceLastIndex.Restart();
+			}
+			_paused = paused;
+		}
+
+		void AllowDiscontinuiny()
+		{
+			_allowDiscontinuity = true;
+		}
+
+		u64 Repair(u64 milliseconds, u64 discontinuityValue = 0)
+		{
+			if (_paused)
+			{
+				s_logger.Warning() << "Stream is paused!" << Backtrace().Get();
+				return 0;
+			}
+			s64 time_since_last = _timeSinceLastIndex.ElapsedMilliseconds();
+			_timeSinceLastIndex.Restart();
+
+			if (!_firstIndex)
+			{
+				_firstIndex = _lastIndex = milliseconds;
+				return 0;
+			}
+
+			bool discontinuity = false;
+			if (milliseconds < _lastIndex.get())
+			{
+				s_logger.Warning() << "Discontinuity - negative difference!";
+				discontinuity = true;
+			}
+			else
+			{
+				s64 proportion = 1000 * (milliseconds - _lastIndex.get()) / time_since_last;
+				if (!(500 < proportion && proportion < 2000))
+				{
+					s_logger.Warning() << "Discontinuity! Proportion: " << proportion;
+					discontinuity = true;
+				}
+			}
+
+			if (discontinuity && !_allowDiscontinuity)
+			{
+				s64 gap = _lastIndex.get() + time_since_last - milliseconds;
+				if (discontinuityValue != 0)
+					gap = _lastIndex.get() + discontinuityValue - milliseconds;
+				_firstIndex = _firstIndex.get() - gap;
+			}
+			_allowDiscontinuity = false;
+			_lastIndex = milliseconds;
+			return milliseconds - _firstIndex.get();
+		}
+	};
+
+
+}
+
+
+#endif
+
