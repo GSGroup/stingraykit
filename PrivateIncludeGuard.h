09c323563d9827095db583f2196c4434554076f7
Борис Сазонов <sazonov@spb.gs.ru>
2012-07-26 23:16:40 +0400

    Added PrivateIncludeGuard.h

---
+++ PrivateIncludeGuard.h
@@ -0,0 +1,9 @@
+#ifndef __GS_STINGRAY_TOOLKIT_PRIVATEINCLUDEGUARD_H__
+#define __GS_STINGRAY_TOOLKIT_PRIVATEINCLUDEGUARD_H__
+
+#if !defined(STINGRAY_LIB_SOURCE)
+#error "This header is ought to be private!"
+#endif
+
+#endif
+
