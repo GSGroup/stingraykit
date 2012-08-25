e3246682ca0f2bcab72ecf1f616a0fe75fa3cfc9
Борис Сазонов <sazonov@spb.gs.ru>
2012-08-25 17:41:26 +0400

    Implemented DiscontinuityRepairer::RepairRelative

--- DiscontinuityRepairer.h
+++ DiscontinuityRepairer.h
@@ -46,6 +46,11 @@ namespace stingray {
 			_allowDiscontinuity = true;
 		}
 
+		void Reset()
+		{
+			_firstIndex = _lastIndex = null;
+		}
+
 		u64 Repair(u64 milliseconds, u64 discontinuityValue = 0)
 		{
 			if (_paused)
@@ -66,34 +71,58 @@ namespace stingray {
 				return _lastIndex.get();
 			_timeSinceLastIndex.Restart();
 
-			bool discontinuity = false;
-			if (milliseconds < _lastIndex.get())
-			{
-				s_logger.Warning() << "Discontinuity - negative difference!";
-				discontinuity = true;
-			}
-			else
-			{
-				s64 proportion = 1000 * (milliseconds - _lastIndex.get()) / time_since_last;
-				if (!(500 < proportion && proportion < 2000))
-				{
-					s_logger.Warning() << "Discontinuity! Proportion: " << proportion;
-					discontinuity = true;
-				}
-			}
+			s64 gap = CalculateGap(_lastIndex.get() - milliseconds, time_since_last, discontinuityValue);
 
-			if (discontinuity && _allowDiscontinuity)
-				s_logger.Info() << "Skipping allowed discontinuity";
-			else if (discontinuity)
-			{
-				s64 gap = _lastIndex.get() + time_since_last - milliseconds;
-				if (discontinuityValue != 0)
-					gap = _lastIndex.get() + discontinuityValue - milliseconds;
+			if (gap != 0 && !_allowDiscontinuity)
 				_firstIndex = _firstIndex.get() - gap;
-			}
+
 			_allowDiscontinuity = false;
 			_lastIndex = milliseconds;
 			return milliseconds - _firstIndex.get();
+		}
+
+		u64 RepairRelative(u64 milliseconds)
+		{
+			if (_paused)
+			{
+				s_logger.Warning() << "Stream is paused!" << Backtrace().Get();
+				return 0;
+			}
+
+			if (!_lastIndex)
+			{
+				_lastIndex = milliseconds;
+				_timeSinceLastIndex.Restart();
+				return 0;
+			}
+
+			s64 time_since_last = _timeSinceLastIndex.ElapsedMilliseconds();
+			if (time_since_last == 0)
+				return 0;
+			_timeSinceLastIndex.Restart();
+
+			s64 gap = CalculateGap(milliseconds - _lastIndex.get(), time_since_last);
+
+			if (gap != 0 && !_allowDiscontinuity)
+				_lastIndex = _lastIndex.get() - gap;
+
+			_allowDiscontinuity = false;
+			u64 result = milliseconds - _lastIndex.get();
+			_lastIndex = milliseconds;
+			return result;
+		}
+
+	private:
+		static s64 CalculateGap(u64 stampDifference, u64 timeDifference, u64 hintDifference = 0)
+		{
+			if (stampDifference > 0)
+			{
+				s64 proportion = 1000 * stampDifference / timeDifference;
+				if (500 < proportion && proportion < 2000)
+					return 0;
+			}
+
+			return (hintDifference ? hintDifference : timeDifference) - stampDifference;
 		}
 	};
 
