1f04696d630c542aece5d97a76fdf81ee0f1b023
Dmitry Koplyarov <koplyarov@spb.gs.ru>
2012-11-19 19:10:57 +0400

    Atomic::CompareAndExchange method

--- Atomic.h
+++ Atomic.h
@@ -16,23 +16,31 @@ namespace stingray
 	struct Atomic
 	{
 #if HAVE_SYNC_AAF
-		template < typename T>
+		template < typename T >
 		static FORCE_INLINE T Inc(T& ptr) { return __sync_add_and_fetch(&ptr, 1); }
 
-		template < typename T>
+		template < typename T >
 		static FORCE_INLINE T Dec(T& ptr) { return __sync_sub_and_fetch(&ptr, 1); }
+
+		/// @returns: value before exchange
+		template < typename T >
+		static FORCE_INLINE T CompareAndExchange(T& ptr, T oldVal, T newVal) { return __sync_val_compare_and_swap(&ptr, oldVal, newVal); }
 #elif HAVE_SYNC_EAA || HAVE_SYNC_EAA_EXT
-		template < typename T>
+		template < typename T >
 		static FORCE_INLINE T Inc(T& ptr) { return __gnu_cxx::__exchange_and_add(&ptr,  1) + 1; }
 
-		template < typename T>
+		template < typename T >
 		static FORCE_INLINE T Dec(T& ptr) { return __gnu_cxx::__exchange_and_add(&ptr, -1) - 1; }
 #elif HAVE_ATOMIC_H
-		template < typename T>
+		template < typename T >
 		static FORCE_INLINE T Inc(T& ptr) { return atomic_increment_val(&ptr); }
 
-		template < typename T>
+		template < typename T >
 		static FORCE_INLINE T Dec(T& ptr) { return atomic_decrement_val(&ptr); }
+
+		/// @returns: value before exchange
+		template < typename T >
+		static FORCE_INLINE T CompareAndExchange(T& ptr, T oldVal, T newVal) { return atomic_compare_and_exchange_val_acq(&ptr, newVal, oldVal); }
 #else
 #	error "no atomic operations"
 #endif
