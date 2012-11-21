32ec6bc4b3d316e11fa7f4bc16a3314e73d40371
Dmitry Koplyarov <koplyarov@spb.gs.ru>
2012-11-21 23:14:05 +0400

    Better copy-on-write MapDictionary enumerators

--- MapDictionary.h
+++ MapDictionary.h
@@ -39,56 +39,91 @@ namespace stingray
 		typedef ValueType_							ValueType;
 		typedef KeyValuePair<KeyType, ValueType>	PairType;
 		typedef std::map<KeyType, ValueType>		MapType;
+		TOOLKIT_DECLARE_PTR(MapType);
 
 	private:
-		MapType		_map;
+		struct Holder
+		{
+			MapTypePtr		Map;
+			Holder(const MapTypePtr& map) : Map(map) { }
+		};
+		TOOLKIT_DECLARE_PTR(Holder);
+
+		struct ReverseEnumerable : public virtual IEnumerable<PairType>
+		{
+			HolderPtr		_holder;
+
+			ReverseEnumerable(const HolderPtr& holder) : _holder(holder) { }
+
+			virtual shared_ptr<IEnumerator<PairType> > GetEnumerator() const
+			{ return make_shared<EnumeratorWrapper<typename MapType::value_type, PairType> >(EnumeratorFromStlIterators(_holder->Map->rbegin(), _holder->Map->rend(), _holder)); }
+		};
+
+	private:
+		MapTypePtr				_map;
+		mutable HolderWeakPtr	_mapEnumeratorHolder;
 
 	public:
-		virtual int GetCount() const { return _map.size(); }
+		MapDictionary()
+			: _map(new MapType)
+		{ }
+
+		virtual int GetCount() const { return _map->size(); }
 
 		virtual ValueType Get(const KeyType& key) const
 		{
-			typename MapType::const_iterator it = _map.find(key);
-			TOOLKIT_CHECK(it != _map.end(), Detail::KeyNotFoundExceptionFactory<KeyType>::Create(key));
+			typename MapType::const_iterator it = _map->find(key);
+			TOOLKIT_CHECK(it != _map->end(), Detail::KeyNotFoundExceptionFactory<KeyType>::Create(key));
 			return it->second;
 		}
 
 		virtual void Set(const KeyType& key, const ValueType& value)
 		{
-			typename MapType::iterator it = _map.find(key);
-			if (it != _map.end())
+			CopyOnWrite();
+			typename MapType::iterator it = _map->find(key);
+			if (it != _map->end())
 				it->second = value;
 			else
-				_map.insert(std::make_pair(key, value));
+				_map->insert(std::make_pair(key, value));
 		}
 
 		virtual void Remove(const KeyType& key)
 		{
-			typename MapType::iterator it = _map.find(key);
-			TOOLKIT_CHECK(it != _map.end(), Detail::KeyNotFoundExceptionFactory<KeyType>::Create(key));
-			_map.erase(it);
+			CopyOnWrite();
+			typename MapType::iterator it = _map->find(key);
+			TOOLKIT_CHECK(it != _map->end(), Detail::KeyNotFoundExceptionFactory<KeyType>::Create(key));
+			_map->erase(it);
 		}
 
 		virtual bool ContainsKey(const KeyType& key) const
-		{ return _map.find(key) != _map.end(); }
+		{ return _map->find(key) != _map->end(); }
 
 		virtual shared_ptr<IEnumerator<PairType> > GetEnumerator() const
 		{
-			shared_ptr<std::vector<PairType> > result(new std::vector<PairType>);
-			result->reserve(_map.size());
-			for (typename MapType::const_iterator it = _map.begin(); it != _map.end(); ++it)
-				result->push_back(PairType(it->first, it->second));
-			return EnumeratorFromStlContainer(*result, result);
+			shared_ptr<Holder> map_enumerator_holder = _mapEnumeratorHolder.lock();
+			if (!map_enumerator_holder)
+				_mapEnumeratorHolder = (map_enumerator_holder = make_shared<Holder>(_map));
+
+			return make_shared<EnumeratorWrapper<typename MapType::value_type, PairType> >(EnumeratorFromStlContainer(*_map, map_enumerator_holder));
 		}
 
 		virtual shared_ptr<IEnumerable<PairType> > Reverse() const
 		{
-			shared_ptr<std::vector<PairType> > result(new std::vector<PairType>);
-			result->reserve(_map.size());
-			for (typename MapType::const_iterator it = _map.begin(); it != _map.end(); ++it)
-				result->push_back(PairType(it->first, it->second));
-			std::reverse(result->begin(), result->end());
-			return EnumerableFromStlContainer(*result, result);
+			shared_ptr<Holder> map_enumerator_holder = _mapEnumeratorHolder.lock();
+			if (!map_enumerator_holder)
+				_mapEnumeratorHolder = (map_enumerator_holder = make_shared<Holder>(_map));
+
+			return make_shared<ReverseEnumerable>(map_enumerator_holder);
+		}
+
+	private:
+		void CopyOnWrite()
+		{
+			if (_mapEnumeratorHolder.lock())
+			{
+				_map.reset(new MapType(*_map));
+				_mapEnumeratorHolder.reset();
+			}
 		}
 	};
 
