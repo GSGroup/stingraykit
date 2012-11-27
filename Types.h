03179c00ede03167a1bca72065300cbd3cea48eb
Boris Sazonov <sazonov@spb.gs.ru>
2012-11-27 13:51:13 +0400

    Fixed Types.h - added back checks on signed/unsigned. Clarified building BuiltinTypes typelist

--- Types.h
+++ Types.h
@@ -2,7 +2,6 @@
 #define __GS_DVRLIB_TOOLKIT_TYPES_H__
 
 
-#include <stdint.h>
 #include <stingray/toolkit/TypeList.h>
 
 
@@ -25,7 +24,6 @@ namespace stingray
 			CompileTimeAssert< sizeof(Type) == Size > ERROR__invalid_size_of_##Type; \
 		};
 
-#if 0 // to avoid 'unsigned < 0' comparison warnings
 #define DETAIL_TOOLKIT_DECLARE_INT_TYPE(NativeType_, BitsCount_) \
 	typedef unsigned NativeType_	u##BitsCount_; \
 	typedef signed NativeType_		s##BitsCount_; \
@@ -35,39 +33,18 @@ namespace stingray
 	TOOLKIT_ASSERT_SIGNED(s##BitsCount_,	true) \
 	template <> struct IntType<BitsCount_, true> { typedef s##BitsCount_ ValueT; }; \
 	template <> struct IntType<BitsCount_, false> { typedef u##BitsCount_ ValueT; }
-#endif
-	
+
 	template < size_t BitsCount, bool Signed = false >
 	struct IntType;
 
-#if 0 // to avoid 'unsigned < 0' comparison warnings
 	DETAIL_TOOLKIT_DECLARE_INT_TYPE(char,		8);
 	DETAIL_TOOLKIT_DECLARE_INT_TYPE(short,		16);
 	DETAIL_TOOLKIT_DECLARE_INT_TYPE(int,		32);
 	DETAIL_TOOLKIT_DECLARE_INT_TYPE(long long,	64);
-#endif
 
-	typedef int8_t		s8;
-	typedef uint8_t		u8;
-	typedef int16_t		s16;
-	typedef uint16_t	u16;
-	typedef int32_t		s32;
-	typedef uint32_t	u32;
-	typedef long long	s64; // to avoid type conflicts with existing 123LL-like constants and "%lld" format specs
-	TOOLKIT_ASSERT_SIZE_OF(s64, 8); // de bene esse
-	typedef uint64_t	u64;
-	template <> struct IntType<8, true> { typedef s8 ValueT; };
-	template <> struct IntType<8, false> { typedef u8 ValueT; };
-	template <> struct IntType<16, true> { typedef s16 ValueT; };
-	template <> struct IntType<16, false> { typedef u16 ValueT; };
-	template <> struct IntType<32, true> { typedef s32 ValueT; };
-	template <> struct IntType<32, false> { typedef u32 ValueT; };
-	template <> struct IntType<64, true> { typedef s64 ValueT; };
-	template <> struct IntType<64, false> { typedef u64 ValueT; };
-
-	typedef TYPELIST(unsigned, unsigned long, size_t, int, off_t, u8, s8, u16, s16, u32, s32, u64, s64, float, double, bool) NotClassTypes;
-	typedef TYPELIST(unsigned, unsigned long, size_t, int, off_t, u8, s8, u16, s16, u32, s32, u64, s64) IntTypes;
 	typedef TYPELIST(u8, s8, u16, s16, u32, s32, u64, s64) FixedWidthIntTypes;
+	typedef TypeListMerge<TypeList_2<FixedWidthIntTypes, TYPELIST(unsigned, unsigned long, size_t, int, off_t)> >::ValueT IntTypes;
+	typedef TypeListMerge<TypeList_2<IntTypes, TYPELIST(float, double, bool)> >::ValueT BuiltinTypes;
 
 	template < typename T >
 	struct IsIntType
