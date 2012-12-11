994b59174c2af63bf4e5eac0108e180cbd4b84f3
Boris Sazonov <sazonov@spb.gs.ru>
2012-12-11 19:56:25 +0400

    Replaced stingray::Rect with BasicRect template and typedef

--- Rect.h
+++ Rect.h
@@ -8,6 +8,7 @@
 namespace stingray
 {
 
+
 	struct Size
 	{
 		int Width, Height;
@@ -26,27 +27,33 @@ namespace stingray
 		inline bool operator!=(const Size &other) const { return !((*this) == other); }
 	};
 
-	struct Rect
+
+	template<typename ValueType_>
+	struct BasicRect
 	{
-		int X1, Y1, X2, Y2;
+		typedef ValueType_ ValueType;
 
-		Rect() : X1(0), Y1(0), X2(0), Y2(0) { }
-		Rect(int x1, int y1, int x2, int y2) : X1(x1), Y1(y1), X2(x2), Y2(y2) { }
-		Rect(int w, int h) : X1(0), Y1(0), X2(w), Y2(h) { }
+		ValueType X1, Y1, X2, Y2;
 
-		int W() const	{ return X2 - X1; }
-		int H() const	{ return Y2 - Y1; }
+		BasicRect() : X1(0), Y1(0), X2(0), Y2(0) { }
+		BasicRect(ValueType x1, ValueType y1, ValueType x2, ValueType y2) : X1(x1), Y1(y1), X2(x2), Y2(y2) { }
+		BasicRect(ValueType w, ValueType h) : X1(0), Y1(0), X2(w), Y2(h) { }
 
-		Rect Move(Position d) const { return Rect(X1 + d.X, Y1 + d.Y, X2 + d.X, Y2 + d.Y); }
-		Rect Move(int dx, int dy) const { return Rect(X1 + dx, Y1 + dy, X2 + dx, Y2 + dy); }
-		Position GetTopLeft() const { return Position(X1, Y1); }
-		Position GetRightBottom() const { return Position(X2, Y2); }
+		ValueType W() const	{ return X2 - X1; }
+		ValueType H() const	{ return Y2 - Y1; }
 
-		Rect Intersect(const Rect& other) const
-		{ return Rect(std::max(X1, other.X1), std::max(Y1, other.Y1), std::min(X2, other.X2), std::min(Y2, other.Y2)); }
+		BasicRect Move(BasicPosition<ValueType> d) const	{ return BasicRect(X1 + d.X, Y1 + d.Y, X2 + d.X, Y2 + d.Y); }
+		BasicRect Move(ValueType dx, ValueType dy) const	{ return BasicRect(X1 + dx, Y1 + dy, X2 + dx, Y2 + dy); }
+		BasicPosition<ValueType> GetTopLeft() const			{ return BasicPosition<ValueType>(X1, Y1); }
+		BasicPosition<ValueType> GetRightBottom() const		{ return BasicPosition<ValueType>(X2, Y2); }
+
+		BasicRect Intersect(const BasicRect& other) const
+		{ return BasicRect(std::max(X1, other.X1), std::max(Y1, other.Y1), std::min(X2, other.X2), std::min(Y2, other.Y2)); }
 
 		std::string ToString() const { return StringBuilder() % "(" % GetTopLeft() % ", " % GetRightBottom() % ")"; }
 	};
+	typedef BasicRect<int> Rect;
+
 
 }
 
