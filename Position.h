8a33cb1aa3e2a0e68f79633601f6afb5da773065
Boris Sazonov <sazonov@spb.gs.ru>
2012-12-11 19:55:44 +0400

    Replaced stingray::Position with BasicPosition template and typedef

--- Position.h
+++ Position.h
@@ -8,15 +8,19 @@
 namespace stingray
 {
 
-	struct Position
-	{
-		int X, Y;
 
-		Position() : X(0), Y(0) { }
-		Position(int x, int y) : X(x), Y(y) { }
+	template<typename ValueType>
+	struct BasicPosition
+	{
+		ValueType X, Y;
+
+		BasicPosition() : X(0), Y(0) { }
+		BasicPosition(ValueType x, ValueType y) : X(x), Y(y) { }
 
 		std::string ToString() const { return StringBuilder() % "(" % X % ", " % Y % ")"; }
 	};
+	typedef BasicPosition<int> Position;
+
 
 }
 
