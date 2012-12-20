bd5b2b3faa45b6f55fc362a0ee225713af79771b
Boris Sazonov <sazonov@spb.gs.ru>
2012-12-20 14:33:19 +0400

    Reimplemented optional - added inplace storage for builtin types

--- optional.h
+++ optional.h
@@ -4,30 +4,87 @@
 #include <stingray/toolkit/safe_bool.h>
 #include <stingray/toolkit/unique_ptr.h>
 
-namespace stingray {
+namespace stingray
+{
 
 
 	namespace Detail
 	{
-		template<typename T, bool IsRef = IsReference<T>::Value >
-		struct OptionalHelper
+		template<typename T, bool StoreByValue = IsBuiltinType<T>::Value || IsPointer<T>::Value || Is1ParamTemplate<shared_ptr, T>::Value, bool IsRef = IsReference<T>::Value>
+		struct OptionalImpl
 		{
-			typedef T									ParamType;
-			typedef T									StoredType;
+		public:
+			typedef T&			ParamType;
+			typedef const T&	ConstParamType;
+			typedef T*			PtrParamType;
+			typedef const T*	ConstPtrParamType;
 
-			static const StoredType& ToStored(const ParamType& val)	{ return val; }
-			static ParamType& ToParam(StoredType& val)				{ return val; }
-			static const ParamType& ToParam(const StoredType& val)	{ return val; }
+		private:
+			unique_ptr<T>	_value;
+
+		public:
+			ConstParamType get() const			{ return *_value; }
+			ParamType get()						{ return *_value; }
+
+			ConstPtrParamType get_ptr() const	{ return _value.get(); }
+			PtrParamType get_ptr()				{ return _value.get(); }
+
+			void reset()						{ _value.reset(); }
+			void reset(ConstParamType value)	{ _value.reset(new T(value)); }
+
+			bool is_initialized() const			{ return _value; }
 		};
 
-		template<typename T>
-		struct OptionalHelper<T, true>
-		{
-			typedef typename Dereference<T>::ValueT		ParamType;
-			typedef typename Dereference<T>::ValueT*	StoredType;
 
-			static StoredType ToStored(const ParamType& val)		{ return &val; }
-			static ParamType& ToParam(StoredType val)				{ return *val; }
+		template<typename T>
+		struct OptionalImpl<T, true, false>
+		{
+		public:
+			typedef T&			ParamType;
+			typedef const T&	ConstParamType;
+			typedef T*			PtrParamType;
+			typedef const T*	ConstPtrParamType;
+
+		private:
+			T		_value;
+			bool	_initialized;
+
+		public:
+			ConstParamType get() const			{ return _value; }
+			ParamType get()						{ return _value; }
+
+			ConstPtrParamType get_ptr() const	{ return &_value; }
+			PtrParamType get_ptr()				{ return &_value; }
+
+			void reset()						{ _value = T(); _initialized = false; }
+			void reset(ConstParamType value)	{ _value = value; _initialized = true; }
+
+			bool is_initialized() const			{ return _initialized; }
+		};
+
+
+		template<typename T>
+		struct OptionalImpl<T, false, true>
+		{
+		public:
+			typedef typename Dereference<T>::ValueT RawT;
+			typedef RawT&	ParamType;
+			typedef RawT&	ConstParamType;
+			typedef RawT*	PtrParamType;
+			typedef RawT*	ConstPtrParamType;
+
+		private:
+			T*		_value;
+			bool	_initialized;
+
+		public:
+			ConstParamType get() const			{ return *_value; }
+			ConstPtrParamType get_ptr() const	{ return _value; }
+
+			void reset()						{ _value = null; _initialized = false; }
+			void reset(ConstParamType value)	{ _value = &value; _initialized = true; }
+
+			bool is_initialized() const			{ return _initialized; }
 		};
 	}
 
@@ -35,82 +92,58 @@ namespace stingray {
 	struct optional : public safe_bool<optional<T> >
 	{
 	private:
-		typedef Detail::OptionalHelper<T>	Helper;
-		typedef typename Helper::StoredType	StoredType;
+		typedef Detail::OptionalImpl<T>	Impl;
 
 	public:
-		typedef typename Helper::ParamType	ParamType;
+		typedef typename Impl::ParamType			ParamType;
+		typedef typename Impl::ConstParamType		ConstParamType;
+		typedef typename Impl::PtrParamType			PtrParamType;
+		typedef typename Impl::ConstPtrParamType	ConstPtrParamType;
 
 	private:
-		unique_ptr<StoredType>				_value;
+		Impl	_impl;
 
 	public:
-		optional()
-		{}
+		optional()									{ reset(); }
+		optional(const NullPtrType&)				{ reset(); }
+		optional(ConstParamType value)				{ reset(value); }
+		optional(const optional& other)				{ reset(other); }
 
-		optional(const NullPtrType&)
-		{}
+		optional& operator=(const NullPtrType&)		{ reset();		return *this; }
+		optional& operator=(ConstParamType value)	{ reset(value);	return *this; }
+		optional& operator=(const optional& other)	{ reset(other);	return *this; }
 
-		optional(const ParamType& value) : _value(new StoredType(Helper::ToStored(value)))
-		{}
+		ConstParamType get() const					{ CheckInitialized(); return _impl.get(); }
+		ParamType       get()						{ CheckInitialized(); return _impl.get(); }
 
-		optional(const optional& other)
-		{
-			if (other._value)
-				_value.reset(new StoredType(*other._value));
-		}
+		ConstPtrParamType get_ptr() const			{ CheckInitialized(); return _impl.get_ptr(); }
+		PtrParamType get_ptr()						{ CheckInitialized(); return _impl.get_ptr(); }
 
-		template<class U>
-		explicit optional(const optional<U>& other)
-		{
-			if (other._value)
-				_value.reset(new StoredType(*other._value));
-		}
+		ConstPtrParamType operator->() const		{ return get_ptr(); }
+		PtrParamType operator->()					{ return get_ptr(); }
 
+		ConstParamType operator*() const			{ return get(); }
+		ParamType operator*()						{ return get(); }
 
-		optional& operator=(const NullPtrType&)		{ reset();								return *this; }
-		optional& operator=(const ParamType& value)	{ _value.reset(new StoredType(value));	return *this; }
+		void reset()								{ _impl.reset(); }
+		void reset(ConstParamType value)			{ _impl.reset(value); }
 
-		optional& operator=(const optional& other)
-		{
-			if (other._value)
-				_value.reset(new StoredType(*other._value));
-			else
-				_value.reset();
-			return *this;
-		}
-
-		template<class U>
-		optional& operator=(const optional<U>& other)
-		{
-			if (other._value)
-				_value.reset(new StoredType(*other._value));
-			else
-				_value.reset();
-		}
-
-
-	    const ParamType& get() const		{ return Helper::ToParam(*TOOLKIT_REQUIRE_NOT_NULL(_value)); }
-    	ParamType&       get()				{ return Helper::ToParam(*TOOLKIT_REQUIRE_NOT_NULL(_value)); }
-
-		const ParamType* get_ptr() const	{ return &get(); }
-		ParamType* get_ptr()				{ return &get(); }
-
-		const ParamType* operator->() const	{ return get_ptr(); }
-		ParamType* operator->()				{ return get_ptr(); }
-
-		const ParamType& operator*() const	{ return get(); }
-		ParamType& operator*()				{ return get(); }
-
-		void reset()						{ _value.reset(); }
-		void reset(const ParamType& value)	{ _value.reset(new StoredType(Helper::ToStored(value))); }
-
-		bool is_initialized() const			{ return _value; }
-		bool boolean_test() const			{ return _value; }
+		bool is_initialized() const					{ return _impl.is_initialized(); }
+		bool boolean_test() const					{ return is_initialized(); }
 
 		bool operator<(const optional& rhs) const
 		{ return rhs.is_initialized() && (!is_initialized() || (get() < rhs.get())); }
 		TOOLKIT_GENERATE_COMPARISON_OPERATORS_FROM_LESS(optional);
+
+	private:
+		void CheckInitialized() const				{ TOOLKIT_CHECK(is_initialized(), "Not initialized!"); }
+		void reset(const optional& other)
+		{
+			if (other.is_initialized())
+				reset(other.get());
+			else
+				reset();
+		}
 	};
 	TOOLKIT_GENERATE_FREE_COMPARISON_OPERATORS_FOR_TEMPLATE_CLASS(template<typename T>, optional<T>);
 
