ca3635c792eee116fe9299e3d2c617fa4da9b9b6
Boris Sazonov <sazonov@spb.gs.ru>
2012-12-24 19:45:53 +0400

    Cleaned up Atomic.h dependencies - replaced FORCE_INLINE with simple inline.

--- Atomic.h
+++ Atomic.h
@@ -9,33 +9,37 @@
 #	include <bits/atomicity.h>
 #endif
 
-#include <stingray/toolkit/toolkit.h>
-
 /*! \cond GS_INTERNAL */
 
 namespace stingray
 {
 
+#ifdef HAVE_SYNC_EAA
+	typedef _Atomic_word	atomic_int_type;
+#else
+	typedef int				atomic_int_type;
+#endif
+
 	struct Atomic
 	{
 #if HAVE_SYNC_AAF
 		template < typename T >
-		static FORCE_INLINE T Inc(T& ptr) { return __sync_add_and_fetch(&ptr, 1); }
+		static inline T Inc(T& ptr) { return __sync_add_and_fetch(&ptr, 1); }
 
 		template < typename T >
-		static FORCE_INLINE T Dec(T& ptr) { return __sync_sub_and_fetch(&ptr, 1); }
+		static inline T Dec(T& ptr) { return __sync_sub_and_fetch(&ptr, 1); }
 #elif HAVE_SYNC_EAA || HAVE_SYNC_EAA_EXT
 		template < typename T >
-		static FORCE_INLINE T Inc(T& ptr) { return __gnu_cxx::__exchange_and_add(&ptr,  1) + 1; }
+		static inline T Inc(T& ptr) { return __gnu_cxx::__exchange_and_add(&ptr,  1) + 1; }
 
 		template < typename T >
-		static FORCE_INLINE T Dec(T& ptr) { return __gnu_cxx::__exchange_and_add(&ptr, -1) - 1; }
+		static inline T Dec(T& ptr) { return __gnu_cxx::__exchange_and_add(&ptr, -1) - 1; }
 #elif HAVE_ATOMIC_H
 		template < typename T >
-		static FORCE_INLINE T Inc(T& ptr) { return atomic_increment_val(&ptr); }
+		static inline T Inc(T& ptr) { return atomic_increment_val(&ptr); }
 
 		template < typename T >
-		static FORCE_INLINE T Dec(T& ptr) { return atomic_decrement_val(&ptr); }
+		static inline T Dec(T& ptr) { return atomic_decrement_val(&ptr); }
 #else
 #	error "no atomic increment/decrement"
 #endif
@@ -43,21 +47,16 @@ namespace stingray
 #if HAVE_SYNC_AAF
 		/// @returns: value before exchange
 		template < typename T >
-		static FORCE_INLINE T CompareAndExchange(T& ptr, T oldVal, T newVal) { return __sync_val_compare_and_swap(&ptr, oldVal, newVal); }
+		static inline T CompareAndExchange(T& ptr, T oldVal, T newVal) { return __sync_val_compare_and_swap(&ptr, oldVal, newVal); }
 #elif HAVE_ATOMIC_H
 		/// @returns: value before exchange
 		template < typename T >
-		static FORCE_INLINE T CompareAndExchange(T& ptr, T oldVal, T newVal) { return atomic_compare_and_exchange_val_acq(&ptr, newVal, oldVal); }
+		static inline T CompareAndExchange(T& ptr, T oldVal, T newVal) { return atomic_compare_and_exchange_val_acq(&ptr, newVal, oldVal); }
 #else
 #	error "No CompareAndExchange"
 #endif
 	};
 
-#ifdef HAVE_SYNC_EAA
-	typedef _Atomic_word	atomic_int_type;
-#else
-	typedef int				atomic_int_type;
-#endif
 
 
 }
