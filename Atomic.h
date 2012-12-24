56c591ad2abb3d8718132a5e425f48a8bbab94aa
Boris Sazonov <sazonov@spb.gs.ru>
2012-12-24 19:53:42 +0400

    Implemented Atomic::Load and Atomic::Store

--- Atomic.h
+++ Atomic.h
@@ -28,18 +28,38 @@ namespace stingray
 
 		template < typename T >
 		static inline T Dec(T& ptr) { return __sync_sub_and_fetch(&ptr, 1); }
+
+		template < typename T >
+		static inline T Load(T& ptr) { return __sync_add_and_fetch(&ptr, 0); }
+
+		template < typename T1, typename T2 >
+		static inline void Store(T1& ptr, T2 val)
+		{
+			ptr = val;
+			__sync_synchronize();
+		}
 #elif HAVE_SYNC_EAA || HAVE_SYNC_EAA_EXT
 		template < typename T >
 		static inline T Inc(T& ptr) { return __gnu_cxx::__exchange_and_add(&ptr,  1) + 1; }
 
 		template < typename T >
 		static inline T Dec(T& ptr) { return __gnu_cxx::__exchange_and_add(&ptr, -1) - 1; }
+
+		template < typename T >
+		static inline T Load(T& ptr) { return __gnu_cxx::__exchange_and_add(&ptr, 0); }
+		// TODO: Implement Store
 #elif HAVE_ATOMIC_H
 		template < typename T >
 		static inline T Inc(T& ptr) { return atomic_increment_val(&ptr); }
 
 		template < typename T >
 		static inline T Dec(T& ptr) { return atomic_decrement_val(&ptr); }
+
+		template < typename T >
+		static inline T Load(T& ptr) { return atomic_exchange_and_add(&ptr, 0); }
+
+		template < typename T >
+		static inline void Store(T& ptr, T val) { atomic_exchange_acq(&ptr, val); }
 #else
 #	error "no atomic increment/decrement"
 #endif
