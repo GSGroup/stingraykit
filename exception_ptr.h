1b6dc114f4b3f436f8802cb87f9c35c0ed1c1a2c
Vladimir Menshakov <menshakov@spb.gs.ru>
2013-01-11 16:59:43 +0400

    added exception_ptr for poors

---
+++ exception_ptr.h
@@ -0,0 +1,34 @@
+#ifndef STINGRAY_TOOLKIT_EXCEPTION_PTR_H
+#define STINGRAY_TOOLKIT_EXCEPTION_PTR_H
+
+#include <stingray/toolkit/shared_ptr.h>
+#include <stingray/toolkit/exception.h>
+
+#include <exception>
+#include <string>
+
+namespace stingray
+{
+	class rethrown_exception : public std::exception
+	{
+		std::string _what;
+
+	public:
+		rethrown_exception(const std::exception &ex) throw() : _what(diagnostic_information(ex)) {}
+		virtual ~rethrown_exception() throw() {}
+
+		virtual const char* what() const throw() { return _what.c_str(); }
+	};
+
+	typedef shared_ptr<rethrown_exception> exception_ptr;
+
+	inline exception_ptr make_exception_ptr(const std::exception &ex)
+	{ return exception_ptr(new rethrown_exception(ex)); }
+
+	inline void rethrow_exception(const exception_ptr& ex)
+	{ if (ex) throw *ex; }
+
+}
+
+#endif
+
