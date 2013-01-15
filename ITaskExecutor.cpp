8368e28e1f5dfba923e78ebb97517a76d1125df8
Ярош Андрей <yarosh@spb.gs.ru>
2013-01-15 20:26:38 +0400

    deadlock with channels fixed

--- ITaskExecutor.cpp
+++ ITaskExecutor.cpp
@@ -1,10 +1,6 @@
 #include <stingray/toolkit/ITaskExecutor.h>
 
-#if defined(PLATFORM_OSPLUS)
-#	include <stingray/platform/osplus/threads/OS21TaskExecutor.h>
-#else
-#	include <stingray/toolkit/ThreadTaskExecutor.h>
-#endif
+#include <stingray/toolkit/ThreadTaskExecutor.h>
 #include <stingray/log/Logger.h>
 
 
@@ -12,11 +8,7 @@ namespace stingray
 {
 
 	ITaskExecutorPtr ITaskExecutor::Create(const std::string& name, const function<void(const std::exception&)>& exceptionHandler)
-#if defined(PLATFORM_OSPLUS)
-	{ return make_shared<osplus::TaskExecutor>(name, exceptionHandler); }
-#else
 	{ return make_shared<ThreadTaskExecutor>(name, exceptionHandler); }
-#endif
 
 
 	void ITaskExecutor::DefaultExceptionHandler(const std::exception& ex)
