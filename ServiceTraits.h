6539d83b5b78616902459b4d7e9748bf9adbd53e
Dmitry Koplyarov <koplyarov@spb.gs.ru>
2013-02-25 16:33:17 +0000

    Added parametrized ctors to DerivedService

--- ServiceTraits.h
+++ ServiceTraits.h
@@ -23,6 +23,11 @@ namespace stingray
 		typedef typename ServiceBase_::ServiceInterfaces									ServiceInterfaces;
 		typedef typename TypeListMerge<TypeList_2<typename ServiceBase_::Dependencies,
 			typename ToTypeList<Dependencies_>::ValueT> >::ValueT							Dependencies;
+
+		DerivedService() { }
+
+		template < typename T > DerivedService(const T& p) : ServiceBase_(p) { }
+		template < typename T1, typename T2 > DerivedService(const T1& p1, const T2& p2) : ServiceBase_(p1, p2) { }
 	};
 
 	template < typename Service >
