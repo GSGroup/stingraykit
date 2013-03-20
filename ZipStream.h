9fe360eadb5693afefa9ba87c24facfb0f6bd48d
Vladimir Menshakov <menshakov@spb.gs.ru>
2013-03-20 16:28:32 +0400

    moarrr tracers

--- ZipStream.h
+++ ZipStream.h
@@ -2,11 +2,13 @@
 #define STINGRAY_TOOLKIT_ZIPSTREAM_H
 
 #include <stingray/toolkit/ByteData.h>
+#include <stingray/log/Logger.h>
 
 namespace stingray
 {
 	class ZipStream
 	{
+		static NamedLogger s_logger;
 	public:
 		static bool CheckGzipHeader(const ConstByteData &src);
 		static ByteArray Compress(const ConstByteData &src, int level, bool gzHeader);
