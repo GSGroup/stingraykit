d57408c01f189448088231bf536eef00d53289d5
Vladimir Menshakov <menshakov@spb.gs.ru>
2013-05-06 20:17:01 +0400

    removed signal include from IFileSystem.h, fixed compilation

--- IByteStream.h
+++ IByteStream.h
@@ -15,6 +15,16 @@ namespace stingray
 
 		virtual size_t Read(void* data, size_t count) = 0;
 		virtual size_t Write(const void* data, size_t count) = 0;
+
+		//fixme: make THIS interface methods
+		template<typename Data>
+		inline size_t ReadData(const Data &data)
+		{ return Read(data.data(), data.size()); }
+
+		template<typename Data>
+		inline size_t WriteData(Data data)
+		{ return Write(data.data(), data.size()); }
+
 		virtual void Seek(int offset, SeekMode mode = SeekMode::Begin) = 0;
 		virtual size_t Tell() const = 0;
 	};
