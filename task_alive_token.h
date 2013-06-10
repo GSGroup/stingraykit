a7abc9403f8321ba2f25ed7bb1738fd94f3aa257
Dmitry Koplyarov <koplyarov@spb.gs.ru>
2013-06-10 14:04:55 +0400

    function_with_token template

--- task_alive_token.h
+++ task_alive_token.h
@@ -117,6 +117,39 @@ namespace stingray
 
 	//void InvokeTask(const function<void ()>& task, const TaskLifeToken::ValueWeakPtr& tokenValue);
 
+
+	template < typename Signature >
+	class function_with_token;
+
+#define DETAIL_TOOLKIT_DECLARE_FUNCTION_WITH_TOKEN(ParamTypenames_, ParamTypes_, ParamDecl_, ParamUsage_) \
+	template < ParamTypenames_ > \
+	class function_with_token<void(ParamTypes_)> : public function_info<void(ParamTypes_)> \
+	{ \
+		typedef function<void(ParamTypes_)>	FuncType; \
+	private: \
+		FuncType				_func; \
+		FutureExecutionTester	_execTester; \
+	public: \
+		function_with_token(const FuncType& func, const FutureExecutionTester& execTester) \
+			: _func(func), _execTester(execTester) \
+		{ } \
+		inline void operator ()(ParamDecl_) const \
+		{ \
+			LocalExecutionGuard guard; \
+			if (_execTester.Execute(guard)) \
+				_func(ParamUsage_); \
+		} \
+	}
+
+#define TY typename
+	DETAIL_TOOLKIT_DECLARE_FUNCTION_WITH_TOKEN(/**/, /**/, /**/, /**/);
+	DETAIL_TOOLKIT_DECLARE_FUNCTION_WITH_TOKEN(MK_PARAM(TY T1), MK_PARAM(T1), MK_PARAM(T1 p1), MK_PARAM(p1));
+	DETAIL_TOOLKIT_DECLARE_FUNCTION_WITH_TOKEN(MK_PARAM(TY T1, TY T2), MK_PARAM(T1, T2), MK_PARAM(T1 p1, T2 p2), MK_PARAM(p1, p2));
+	DETAIL_TOOLKIT_DECLARE_FUNCTION_WITH_TOKEN(MK_PARAM(TY T1, TY T2, TY T3), MK_PARAM(T1, T2, T3), MK_PARAM(T1 p1, T2 p2, T3 p3), MK_PARAM(p1, p2, p3));
+	DETAIL_TOOLKIT_DECLARE_FUNCTION_WITH_TOKEN(MK_PARAM(TY T1, TY T2, TY T3, TY T4), MK_PARAM(T1, T2, T3, T4), MK_PARAM(T1 p1, T2 p2, T3 p3, T4 p4), MK_PARAM(p1, p2, p3, p4));
+	DETAIL_TOOLKIT_DECLARE_FUNCTION_WITH_TOKEN(MK_PARAM(TY T1, TY T2, TY T3, TY T4, TY T5), MK_PARAM(T1, T2, T3, T4, T5), MK_PARAM(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5), MK_PARAM(p1, p2, p3, p4, p5));
+#undef TY
+
 }
 
 
