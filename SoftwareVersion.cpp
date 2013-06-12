52d2fada0fe47fcc25cd7b95566508f06b578b38
Sergey Zubov <zubov@spb.gs.ru>
2013-06-12 21:12:13 +0400

    fixed version parsing

--- SoftwareVersion.cpp
+++ SoftwareVersion.cpp
@@ -9,7 +9,7 @@ namespace stingray
 	SoftwareVersion SoftwareVersion::FromString(const std::string& version)
 	{
 		unsigned major, minor, build;
-		TOOLKIT_CHECK(std::sscanf("%u.%u.%u", version.c_str(), &major, &minor, &build) == 3, FormatException());
+		TOOLKIT_CHECK(std::sscanf(version.c_str(), "%u.%u.%u", &major, &minor, &build) == 3, FormatException());
 		return SoftwareVersion(major, minor, build);
 	}
 
