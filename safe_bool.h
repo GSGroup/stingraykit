6c539b9b62b274dbbda77874214415ba7a3a06f7
Vladimir Menshakov <menshakov@spb.gs.ru>
2013-08-13 15:11:34 +0400

    fixed formatting in safe_bool, but safe_bool_base into Detail namespace

--- safe_bool.h
+++ safe_bool.h
@@ -6,30 +6,33 @@
 namespace stingray 
 {
 
-class safe_bool_base
-{
-protected:
-	void this_type_does_not_support_comparisons() const {}
-	
-	inline safe_bool_base() {}
-	inline ~safe_bool_base() {}
-
-	inline safe_bool_base(const safe_bool_base&) {}
-	inline safe_bool_base& operator=(const safe_bool_base&) {return *this;}
-};
-
-
-template <typename T = void> class safe_bool : private safe_bool_base {
-	typedef void (safe_bool::*bool_type)() const;
-public:
-	inline operator bool_type() const
+	namespace Detail
 	{
-		return (static_cast<const T*>(this))->boolean_test()? &safe_bool::this_type_does_not_support_comparisons : 0;
+		class safe_bool_base
+		{
+		protected:
+			void this_type_does_not_support_comparisons() const {}
+
+			inline safe_bool_base() {}
+			inline ~safe_bool_base() {}
+
+			inline safe_bool_base(const safe_bool_base&) {}
+			inline safe_bool_base& operator=(const safe_bool_base&) {return *this;}
+		};
 	}
 
-protected:
-	inline ~safe_bool() {}
-};
+
+	template <typename T = void> class safe_bool : private Detail::safe_bool_base {
+		typedef void (safe_bool::*bool_type)() const;
+	public:
+		inline operator bool_type() const
+		{
+			return (static_cast<const T*>(this))->boolean_test()? &safe_bool::this_type_does_not_support_comparisons : 0;
+		}
+
+	protected:
+		inline ~safe_bool() {}
+	};
 
 }
 
