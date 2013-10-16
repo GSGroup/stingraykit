8b8476319e36ea48e8dd226de9868542baaf3ed5
Boris Sazonov <sazonov@spb.gs.ru>
2013-10-16 19:02:24 +0400

    Removed whitespaces at end of lines

--- DependsOn.h
+++ DependsOn.h
@@ -27,7 +27,7 @@ namespace stingray
 		};
 
 		template < typename Dependency,
-			bool IsSingleton_ = IsSingleton<Dependency>::Value, 
+			bool IsSingleton_ = IsSingleton<Dependency>::Value,
 			bool IsCustomDependecy_ = IsCustomDependency<Dependency>::Value >
 		struct InitDependencyImpl
 		{
@@ -78,34 +78,34 @@ namespace stingray
 	protected:
 		Dependent()
 		{ }
-	
+
 	public:
 		virtual void InitDependencies() const
 		{ ForEachInTypeList<typename ToTypeList<Dependencies>::ValueT, Detail::InitDependency>::Do(); }
 	};
 
-	
+
 	template < typename Base, typename Dependencies >
 	class DependentWithBase : public Base
 	{
 	protected:
 		DependentWithBase() { }
-	
+
 		template < typename T1 >
 		DependentWithBase(const T1& p1) : Base(p1) { }
 
 		template < typename T1, typename T2 >
 		DependentWithBase(const T1& p1, const T2& p2) : Base(p1, p2) { }
-	
+
 	public:
 		virtual void InitDependencies() const
 		{
-			CompileTimeAssert<Inherits<Base, IDependent>::Value > ERROR__base_class_must_inherit_from_IDependent; 
+			CompileTimeAssert<Inherits<Base, IDependent>::Value > ERROR__base_class_must_inherit_from_IDependent;
 			(void)ERROR__base_class_must_inherit_from_IDependent;
 			Base::InitDependencies(); ForEachInTypeList<typename ToTypeList<Dependencies>::ValueT, Detail::InitDependency>::Do();
 		}
 	};
-	
+
 	/*! \endcond */
 
 }
