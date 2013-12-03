d319743c258fc8b2cd50b19f31154dac85deb110
Sergey.Alexeev <sergey.alexeev@spb.gs.ru>
2013-12-03 23:33:42 +0400

    fixed build on st platform

--- TypeInfo.cpp
+++ TypeInfo.cpp
@@ -6,31 +6,31 @@
 namespace stingray
 {
 
-	std::string TypeInfo::GetName() const
-	{
-		return Demangle(GetRawName());
-	}
-
-
-	std::string TypeInfo::GetNamespaceName() const
-	{
-		std::string name = GetName();
-		size_t delim = name.find_last_of("::");
-		return delim == std::string::npos ? "" : std::string(name, 0, delim);
-	}
-
-
-	std::string TypeInfo::GetClassName() const
-	{
-		std::string name = GetName();
-		size_t delim = name.find_last_of("::");
-		return delim == std::string::npos ? name : std::string(name, delim + 1);
-	}
-
-
-	std::string TypeInfo::ToString() const
-	{
-		return GetName();
-	}
+//	std::string TypeInfo::GetName() const
+//	{
+//		return Demangle(GetRawName());
+//	}
+//
+//
+//	std::string TypeInfo::GetNamespaceName() const
+//	{
+//		std::string name = GetName();
+//		size_t delim = name.find_last_of("::");
+//		return delim == std::string::npos ? "" : std::string(name, 0, delim);
+//	}
+//
+//
+//	std::string TypeInfo::GetClassName() const
+//	{
+//		std::string name = GetName();
+//		size_t delim = name.find_last_of("::");
+//		return delim == std::string::npos ? name : std::string(name, delim + 1);
+//	}
+//
+//
+//	std::string TypeInfo::ToString() const
+//	{
+//		return GetName();
+//	}
 
 }
