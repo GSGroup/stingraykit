0650e53d5b5a41d77b9b958e53e18c2a665e65a6
Boris Sazonov <sazonov@spb.gs.ru>
2013-12-18 21:03:28 +0400

    Formatting fix

--- MemberToValueComparer.h
+++ MemberToValueComparer.h
@@ -7,111 +7,113 @@ namespace stingray
 {
 
 
-template < typename T >
-struct MemberExtractor;
+	template < typename T >
+	struct MemberExtractor;
 
-template < typename C, typename T >
-struct MemberExtractor<T (C::*)() const>
-{
-	typedef T MemberType;
-	typedef C ClassType;
-	typedef MemberType (ClassType::*MemberPointer)() const;
-	static MemberType GetValue(const ClassType &obj, MemberPointer ptr) { return (obj.*ptr)(); }
-};
-
-template < typename C, typename T >
-struct MemberExtractor<T C::*>
-{
-	typedef T MemberType;
-	typedef C ClassType;
-	typedef MemberType ClassType::*MemberPointer;
-	static MemberType GetValue(const ClassType &obj, MemberPointer ptr) { return obj.*ptr; }
-};
-
-struct AllowDereferencing
-{
-	template <typename T> static const T& Process(const T& t)					{ return t; }
-	template <typename T> static const T& Process(const shared_ptr<T>& t)		{ return *t; }
-	template <typename T> static const T& Process(const shared_ptr<const T>& t)	{ return *t; }
-};
-
-struct NoDereferencing
-{
-	template <typename T> static const T& Process(const T& t)				{ return t; }
-};
-
-
-template <typename MemberPointer, typename Comparer = std::equal_to<typename MemberExtractor<MemberPointer>::MemberType>, typename DereferencingManager = AllowDereferencing>
-struct MemberExtractorComparer
-{
-	typedef MemberExtractor<MemberPointer>		Extractor;
-	typedef typename Extractor::MemberType		MemberType;
-	typedef typename Extractor::ClassType		ClassType;
-private:
-	MemberPointer	_ptr;
-	Comparer		_comparer;
-public:
-	MemberExtractorComparer(MemberPointer ptr, Comparer comparer = Comparer())
-		: _ptr(ptr), _comparer(comparer)
-	{}
-
-	// Used for upper_bound
-	template <typename T>
-	bool operator ()(MemberType value, const T &obj) const
+	template < typename C, typename T >
+	struct MemberExtractor<T (C::*)() const>
 	{
-		const ClassType& deref_obj(DereferencingManager::Process(obj));
-		return _comparer(value, Extractor::GetValue(deref_obj, _ptr));
+		typedef T MemberType;
+		typedef C ClassType;
+		typedef MemberType (ClassType::*MemberPointer)() const;
+		static MemberType GetValue(const ClassType &obj, MemberPointer ptr) { return (obj.*ptr)(); }
+	};
+
+	template < typename C, typename T >
+	struct MemberExtractor<T C::*>
+	{
+		typedef T MemberType;
+		typedef C ClassType;
+		typedef MemberType ClassType::*MemberPointer;
+		static MemberType GetValue(const ClassType &obj, MemberPointer ptr) { return obj.*ptr; }
+	};
+
+	struct AllowDereferencing
+	{
+		template <typename T> static const T& Process(const T& t)					{ return t; }
+		template <typename T> static const T& Process(const shared_ptr<T>& t)		{ return *t; }
+		template <typename T> static const T& Process(const shared_ptr<const T>& t)	{ return *t; }
+	};
+
+	struct NoDereferencing
+	{
+		template <typename T> static const T& Process(const T& t)				{ return t; }
+	};
+
+
+
+
+	template <typename MemberPointer, typename Comparer = std::equal_to<typename MemberExtractor<MemberPointer>::MemberType>, typename DereferencingManager = AllowDereferencing>
+	struct MemberExtractorComparer
+	{
+		typedef MemberExtractor<MemberPointer>		Extractor;
+		typedef typename Extractor::MemberType		MemberType;
+		typedef typename Extractor::ClassType		ClassType;
+	private:
+		MemberPointer	_ptr;
+		Comparer		_comparer;
+	public:
+		MemberExtractorComparer(MemberPointer ptr, Comparer comparer = Comparer())
+			: _ptr(ptr), _comparer(comparer)
+		{}
+
+		// Used for upper_bound
+		template <typename T>
+		bool operator ()(MemberType value, const T &obj) const
+		{
+			const ClassType& deref_obj(DereferencingManager::Process(obj));
+			return _comparer(value, Extractor::GetValue(deref_obj, _ptr));
+		}
+
+		// Used for lower_bound
+		template <typename T>
+		bool operator ()(const T &obj, MemberType value) const
+		{
+			const ClassType& deref_obj(DereferencingManager::Process(obj));
+			return _comparer(Extractor::GetValue(deref_obj, _ptr), value);
+		}
+	};
+
+	template <typename MemberPointer, typename Comparer = std::equal_to<typename MemberExtractor<MemberPointer>::MemberType>, typename DereferencingManager = AllowDereferencing>
+	struct MemberToValueComparer
+	{
+		typedef MemberExtractor<MemberPointer>		Extractor;
+		typedef typename Extractor::MemberType		MemberType;
+		typedef typename Extractor::ClassType		ClassType;
+	private:
+		MemberPointer	_ptr;
+		MemberType		_value;
+		Comparer		_comparer;
+	public:
+		MemberToValueComparer(MemberPointer ptr, MemberType memberValue, Comparer comparer = Comparer())
+			: _ptr(ptr), _value(memberValue), _comparer(comparer)
+		{}
+
+		template <typename T>
+		bool operator ()(const T &obj) const
+		{
+			const ClassType& deref_obj(DereferencingManager::Process(obj));
+			return _comparer(_value, Extractor::GetValue(deref_obj, _ptr));
+		}
+	};
+
+	template <template<typename> class Comparer, typename MemberPointer, typename ValueType>
+	MemberToValueComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType> > CompareMemberToValue(MemberPointer ptr, ValueType value)
+	{
+		return MemberToValueComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType> >(ptr, value);
 	}
 
-	// Used for lower_bound
-	template <typename T>
-	bool operator ()(const T &obj, MemberType value) const
+	template <template<typename> class Comparer, typename MemberPointer>
+	MemberExtractorComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType> > CompareMember(MemberPointer ptr)
 	{
-		const ClassType& deref_obj(DereferencingManager::Process(obj));
-		return _comparer(Extractor::GetValue(deref_obj, _ptr), value);
+		return MemberExtractorComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType> >(ptr, Comparer<typename MemberExtractor<MemberPointer>::MemberType>());
 	}
-};
 
-template <typename MemberPointer, typename Comparer = std::equal_to<typename MemberExtractor<MemberPointer>::MemberType>, typename DereferencingManager = AllowDereferencing>
-struct MemberToValueComparer
-{
-	typedef MemberExtractor<MemberPointer>		Extractor;
-	typedef typename Extractor::MemberType		MemberType;
-	typedef typename Extractor::ClassType		ClassType;
-private:
-	MemberPointer	_ptr;
-	MemberType		_value;
-	Comparer		_comparer;
-public:
-	MemberToValueComparer(MemberPointer ptr, MemberType memberValue, Comparer comparer = Comparer())
-		: _ptr(ptr), _value(memberValue), _comparer(comparer)
-	{}
-
-	template <typename T>
-	bool operator ()(const T &obj) const
+	template <template<typename> class Comparer, typename DereferencingManager, typename MemberPointer>
+	MemberExtractorComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType>, DereferencingManager> CompareMember(MemberPointer ptr)
 	{
-		const ClassType& deref_obj(DereferencingManager::Process(obj));
-		return _comparer(_value, Extractor::GetValue(deref_obj, _ptr));
+		return MemberExtractorComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType>, DereferencingManager >(ptr, Comparer<typename MemberExtractor<MemberPointer>::MemberType>());
 	}
-};
-
-template <template<typename> class Comparer, typename MemberPointer, typename ValueType>
-MemberToValueComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType> > CompareMemberToValue(MemberPointer ptr, ValueType value)
-{
-	return MemberToValueComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType> >(ptr, value);
-}
-
-template <template<typename> class Comparer, typename MemberPointer>
-MemberExtractorComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType> > CompareMember(MemberPointer ptr)
-{
-	return MemberExtractorComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType> >(ptr, Comparer<typename MemberExtractor<MemberPointer>::MemberType>());
-}
-
-template <template<typename> class Comparer, typename DereferencingManager, typename MemberPointer>
-MemberExtractorComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType>, DereferencingManager> CompareMember(MemberPointer ptr)
-{
-	return MemberExtractorComparer<MemberPointer, Comparer<typename MemberExtractor<MemberPointer>::MemberType>, DereferencingManager >(ptr, Comparer<typename MemberExtractor<MemberPointer>::MemberType>());
-}
 
 
 }
