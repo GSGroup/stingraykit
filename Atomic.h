df7638311b6576fe386acadbc27644b5ac9fd10b
Boris Sazonov <sazonov@spb.gs.ru>
2013-12-22 19:37:39 +0400

    Implemented STINGRAY_ANNOTATE_HAPPENS_BEFORE(...), STINGRAY_ANNOTATE_HAPPENS_AFTER(...), and STINGRAY_ANNOTATE_RELEASE(...) macro - necessary to properly annotate smartptrs for helgrind

--- Atomic.h
+++ Atomic.h
@@ -13,6 +13,9 @@
 #	include <ext/atomicity.h>
 #endif
 
+#if STINGRAY_USE_HELGRIND_ANNOTATIONS
+#	include <valgrind/helgrind.h>
+#endif
 
 namespace stingray
 {
@@ -26,6 +29,31 @@ namespace stingray
 	typedef _Atomic_word	atomic_int_type;
 #else
 	typedef int				atomic_int_type;
+#endif
+
+#if STINGRAY_USE_HELGRIND_ANNOTATIONS
+#	define STINGRAY_ANNOTATE_HAPPENS_BEFORE(Marker_) \
+	do \
+	{ \
+		ANNOTATE_HAPPENS_BEFORE(Marker_); \
+	} while (0)
+#	define STINGRAY_ANNOTATE_HAPPENS_AFTER(Marker_) \
+	do \
+	{ \
+		ANNOTATE_HAPPENS_AFTER(Marker_); \
+	} while (0)
+#	define STINGRAY_ANNOTATE_RELEASE(Marker_) \
+	do \
+	{ \
+		ANNOTATE_HAPPENS_BEFORE_FORGET_ALL(Marker_); \
+	} while (0)
+#else
+#	define STINGRAY_ANNOTATE_HAPPENS_BEFORE(Marker_) \
+	do { } while (0)
+#	define STINGRAY_ANNOTATE_HAPPENS_AFTER(Marker_) \
+	do { } while (0)
+#	define STINGRAY_ANNOTATE_RELEASE(Marker_) \
+	do { } while (0)
 #endif
 
 	struct Atomic
