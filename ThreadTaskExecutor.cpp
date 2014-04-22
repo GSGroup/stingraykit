11c22e7b21679d05d9f33912204b5351da7e4921
Boris Sazonov <sazonov@spb.gs.ru>
2014-04-22 14:32:11 +0400

    Fixed indentation in ThreadTaskExecutor.cpp

--- ThreadTaskExecutor.cpp
+++ ThreadTaskExecutor.cpp
@@ -10,85 +10,87 @@
 namespace stingray
 {
 
-		static const size_t TaskCountLimit = 1024;
+	static const size_t TaskCountLimit = 1024;
 
-		ThreadTaskExecutor::ThreadTaskExecutor(const std::string& name, const ExceptionHandlerType& exceptionHandler, bool profileCalls)
-			: _name(name), _working(true), _paused(false), _exceptionHandler(exceptionHandler), _profileCalls(profileCalls)
-		{ _worker.reset(new Thread(name, bind(&ThreadTaskExecutor::ThreadFunc, this))); }
-
-
-		ThreadTaskExecutor::~ThreadTaskExecutor()
-		{
-			//_worker->Interrupt();
-			{
-				MutexLock l(_syncRoot);
-				_working = false;
-				_condVar.Broadcast();
-			}
-			_worker->Join();
-		}
+	ThreadTaskExecutor::ThreadTaskExecutor(const std::string& name, const ExceptionHandlerType& exceptionHandler, bool profileCalls)
+		: _name(name), _working(true), _paused(false), _exceptionHandler(exceptionHandler), _profileCalls(profileCalls)
+	{ _worker.reset(new Thread(name, bind(&ThreadTaskExecutor::ThreadFunc, this))); }
 
 
-		void ThreadTaskExecutor::AddTask(const TaskType& task, const FutureExecutionTester& tester)
+	ThreadTaskExecutor::~ThreadTaskExecutor()
+	{
+		//_worker->Interrupt();
 		{
 			MutexLock l(_syncRoot);
-			_queue.push(std::make_pair(task, tester));
-			if (_queue.size() > TaskCountLimit)
-				Logger::Error() << "[ThreadTaskExecutor] Task queue size limit exceeded for executor '" << _name << "': " << _queue.size();
+			_working = false;
 			_condVar.Broadcast();
 		}
+		_worker->Join();
+	}
 
-		void ThreadTaskExecutor::AddTask(const TaskType& task)
-		{ AddTask(task, null); }
+
+	void ThreadTaskExecutor::AddTask(const TaskType& task, const FutureExecutionTester& tester)
+	{
+		MutexLock l(_syncRoot);
+		_queue.push(std::make_pair(task, tester));
+		if (_queue.size() > TaskCountLimit)
+			Logger::Error() << "[ThreadTaskExecutor] Task queue size limit exceeded for executor '" << _name << "': " << _queue.size();
+		_condVar.Broadcast();
+	}
 
 
-		void ThreadTaskExecutor::Pause(bool pause)
+	void ThreadTaskExecutor::AddTask(const TaskType& task)
+	{ AddTask(task, null); }
+
+
+	void ThreadTaskExecutor::Pause(bool pause)
+	{
+		MutexLock l(_syncRoot);
+		if (_paused == pause)
+			return;
+
+		_paused = pause;
+		if (!_paused)
+			_condVar.Broadcast();
+	}
+
+
+	std::string ThreadTaskExecutor::GetProfilerMessage(const function<void()>& func)
+	{ return StringBuilder() % get_function_name(func) % " in ThreadTaskExecutor '" % _name % "'"; }
+
+
+	void ThreadTaskExecutor::ThreadFunc()
+	{
+		MutexLock l(_syncRoot);
+		while (_working)
 		{
-			MutexLock l(_syncRoot);
-			if (_paused == pause)
-				return;
-
-			_paused = pause;
-			if (!_paused)
-				_condVar.Broadcast();
-		}
-
-
-		std::string ThreadTaskExecutor::GetProfilerMessage(const function<void()>& func)
-		{ return StringBuilder() % get_function_name(func) % " in ThreadTaskExecutor '" % _name % "'"; }
-
-		void ThreadTaskExecutor::ThreadFunc()
-		{
-			MutexLock l(_syncRoot);
-			while (_working)
+			while (!_paused && _working && !_queue.empty())
 			{
-				while (!_paused && _working && !_queue.empty())
+				TaskPair top = _queue.front();
+				_queue.pop();
+				try
 				{
-					TaskPair top = _queue.front();
-					_queue.pop();
-					try
+					MutexUnlock ul(l);
+					LocalExecutionGuard guard;
+					if (top.second.Execute(guard))
 					{
-						MutexUnlock ul(l);
-						LocalExecutionGuard guard;
-						if (top.second.Execute(guard))
+						if (_profileCalls)
 						{
-							if (_profileCalls)
-							{
-								AsyncProfiler::Session profiler_session(ExecutorsProfiler::Instance().GetProfiler(), bind(&ThreadTaskExecutor::GetProfilerMessage, this, ref(top.first)), 10000, AsyncProfiler::Session::Behaviour::Silent, AsyncProfiler::Session::NameGetterTag());
-								top.first();
-							}
-							else
-								top.first();
+							AsyncProfiler::Session profiler_session(ExecutorsProfiler::Instance().GetProfiler(), bind(&ThreadTaskExecutor::GetProfilerMessage, this, ref(top.first)), 10000, AsyncProfiler::Session::Behaviour::Silent, AsyncProfiler::Session::NameGetterTag());
+							top.first();
 						}
-						Thread::InterruptionPoint();
+						else
+							top.first();
+					Thread::InterruptionPoint();
 					}
-					catch(const std::exception& ex)
-					{ _exceptionHandler(ex); }
 				}
-				if (_working)
-					_condVar.Wait(_syncRoot);
+				catch(const std::exception& ex)
+				{ _exceptionHandler(ex); }
 			}
+			if (_working)
+				_condVar.Wait(_syncRoot);
 		}
+	}
 
 }
 
