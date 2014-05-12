e9007c575b266e9365fe4b3d833794016e8cdbbb
Boris Sazonov <sazonov@spb.gs.ru>
2014-05-12 19:48:02 +0400

    Moved abs and AlingUp/Down to math.h

---
+++ math.h
@@ -0,0 +1,23 @@
+#ifndef STINGRAY_TOOLKIT_MATH_H
+#define STINGRAY_TOOLKIT_MATH_H
+
+
+#include <stingray/toolkit/exception.h>
+
+namespace stingray
+{
+
+	inline s8 abs(s8 val)	{ return val >= 0 ? val : -val; }
+	inline s16 abs(s16 val)	{ return val >= 0 ? val : -val; }
+	inline s32 abs(s32 val)	{ return val >= 0 ? val : -val; }
+	inline s64 abs(s64 val)	{ return val >= 0 ? val : -val; }
+
+	inline size_t AlignUp(size_t value, size_t boundary)
+	{ return boundary * ((value + boundary - 1) / boundary); }
+
+	inline size_t AlignDown(size_t value, size_t boundary)
+	{ return boundary * (value / boundary); }
+
+}
+
+#endif
--- toolkit.h
+++ toolkit.h
@@ -281,17 +281,6 @@ namespace stingray
 
 	std::string Demangle(const std::string& s);
 
-	inline s8 abs(s8 val)	{ return val >= 0 ? val : -val; }
-	inline s16 abs(s16 val)	{ return val >= 0 ? val : -val; }
-	inline s32 abs(s32 val)	{ return val >= 0 ? val : -val; }
-	inline s64 abs(s64 val)	{ return val >= 0 ? val : -val; }
-
-	inline size_t AlignUp(size_t value, size_t boundary)
-	{ return boundary * ((value + boundary - 1) / boundary); }
-
-	inline size_t AlignDown(size_t value, size_t boundary)
-	{ return boundary * (value / boundary); }
-
 }
 
 
