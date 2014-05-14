84a691e16c965523e6452236188b32976be05bf1
Boris Sazonov <sazonov@spb.gs.ru>
2014-05-14 18:25:41 +0400

    Implemented equality comparison for BasicPosition

--- Position.h
+++ Position.h
@@ -17,7 +17,10 @@ namespace stingray
 		BasicPosition() : X(0), Y(0) { }
 		BasicPosition(ValueType x, ValueType y) : X(x), Y(y) { }
 
-		std::string ToString() const { return StringBuilder() % "(" % X % ", " % Y % ")"; }
+		std::string ToString() const						{ return StringBuilder() % "(" % X % ", " % Y % ")"; }
+
+		bool operator == (const BasicPosition& other) const	{ return X == other.X && Y == other.Y; }
+		bool operator != (const BasicPosition& other) const	{ return !((*this) == other); }
 	};
 	typedef BasicPosition<int> Position;
 
