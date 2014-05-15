f5972822ffa58bde5ae90535165c9bd648eca6ab
Dmitry Koplyarov <koplyarov@spb.gs.ru>
2014-05-15 16:29:23 +0400

    Changed error in PrivateIncludeGuard.h to be able to see headers trace

--- PrivateIncludeGuard.h
+++ PrivateIncludeGuard.h
@@ -2,7 +2,12 @@
 #define STINGRAY_TOOLKIT_PRIVATEINCLUDEGUARD_H
 
 #if !defined(STINGRAY_LIB_SOURCE)
-#error "This header is ought to be private!"
+static void _failer_()
+{
+	class ErrorClass;
+	static ErrorClass ERROR__This_header_is_ought_to_be_private;
+}
+//#error "This header is ought to be private!"
 #endif
 
 #endif
