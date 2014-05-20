f93262e3a0efc68b997a8b1e4846ceb584dc33a2
Vladimir Menshakov <menshakov@spb.gs.ru>
2014-05-20 22:21:09 +0400

    added missing file, sorry :(

---
+++ StringReader.h
@@ -0,0 +1,68 @@
+#ifndef STINGRAY_TOOLKIT_STRINGREADER_H
+#define STINGRAY_TOOLKIT_STRINGREADER_H
+
+
+#include <sstream>
+#include <string>
+
+
+namespace stingray
+{
+
+	class StringReader
+	{
+
+		class StreamPeeker
+		{
+		private:
+			std::istringstream&	_stream;
+
+		public:
+			explicit StreamPeeker(std::istringstream& stream)
+				: _stream(stream)
+			{ }
+
+			~StreamPeeker()
+			{ _stream.peek(); }
+		};
+
+	private:
+		std::istringstream		_stream;
+
+	public:
+		StringReader(const std::string& text)
+			: _stream(text)
+		{ }
+
+		std::string ReadLine()
+		{
+			StreamPeeker peeker(_stream);
+
+			std::string result;
+
+			for (char ch; _stream.get(ch); )
+			{
+				if (ch == '\n')
+					return result;
+
+				if (ch == '\r')
+				{
+					if (_stream.peek() == '\n')
+						_stream.get();
+
+					return result;
+				}
+
+				result.push_back(ch);
+			}
+
+			return result;
+		}
+
+		bool IsEndOfString() const { return _stream.eof(); }
+	};
+
+}
+
+
+#endif
