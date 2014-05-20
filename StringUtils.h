f0bf9be119b0f1e2375ed83f5369f6611592dcb8
Vladimir Menshakov <menshakov@spb.gs.ru>
2014-05-20 22:11:47 +0400

    moved StringReader to separate header, removed some of sstring inclusion

--- StringUtils.h
+++ StringUtils.h
@@ -3,7 +3,6 @@
 
 #include <algorithm>
 #include <ctype.h>
-#include <sstream>
 #include <string>
 
 #include <stingray/toolkit/Dummy.h>
@@ -608,60 +607,6 @@ namespace stingray
 
 	typedef BasicStringBuilder<char>	StringBuilder;
 	typedef BasicStringBuilder<wchar_t>	WideStringBuilder;
-
-
-	class StringReader
-	{
-
-		class StreamPeeker
-		{
-		private:
-			std::istringstream&	_stream;
-
-		public:
-			explicit StreamPeeker(std::istringstream& stream)
-				: _stream(stream)
-			{ }
-
-			~StreamPeeker()
-			{ _stream.peek(); }
-		};
-
-	private:
-		std::istringstream		_stream;
-
-	public:
-		StringReader(const std::string& text)
-			: _stream(text)
-		{ }
-
-		std::string ReadLine()
-		{
-			StreamPeeker peeker(_stream);
-
-			std::string result;
-
-			for (char ch; _stream.get(ch); )
-			{
-				if (ch == '\n')
-					return result;
-
-				if (ch == '\r')
-				{
-					if (_stream.peek() == '\n')
-						_stream.get();
-
-					return result;
-				}
-
-				result.push_back(ch);
-			}
-
-			return result;
-		}
-
-		bool IsEndOfString() const { return _stream.eof(); }
-	};
 
 
 }
