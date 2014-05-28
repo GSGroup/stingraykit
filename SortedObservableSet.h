46e3a0483a6020e6854507c07a14fbff3020133a
Boris Sazonov <sazonov@spb.gs.ru>
2014-05-28 20:28:47 +0400

    Reimplemented SortedObservableSet according to new IObservableSet approach

--- SortedObservableSet.h
+++ SortedObservableSet.h
@@ -13,9 +13,35 @@ namespace stingray
 	 * @{
 	 */
 
-	template < typename ValueType_ , typename CompareType_ = std::less<ValueType_> > 
+	template < typename ValueType_ , typename CompareType_ = std::less<ValueType_> >
 	class SortedObservableSet : public ObservableSetWrapper<SortedSet<ValueType_, CompareType_> >
-	{ };
+	{
+		typedef signal_policies::threading::ExternalMutex ExternalMutex;
+
+	private:
+		Mutex															_mutex;
+		signal<void(CollectionOp, const ValueType_&), ExternalMutex>	_onChanged;
+
+	public:
+		SortedObservableSet() : _onChanged(ExternalMutex(_mutex), bind(&SortedObservableSet::OnChangedPopulator, this, _1))
+		{ }
+
+		virtual signal_connector<void(CollectionOp, const ValueType_&)>	OnChanged() const
+		{ return _onChanged.connector(); }
+
+		virtual const Mutex& GetSyncRoot() const
+		{ return _mutex; }
+
+	protected:
+		virtual void InvokeOnChanged(CollectionOp op, const ValueType_& value)
+		{ _onChanged(op, value); }
+
+		void OnChangedPopulator(const function<void(CollectionOp, const ValueType_&)>& slot)
+		{
+			FOR_EACH(ValueType_ v IN this->GetEnumerator())
+				slot(CollectionOp::Added, v);
+		}
+	};
 
 	/** @} */
 
