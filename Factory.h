515d5784789138c3613a898bccaca8ff892065af
Sergey Zubov <zubov@spb.gs.ru>
2014-06-27 15:01:22 +0400

    add TOOLKIT_REGISTER_CLASS_VERSION and TOOLKIT_REGISTER_CLASS_VERSION_EXPLICIT macros

--- Factory.h
+++ Factory.h
@@ -3,6 +3,7 @@
 
 
 #include <stingray/toolkit/Singleton.h>
+#include <stingray/toolkit/Version.h>
 
 
 namespace stingray
@@ -65,6 +66,10 @@ namespace stingray
 				creator.release();
 			}
 
+			template < typename ClassType >
+			void Register(const std::string& name, const Version& version)
+			{ }
+
 		private:
 			// Defined in stingray/toolkit/_FactoryClasses.cpp
 			void RegisterTypes();
@@ -101,5 +106,13 @@ namespace stingray
 	virtual std::string GetClassName() const { return RemovePrefix(TypeInfo(typeid(Class_)).GetName(), "stingray::"); }
 
 #define TOOLKIT_REGISTER_CLASS_EXPLICIT(Class_) stingray::Detail::Factory::Instance().Register<Class_>(#Class_)
+
+
+#define TOOLKIT_REGISTER_CLASS_VERSION(Class_, ClassName_, ClassVersion_) \
+	friend class stingray::Detail::SimpleFactoryObjectCreator<Class_>; \
+	virtual std::string GetClassName() const { return ClassName_; }
+
+#define TOOLKIT_REGISTER_CLASS_VERSION_EXPLICIT(Class_, ClassName_, ClassVersion_) stingray::Detail::Factory::Instance().Register<Class_>(ClassName_, Version::FromString(#ClassVersion_))
+
 
 #endif
