e5b816bc5f2ad4c72e3be8e801ec3241eb409d65
Dmitry Koplyarov <koplyarov@spb.gs.ru>
2014-08-24 19:20:59 +0400

    TOOLKIT_NONPOLYMORPHIC macro

--- Final.h
+++ Final.h
@@ -5,10 +5,13 @@
 namespace stingray
 {
 
-	// Usage: 
+	// Usage:
 	// class A : TOOLKIT_FINAL(A) { };
 
 #define TOOLKIT_FINAL(ClassName_) private virtual stingray::Final<ClassName_>
+#define TOOLKIT_NONPOLYMORPHIC(ClassName_) TOOLKIT_FINAL(ClassName_), public virtual stingray::NonPolymorphicMarker
+
+	class NonPolymorphicMarker { };
 
 	template < typename >
 	class Final
