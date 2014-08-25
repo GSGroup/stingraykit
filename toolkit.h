7b265fc200fff0453c4fa6109083adc82e054f10
Vladimir Menshakov <menshakov@spb.gs.ru>
2014-08-25 19:48:03 +0400

    fixed another missing stingray:: namespace specification

--- toolkit.h
+++ toolkit.h
@@ -89,11 +89,11 @@ namespace stingray
 		operator Enum () const { return _enumVal; } \
 		Enum val() const { return _enumVal; } \
 		template<typename T> inline bool operator<(T other) const { \
-			stingray::CompileTimeAssert<SameType<ClassName, T>::Value> ERROR_invalid_enum_used; \
+			stingray::CompileTimeAssert<stingray::SameType<ClassName, T>::Value> ERROR_invalid_enum_used; \
 			return _enumVal < other._enumVal; \
 		} \
 		template<typename T> inline bool operator==(T other) const { \
-			stingray::CompileTimeAssert<SameType<ClassName, T>::Value> ERROR_invalid_enum_used; \
+			stingray::CompileTimeAssert<stingray::SameType<ClassName, T>::Value> ERROR_invalid_enum_used; \
 			return _enumVal == other._enumVal; \
 		} \
 		inline bool operator==(Enum value) const { return _enumVal == value; } \
