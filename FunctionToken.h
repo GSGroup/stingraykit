947bf1cb58b37ae0a90bfc63111b52d8e16cb100
Boris Sazonov <sazonov@spb.gs.ru>
2014-09-10 21:13:25 +0400

    Added FunctionToken

---
+++ FunctionToken.h
@@ -0,0 +1,27 @@
+#ifndef STINGRAY_TOOLKIT_FUNCTIONTOKEN_H
+#define STINGRAY_TOOLKIT_FUNCTIONTOKEN_H
+
+
+#include <stingray/log/Logger.h>
+#include <stingray/toolkit/IToken.h>
+#include <stingray/toolkit/function.h>
+
+namespace stingray
+{
+
+	class FunctionToken : public virtual IToken
+	{
+	private:
+		function<void()>	_cleanupFunc;
+
+	public:
+		FunctionToken(const function<void()>& cleanupFunc) : _cleanupFunc(cleanupFunc)
+		{ }
+
+		virtual ~FunctionToken()
+		{ STINGRAY_TRY_NO_MESSAGE(_cleanupFunc()); }
+	};
+
+}
+
+#endif
