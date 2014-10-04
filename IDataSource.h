ca55ce07343624ff32b9d537b9df0d25e2eb24b2
Vladimir Menshakov <menshakov@spb.gs.ru>
2014-10-04 17:17:53 +0400

    added simple bytestream data source class

--- IDataSource.h
+++ IDataSource.h
@@ -4,6 +4,7 @@
 
 #include <stingray/threads/CancellationToken.h>
 #include <stingray/toolkit/ByteData.h>
+#include <stingray/toolkit/IByteStream.h>
 #include <stingray/toolkit/bind.h>
 #include <stingray/toolkit/reference.h>
 
@@ -188,6 +189,41 @@ namespace stingray
 	};
 	TOOLKIT_DECLARE_PTR(ByteDataPacketSource);
 
+	class ByteStreamDataSource : public IDataSource
+	{
+		static const size_t	DefaultReadSize = 128 * 1024;
+
+	private:
+		IByteStreamPtr		_stream;
+		size_t				_readSize;
+		ConstByteArray		_data;
+
+	public:
+		ByteStreamDataSource(const IByteStreamPtr &stream, size_t readSize = DefaultReadSize) : _stream(stream), _readSize(readSize) { }
+
+		virtual void Read(IDataConsumer& consumer, const CancellationToken& token)
+		{
+			if (_data.empty()) //fixme: replace by something more smart, like circular buffer
+			{
+				ByteArray data(_readSize);
+				size_t s = _stream->Read(data.GetByteData());
+				if (s == 0)
+				{
+					consumer.EndOfData();
+					return;
+				}
+				_data = ConstByteArray(data, 0, s);
+			}
+			size_t processed = consumer.Process(_data.GetByteData(), token);
+			TOOLKIT_CHECK(processed <= _data.size(), "invalid return value for data consumer");
+			if (processed < _data.size())
+			{
+				_data = ConstByteArray(_data, processed, _data.size() - processed);
+			}
+			else
+				_data = ConstByteArray(); //all data have been processed
+		}
+	};
 
 }
 
