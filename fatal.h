6bd9c307e4fbdf9cc949b0eb06e3f6f01c34f4d5
Vladimir Menshakov <menshakov@spb.gs.ru>
2014-10-17 19:07:47 +0400

    added missing namespace specification

--- fatal.h
+++ fatal.h
@@ -6,7 +6,7 @@
 #define TOOLKIT_FATAL(message) \
 	do { \
 		std::string msg = TOOLKIT_WHERE.ToString() + std::string(": ") + std::string(message); \
-		DebuggingHelper::TerminateWithMessage(msg); \
+		stingray::DebuggingHelper::TerminateWithMessage(msg); \
 	} while(0)
 
 #endif
