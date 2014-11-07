49c10ae3489b382eaf974aa47254dfae2b05d71a
Sergey Zubov <zubov@spb.gs.ru>
2014-11-07 12:17:39 +0300

    implement IntegerSequenceGenerator

---
+++ IntegerSequenceGenerator.h
@@ -0,0 +1,28 @@
+#ifndef STINGRAY_TOOLKIT_INTEGERSEQUENCEGENERATOR_H
+#define STINGRAY_TOOLKIT_INTEGERSEQUENCEGENERATOR_H
+
+
+#include <stingray/threads/atomic.h>
+
+
+namespace stingray
+{
+
+	template < typename IntegerType >
+	class IntegerSequenceGenerator
+	{
+	private:
+		atomic<IntegerType>		_sequence;
+
+	public:
+		explicit IntegerSequenceGenerator(IntegerType sequenceStart = IntegerType())
+			: _sequence(sequenceStart)
+		{ }
+
+		IntegerType Next() { return _sequence++; }
+	};
+
+}
+
+
+#endif
