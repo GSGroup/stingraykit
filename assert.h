ab6943f8b5a1a7bbdbf780fff605570110048a96
Boris Sazonov <sazonov@spb.gs.ru>
2014-12-03 14:57:42 +0300

    Added TOOLKIT_DEBUG_ASSERT(...) - debug-only check

--- assert.h
+++ assert.h
@@ -6,4 +6,7 @@
 #define TOOLKIT_ASSERT(value) \
 		do { if (STINGRAY_UNLIKELY(!(value))) TOOLKIT_FATAL("assertion " #value " failed"); } while (false)
 
+#define TOOLKIT_DEBUG_ASSERT(value) \
+		do { TOOLKIT_DEBUG_ONLY(if (STINGRAY_UNLIKELY(!(value))) TOOLKIT_FATAL("assertion " #value " failed")); } while (false)
+
 #endif
