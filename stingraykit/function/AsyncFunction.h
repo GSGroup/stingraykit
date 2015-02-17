2f973f04e220667719545e93b6d0264ca8e28a2a
Boris Sazonov <sazonov@spb.gs.ru>
2015-02-17 17:24:17 +0300

    Reimplemented AsyncFunction similarly to CancellableFunction

--- stingraykit/function/AsyncFunction.h
+++ stingraykit/function/AsyncFunction.h
@@ -1,14 +1,12 @@
 #ifndef STINGRAYKIT_FUNCTION_ASYNCFUNCTION_H
 #define STINGRAYKIT_FUNCTION_ASYNCFUNCTION_H
 
-
-#include <stingraykit/thread/ITaskExecutor.h>
 #include <stingraykit/Macro.h>
+#include <stingraykit/function/CancellableFunction.h>
 #include <stingraykit/function/bind.h>
 #include <stingraykit/function/function.h>
 #include <stingraykit/future.h>
-
-
+#include <stingraykit/thread/ITaskExecutor.h>
 
 namespace stingray
 {
@@ -20,71 +18,71 @@ namespace stingray
 
 	namespace Detail
 	{
-		template < typename Signature, bool HasReturnType = !SameType<typename function_info<Signature>::RetType, void>::Value >
+		template <typename FunctorType, bool HasReturnType = !SameType<typename function_info<FunctorType>::RetType, void>::Value>
 		class AsyncFunctionBase
 		{
 		public:
 			typedef void											RetType;
-			typedef typename function_info<Signature>::ParamTypes	ParamTypes;
-			typedef function<Signature>								FunctionType;
+			typedef typename function_info<FunctorType>::ParamTypes	ParamTypes;
 
 		protected:
 			ITaskExecutorPtr	_executor;
-			FunctionType		_func;
+			FunctorType			_func;
 			TaskLifeToken		_token;
 
 		protected:
-			inline AsyncFunctionBase(const ITaskExecutorPtr& executor, const FunctionType& func)
-				: _executor(STINGRAYKIT_REQUIRE_NOT_NULL(executor)), _func(func)
+			AsyncFunctionBase(const ITaskExecutorPtr& executor, const FunctorType& func) :
+				_executor(STINGRAYKIT_REQUIRE_NOT_NULL(executor)), _func(func)
 			{ }
 
-			inline AsyncFunctionBase(const ITaskExecutorPtr& executor, const FunctionType& func, const TaskLifeToken& token)
-				: _executor(STINGRAYKIT_REQUIRE_NOT_NULL(executor)), _func(func), _token(token)
+			AsyncFunctionBase(const ITaskExecutorPtr& executor, const FunctorType& func, const TaskLifeToken& token) :
+				_executor(STINGRAYKIT_REQUIRE_NOT_NULL(executor)), _func(func), _token(token)
 			{ }
 
-			RetType DoAddTask(const function<void()>& func) const
+			~AsyncFunctionBase()
+			{ }
+
+			template <typename BoundFunctor>
+			RetType DoAddTask(const BoundFunctor& func) const
 			{ _executor->AddTask(func, _token.GetExecutionTester()); }
-
-			inline ~AsyncFunctionBase() { }
-
-		public:
-			TaskLifeToken GetToken() const { return _token; }
 		};
 
 
-		template < typename Signature >
-		class AsyncFunctionBase<Signature, true>
+		template <typename FunctorType>
+		class AsyncFunctionBase<FunctorType, true>
 		{
 		public:
-			typedef typename function_info<Signature>::RetType		AsyncRetType;
-			typedef promise<AsyncRetType>							PromiseType;
+			typedef typename function_info<FunctorType>::RetType		AsyncRetType;
+			typedef promise<AsyncRetType>								PromiseType;
 			STINGRAYKIT_DECLARE_PTR(PromiseType);
 
-			typedef future<AsyncRetType>							RetType;
-			typedef typename function_info<Signature>::ParamTypes	ParamTypes;
-			typedef function<Signature>								FunctionType;
+			typedef future<AsyncRetType>								RetType;
+			typedef typename function_info<FunctorType>::ParamTypes		ParamTypes;
 
 		protected:
 			ITaskExecutorPtr	_executor;
-			FunctionType		_func;
+			FunctorType			_func;
 			TaskLifeToken		_token;
 
 		protected:
-			inline AsyncFunctionBase(const ITaskExecutorPtr& executor, const FunctionType& func)
-				: _executor(STINGRAYKIT_REQUIRE_NOT_NULL(executor)), _func(func)
+			AsyncFunctionBase(const ITaskExecutorPtr& executor, const FunctorType& func) :
+				_executor(STINGRAYKIT_REQUIRE_NOT_NULL(executor)), _func(func)
 			{ }
 
-			RetType DoAddTask(const function<AsyncRetType()>& func) const
+			~AsyncFunctionBase()
+			{ }
+
+			template <typename BoundFunctor>
+			RetType DoAddTask(const BoundFunctor& func) const
 			{
 				PromiseTypePtr promise(new PromiseType);
-				_executor->AddTask(bind(&AsyncFunctionBase::FuncWrapper, func, promise), _token.GetExecutionTester());
+				_executor->AddTask(bind(&AsyncFunctionBase::FuncWrapper<BoundFunctor>, func, promise), _token.GetExecutionTester());
 				return promise->get_future();
 			}
 
-			inline ~AsyncFunctionBase() { }
-
 		private:
-			static void FuncWrapper(const function<AsyncRetType()>& func, const PromiseTypePtr& promise)
+			template <typename BoundFunctor>
+			static void FuncWrapper(const BoundFunctor& func, const PromiseTypePtr& promise)
 			{
 				STINGRAYKIT_REQUIRE_NOT_NULL(promise);
 				try
@@ -92,88 +90,46 @@ namespace stingray
 				catch (const std::exception& ex)
 				{ promise->set_exception(make_exception_ptr(ex)); }
 			}
-
-		public:
-			TaskLifeToken GetToken() const { return _token; }
 		};
 	}
 
-	template < typename Signature >
-	class AsyncFunction;
 
-
-	template < typename R >
-	class AsyncFunction < R() > : public Detail::AsyncFunctionBase<R()>
+	template <typename FunctorType>
+	class AsyncFunction : public Detail::AsyncFunctionBase<FunctorType>
 	{
-		typedef Detail::AsyncFunctionBase<R()>	base;
+		typedef Detail::AsyncFunctionBase<FunctorType>	base;
 
 	public:
-		inline AsyncFunction(const ITaskExecutorPtr& executor, const function<R()>& func) :
+		AsyncFunction(const ITaskExecutorPtr& executor, const FunctorType& func) :
 			base(executor, func)
 		{ }
 
-		inline AsyncFunction(const ITaskExecutorPtr& executor, const function<R()>& func, const TaskLifeToken& token) :
+		AsyncFunction(const ITaskExecutorPtr& executor, const FunctorType& func, const TaskLifeToken& token) :
 			base(executor, func, token)
 		{ }
 
-		inline typename base::RetType  operator ()() const { return base::DoAddTask(base::_func); }
+		TaskLifeToken GetToken() const { return this->_token; }
+
+		STINGRAYKIT_CONST_FORWARDING(typename base::RetType, operator (), Do)
 
 		std::string get_name() const
-		{ return "{ AsyncFunction: " + get_function_name(base::_func) + " }"; }
+		{ return "{ AsyncFunction: " + get_function_name(this->_func) + " }"; }
+
+	private:
+		template< typename ParamTypeList >
+		typename base::RetType Do(const Tuple<ParamTypeList>& params) const
+		{ return base::DoAddTask(Detail::Binder<typename TypeListTransform<ParamTypeList, Dereference>::ValueT, FunctorType>(this->_func, params)); }
 	};
 
 
-#define TY typename
-#define P_(N) typename GetConstReferenceType<T##N>::ValueT p##N
-
-#define DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(Typenames_, Types_, Decl_, Usage_) \
-	template < typename R, Typenames_ > \
-	class AsyncFunction < R(Types_) > : public Detail::AsyncFunctionBase<R(Types_)> \
-	{ \
-		typedef Detail::AsyncFunctionBase<R(Types_)>	base; \
-		\
-	public: \
-		AsyncFunction(const ITaskExecutorPtr& executor, const function<R(Types_)>& func) \
-			: base(executor, func) \
-		{ } \
-		\
-		AsyncFunction(const ITaskExecutorPtr& executor, const function<R(Types_)>& func, const TaskLifeToken& token) \
-			: base(executor, func, token) \
-		{ } \
-		\
-		typename base::RetType operator ()(Decl_) const \
-		{ \
-			return base::DoAddTask(bind(this->_func, Usage_)); \
-		} \
-		\
-		std::string get_name() const \
-		{ return "{ AsyncFunction: " + get_function_name(base::_func) + " }"; } \
-	}
-
-
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1), MK_PARAM(T1), MK_PARAM(P_(1)), MK_PARAM(p1));
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1, TY T2), MK_PARAM(T1, T2), MK_PARAM(P_(1), P_(2)), MK_PARAM(p1, p2));
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1, TY T2, TY T3), MK_PARAM(T1, T2, T3), MK_PARAM(P_(1), P_(2), P_(3)), MK_PARAM(p1, p2, p3));
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1, TY T2, TY T3, TY T4), MK_PARAM(T1, T2, T3, T4), MK_PARAM(P_(1), P_(2), P_(3), P_(4)), MK_PARAM(p1, p2, p3, p4));
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1, TY T2, TY T3, TY T4, TY T5), MK_PARAM(T1, T2, T3, T4, T5), MK_PARAM(P_(1), P_(2), P_(3), P_(4), P_(5)), MK_PARAM(p1, p2, p3, p4, p5));
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1, TY T2, TY T3, TY T4, TY T5, TY T6), MK_PARAM(T1, T2, T3, T4, T5, T6), MK_PARAM(P_(1), P_(2), P_(3), P_(4), P_(5), P_(6)), MK_PARAM(p1, p2, p3, p4, p5, p6));
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1, TY T2, TY T3, TY T4, TY T5, TY T6, TY T7), MK_PARAM(T1, T2, T3, T4, T5, T6, T7), MK_PARAM(P_(1), P_(2), P_(3), P_(4), P_(5), P_(6), P_(7)), MK_PARAM(p1, p2, p3, p4, p5, p6, p7));
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1, TY T2, TY T3, TY T4, TY T5, TY T6, TY T7, TY T8), MK_PARAM(T1, T2, T3, T4, T5, T6, T7, T8), MK_PARAM(P_(1), P_(2), P_(3), P_(4), P_(5), P_(6), P_(7), P_(8)), MK_PARAM(p1, p2, p3, p4, p5, p6, p7, p8));
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1, TY T2, TY T3, TY T4, TY T5, TY T6, TY T7, TY T8, TY T9), MK_PARAM(T1, T2, T3, T4, T5, T6, T7, T8, T9), MK_PARAM(P_(1), P_(2), P_(3), P_(4), P_(5), P_(6), P_(7), P_(8), P_(9)), MK_PARAM(p1, p2, p3, p4, p5, p6, p7, p8, p9));
-	DETAIL_STINGRAYKIT_DECLARE_ASYNC_FUNCTION(MK_PARAM(TY T1, TY T2, TY T3, TY T4, TY T5, TY T6, TY T7, TY T8, TY T9, TY T10), MK_PARAM(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10), MK_PARAM(P_(1), P_(2), P_(3), P_(4), P_(5), P_(6), P_(7), P_(8), P_(9), P_(10)), MK_PARAM(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10));
-
-#undef P_
-#undef TY
+	template <typename FunctorType>
+	AsyncFunction<FunctorType> MakeAsyncFunction(const ITaskExecutorPtr& executor, const FunctorType& func)
+	{ return AsyncFunction<FunctorType>(executor, func); }
 
 
 	template <typename FunctorType>
-	AsyncFunction<typename function_info<FunctorType>::Signature> MakeAsyncFunction(const ITaskExecutorPtr& executor, const FunctorType& func)
-	{ return AsyncFunction<typename function_info<FunctorType>::Signature>(executor, func); }
-
-
-	template <typename FunctorType>
-	AsyncFunction<typename function_info<FunctorType>::Signature> MakeAsyncFunction(const ITaskExecutorPtr& executor, const FunctorType& func, const TaskLifeToken& token)
-	{ return AsyncFunction<typename function_info<FunctorType>::Signature>(executor, func, token); }
+	AsyncFunction<FunctorType> MakeAsyncFunction(const ITaskExecutorPtr& executor, const FunctorType& func, const TaskLifeToken& token)
+	{ return AsyncFunction<FunctorType>(executor, func, token); }
 
 	/** @} */
 
