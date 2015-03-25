85622b5e82ee1c2dc085de856bb35e03b4361604
Vladimir Menshakov <menshakov@spb.gs.ru>
2015-03-25 14:39:07 +0300

    sorted headers, added missing <algorithm>, added Distance() method

--- stingraykit/collection/Range.h
+++ stingraykit/collection/Range.h
@@ -1,8 +1,9 @@
 #ifndef STINGRAYKIT_COLLECTION_RANGE_H
 #define STINGRAYKIT_COLLECTION_RANGE_H
 
-#include <utility>
 #include <stingraykit/function/function.h>
+#include <utility>
+#include <algorithm>
 
 namespace stingray
 {
@@ -17,17 +18,20 @@ namespace stingray
 
 		Range(const ValueType & begin, const ValueType & end): Begin(begin), End(end) { }
 
-		template<typename OutputIterator>
-		OutputIterator CopyTo(const OutputIterator &out) const
+		template<typename OutputIteratorType>
+		OutputIteratorType CopyTo(const OutputIteratorType &out) const
 		{ return std::copy(Begin, End, out); }
 
-		template<typename OutputIterator, typename TransformFunc>
-		OutputIterator CopyTo(const TransformFunc & transform, OutputIterator out) const
+		template<typename OutputIteratorType, typename TransformFunc>
+		OutputIteratorType CopyTo(const TransformFunc & transform, OutputIteratorType out) const
 		{
 			for(ValueType i = Begin; i != End; ++i)
 				*out++ = transform(*i);
 			return out;
 		}
+
+		size_t Distance() const
+		{ return std::distance(Begin, End); }
 	};
 
 	template<typename RangeValueType>
