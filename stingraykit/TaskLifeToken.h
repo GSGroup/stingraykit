df81929babb628897bdc22d58a00d28cc42f0779
Sergey Zubov <zubov@spb.gs.ru>
2015-03-27 14:53:00 +0300

    fix header guard

--- stingraykit/TaskLifeToken.h
+++ stingraykit/TaskLifeToken.h
@@ -1,5 +1,5 @@
-#ifndef STINGRAYKIT_TASKALIVETOKEN_H
-#define STINGRAYKIT_TASKALIVETOKEN_H
+#ifndef STINGRAYKIT_TASKLIFETOKEN_H
+#define STINGRAYKIT_TASKLIFETOKEN_H
 
 // Copyright (c) 2011 - 2015, GS Group, https://github.com/GSGroup
 // Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
