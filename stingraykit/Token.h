5477a2f4643330edc629f698c8bd63d1faaaf28c
Sergey Zubov <cybosser@gmail.com>
2015-04-10 12:16:45 +0300

    fix header guard

--- stingraykit/Token.h
+++ stingraykit/Token.h
@@ -1,5 +1,5 @@
-#ifndef STINGRAYKIT_ITOKEN_H
-#define STINGRAYKIT_ITOKEN_H
+#ifndef STINGRAYKIT_TOKEN_H
+#define STINGRAYKIT_TOKEN_H
 
 // Copyright (c) 2011 - 2015, GS Group, https://github.com/GSGroup
 // Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
