622bf31e638f9cefcebe9b6515dc8c82cd204995
Sergey Zubov <zubov@spb.gs.ru>
2015-07-15 17:39:20 +0300

    revise CaseInsensitiveLess, implement CaseInsensitiveEquals

--- stingraykit/string/Comparers.h
+++ stingraykit/string/Comparers.h
@@ -1,24 +1,49 @@
 #ifndef STINGRAYKIT_STRING_COMPARERS_H
 #define STINGRAYKIT_STRING_COMPARERS_H
 
-#include <algorithm>
-#include <cctype>
+#include <stingraykit/compare/comparers.h>
 
 namespace stingray
 {
+
+	namespace Detail
+	{
+
+		template < class Comparer >
+		class CaseInsensitiveCompare
+		{
+		private:
+			Comparer	_comparer;
+
+		public:
+			explicit CaseInsensitiveCompare(Comparer comparer = Comparer())
+				: _comparer(comparer)
+			{ }
+
+			bool operator()(char first, char second) const
+			{
+				return _comparer(std::tolower(first), std::tolower(second));
+			}
+		};
+
+	}
 
 	class CaseInsensitiveLess
 	{
 	public:
 		bool operator()(const std::string& first, const std::string& second) const
 		{
-			return std::lexicographical_compare(first.begin(), first.end(), second.begin(), second.end(), Compare);
+			return std::lexicographical_compare(first.begin(), first.end(), second.begin(), second.end(), Detail::CaseInsensitiveCompare<comparers::Less>());
 		}
+	};
 
-	private:
-		static bool Compare(char first, char second)
+
+	class CaseInsensitiveEquals
+	{
+	public:
+		bool operator()(const std::string& first, const std::string& second) const
 		{
-			return std::tolower(first) < std::tolower(second);
+			return first.length() == second.length() && std::equal(first.begin(), first.end(), second.begin(), Detail::CaseInsensitiveCompare<comparers::Equals>());
 		}
 	};
 
