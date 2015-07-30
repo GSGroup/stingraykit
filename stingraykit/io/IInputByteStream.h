9ee38e8371219d80488d39c9b083efcdc338b3b7
Igor Zavorotkin <zavorotkin@spb.gs.ru>
2015-07-30 18:36:28 +0300

    introduced IInputByteStream

---
+++ stingraykit/io/IInputByteStream.h
@@ -0,0 +1,20 @@
+#ifndef STINGRAY_STINGRAYKIT_STINGRAYKIT_IO_IINPUTBYTESTREAM_H
+#define STINGRAY_STINGRAYKIT_STINGRAYKIT_IO_IINPUTBYTESTREAM_H
+
+#include <stingraykit/collection/ByteData.h>
+#include <stingraykit/thread/DummyCancellationToken.h>
+
+namespace stingray
+{
+
+	struct IInputByteStream
+	{
+		virtual ~IInputByteStream() { }
+
+		virtual u64 Read(ByteData data, const ICancellationToken& token = DummyCancellationToken()) = 0;
+	};
+	STINGRAYKIT_DECLARE_PTR(IInputByteStream);
+
+}
+
+#endif
