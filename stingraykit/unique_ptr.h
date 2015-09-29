ebd57f8b163271a57eb4efed02f8f63e32e4b8ca
Boris Sazonov <sazonov@spb.gs.ru>
2015-09-29 23:15:21 +0300

    Added unique_ptr<T[]>, similar to standard one

--- stingraykit/unique_ptr.h
+++ stingraykit/unique_ptr.h
@@ -21,6 +21,8 @@ namespace stingray
 	class unique_ptr : public safe_bool<unique_ptr<T> >
 	{
 		STINGRAYKIT_NONCOPYABLE(unique_ptr);
+
+	private:
 		T* _rawPtr;
 
 	public:
@@ -45,6 +47,44 @@ namespace stingray
 		inline void reset(T* ptr = 0)
 		{
 			CheckedDelete(_rawPtr);
+			_rawPtr = ptr;
+		}
+
+	private:
+		inline void check_ptr() const
+		{ STINGRAYKIT_CHECK(_rawPtr, NullPointerException()); }
+	};
+
+
+	template<typename T>
+	class unique_ptr<T[]> : public safe_bool<unique_ptr<T> >
+	{
+		STINGRAYKIT_NONCOPYABLE(unique_ptr);
+
+	private:
+		T* _rawPtr;
+
+	public:
+		explicit inline unique_ptr(T* rawPtr = 0) :
+			_rawPtr(rawPtr)
+		{}
+
+		~unique_ptr()
+		{ CheckedArrayDelete(_rawPtr); }
+
+		inline bool operator == (T* ptr) const						{ return _rawPtr == ptr; }
+		inline bool operator != (T* ptr) const						{ return !(*this == ptr); }
+		inline bool operator == (const unique_ptr<T>& other) const	{ return other == _rawPtr; }
+		inline bool operator != (const unique_ptr<T>& other) const	{ return !(*this == other); }
+		inline bool boolean_test() const							{ return _rawPtr != 0; }
+		inline T* get() const										{ return _rawPtr; }
+		inline T& operator [] (size_t i) const						{ check_ptr(); return _rawPtr[i]; }
+		inline T* release()											{ T* ptr = _rawPtr; _rawPtr = 0; return ptr; }
+		inline void swap(unique_ptr<T>& other)						{ std::swap(_rawPtr, other._rawPtr); }
+
+		inline void reset(T* ptr = 0)
+		{
+			CheckedArrayDelete(_rawPtr);
 			_rawPtr = ptr;
 		}
 
