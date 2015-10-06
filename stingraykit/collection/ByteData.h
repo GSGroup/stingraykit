40d5c26df382721bacefaee323a72e48f3e7b6c7
Boris Sazonov <sazonov@spb.gs.ru>
2015-10-06 14:35:22 +0300

    Added BasicByteData ctor from stingray::array<T, N>

--- stingraykit/collection/ByteData.h
+++ stingraykit/collection/ByteData.h
@@ -8,14 +8,15 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/collection/iterator_base.h>
-#include <stingraykit/shared_ptr.h>
 #include <stingraykit/Dummy.h>
 #include <stingraykit/MetaProgramming.h>
 #include <stingraykit/TypeInfo.h>
+#include <stingraykit/collection/array.h>
+#include <stingraykit/collection/iterator_base.h>
+#include <stingraykit/shared_ptr.h>
 
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 #define DETAIL_BYTEDATA_INDEX_CHECK(Arg1, Arg2) STINGRAYKIT_CHECK((Arg1) <= (Arg2), IndexOutOfRangeException(Arg1, Arg2))
 
@@ -329,6 +330,16 @@ namespace stingray
 
 		template < typename U >
 		BasicByteData(const std::vector<U>& data) :
+			_data(data.empty() ? NULL : &data[0]), _size(data.size())
+		{ }
+
+		template < typename U, size_t N >
+		BasicByteData(array<U, N>& data) :
+			_data(data.empty() ? NULL : &data[0]), _size(data.size())
+		{ }
+
+		template < typename U, size_t N >
+		BasicByteData(const array<U, N>& data) :
 			_data(data.empty() ? NULL : &data[0]), _size(data.size())
 		{ }
 
