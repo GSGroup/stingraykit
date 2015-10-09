18223430c8d9f7fc78275875880d2fd6f73f05d1
Boris Sazonov <sazonov@spb.gs.ru>
2015-10-09 13:22:08 +0300

    Revised optional::assign(...). Apart from self-assignment check, it wasn't providing strong exception guarantee. New version provides the same level of exception safety as an assignment operator of a wrapped class.

--- stingraykit/optional.h
+++ stingraykit/optional.h
@@ -91,11 +91,13 @@ namespace stingray
 
 		void assign(ConstParamType value)
 		{
-			if (_initialized && &_value.Ref() == &value)
-				return;
-			reset();
-			_value.Ctor(value);
-			_initialized = true;
+			if (_initialized)
+				_value.Ref() = value;
+			else
+			{
+				_value.Ctor(value);
+				_initialized = true;
+			}
 		}
 
 		void assign(const optional& other)
