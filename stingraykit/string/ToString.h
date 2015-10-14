0cc488bccb5e25c347e9825d5475a5a326d53058
Igor Zavorotkin <zavorotkin@spb.gs.ru>
2015-10-14 16:35:53 +0300

    set IsStringRepresentable to false if nested value_type isn't string representable for begin/end containers

--- stingraykit/string/ToString.h
+++ stingraykit/string/ToString.h
@@ -331,6 +331,11 @@ namespace stingray
 
 
 	template < typename T >
+	struct IsStringRepresentable<T, Detail::TypeToStringObjectType::HasBeginEnd>
+	{ static const bool Value = IsStringRepresentable<typename T::value_type>::Value; };
+
+
+	template < typename T >
 	struct IsStringRepresentable<shared_ptr<T>, Detail::TypeToStringObjectType::Other>
 	{ static const bool Value = IsStringRepresentable<T>::Value; };
 
