d75ecb3185ed550f36dcee974a1e591abe7b18dc
Sergey Zubov <zubov@spb.gs.ru>
2015-11-30 20:14:35 +0300

    Revert "remove DataSourceReader"
    This reverts commit 660b14fff354de0c7de60a9aae45beb4e5109d4e.

---
+++ stingraykit/io/DataSourceReader.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2011 - 2015, GS Group, https://github.com/GSGroup
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+// provided that the above copyright notice and this permission notice appear in all copies.
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+// WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#include <stingraykit/io/DataSourceReader.h>
+
+
+namespace stingray
+{
+
+	DataSourceReader::DataSourceReader(const IDataSourcePtr& source)
+		: _source(source)
+	{ }
+
+
+	ByteArray DataSourceReader::ReadToEnd(const ICancellationToken& token)
+	{
+		class DataStorage : public virtual IDataConsumer
+		{
+		private:
+			ByteArray	_data;
+
+		public:
+			virtual size_t Process(ConstByteData data, const ICancellationToken&)
+			{
+				_data.append(data.begin(), data.end());
+				return data.size();
+			}
+
+			virtual void EndOfData(const ICancellationToken&) { }
+
+			ByteArray GetData() const { return _data; }
+		};
+
+		DataStorage storage;
+		ReactiveDataSource(_source).Read(storage, token);
+		return storage.GetData();
+	}
+
+}
