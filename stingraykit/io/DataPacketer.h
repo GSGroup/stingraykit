7dc640cbad15afb23236f8f49920e28a05ec59af
Vladimir Golubev <vladimir.golubev@iksatprof.ru>
2016-10-04 16:26:56 +0300

    Add offset customization to DataDepacketer and raw reference ctor

--- stingraykit/io/DataPacketer.h
+++ stingraykit/io/DataPacketer.h
@@ -17,16 +17,25 @@ namespace stingray
 	class DataPacketer : public virtual IPacketSource<EmptyType>
 	{
 	private:
-		IDataSourcePtr			_source;
+		IDataSourcePtr			_lifeAssurance;
+		IDataSource&				_source;
 		size_t					_packetSize;
 
 	public:
-		DataPacketer(const IDataSourcePtr& source, size_t packetSize) : _source(source), _packetSize(packetSize)
+		DataPacketer(const IDataSourcePtr& source, size_t packetSize)
+			:	_lifeAssurance(source),
+				_source(*source),
+				_packetSize(packetSize)
 		{ }
 
+		DataPacketer(IDataSource& source, size_t packetSize)
+			:	_lifeAssurance(null),
+				_source(source),
+				_packetSize(packetSize)
+		{ }
 
 		virtual void Read(IPacketConsumer<EmptyType>& consumer, const ICancellationToken& token)
-		{ _source->ReadToFunction(bind(&DataPacketer::Do, this, ref(consumer), _1, _2), token); }
+		{ _source.ReadToFunction(bind(&DataPacketer::Do, this, ref(consumer), _1, _2), token); }
 
 	private:
 		size_t Do(IPacketConsumer<EmptyType>& consumer, ConstByteData data, const ICancellationToken& token)
@@ -43,20 +52,30 @@ namespace stingray
 		STINGRAYKIT_DECLARE_PTR(PacketSource);
 
 	private:
-		PacketSourcePtr	_source;
+		PacketSourcePtr		_lifeAssurance;
+		PacketSource&		_source;
+		const size_t 		_startOffset;
 
 	public:
-		DataDepacketer(const PacketSourcePtr& source) : _source(source)
+		DataDepacketer(const PacketSourcePtr& source, size_t startOffset = 0)
+			:	_lifeAssurance(source),
+				_source(*source),
+				_startOffset(startOffset)
 		{ }
 
+		DataDepacketer(PacketSource& source, size_t startOffset = 0)
+			:	_lifeAssurance(null),
+				_source(source),
+				_startOffset(startOffset)
+		{ }
 
 		virtual void Read(IDataConsumer& consumer, const ICancellationToken& token)
-		{ _source->ReadToFunction(bind(&DataDepacketer::Do, this, ref(consumer), _1, _2), token); }
+		{ _source.ReadToFunction(bind(&DataDepacketer::Do, this, ref(consumer), _1, _2), token); }
 
 	private:
 		bool Do(IDataConsumer& consumer, const Packet<EmptyType>& packet, const ICancellationToken& token)
 		{
-			size_t offset = 0;
+			size_t offset = _startOffset;
 			while (token && offset < packet.GetSize())
 				offset += consumer.Process(ConstByteData(packet.GetData(), offset), token);
 			return true;
