a510a9c115c1033d10e3508ec62701f43c80886f
Vladimir Golubev <vladimir.golubev@iksatprof.ru>
2016-10-13 18:42:52 +0300

    Refactor DataPacketer and DataDepacketer.
    Add *Ptr declarations.
    Make Read's call ReadToFunction with correct EOD func. This is necessary, because in case _source calls
    EOD on consumer, his EndOfData will be called instead of DefaultEndOfData, which throws NotImplemented.
    Make DataPacketer use the provided data size, in case it is not set. This is necessary, because
    the _source can provide variable-length data (for example FileDataSource may give us data,
    sized <= internal buffer size).
    Add start offset and SetStartOffset(...) to Depacketer.

--- stingraykit/io/DataPacketer.h
+++ stingraykit/io/DataPacketer.h
@@ -17,33 +17,36 @@ namespace stingray
 	class DataPacketer : public virtual IPacketSource<EmptyType>
 	{
 	private:
-		IDataSourcePtr			_lifeAssurance;
-		IDataSource&				_source;
-		size_t					_packetSize;
+		IDataSourcePtr			_source;
+		optional<size_t>		_packetSize;
 
 	public:
 		DataPacketer(const IDataSourcePtr& source, size_t packetSize)
-			:	_lifeAssurance(source),
-				_source(*source),
+			:	_source(source),
 				_packetSize(packetSize)
 		{ }
 
-		DataPacketer(IDataSource& source, size_t packetSize)
-			:	_lifeAssurance(null),
-				_source(source),
-				_packetSize(packetSize)
+		DataPacketer(const IDataSourcePtr& source)
+			:	_source(source)
 		{ }
 
 		virtual void Read(IPacketConsumer<EmptyType>& consumer, const ICancellationToken& token)
-		{ _source.ReadToFunction(bind(&DataPacketer::Do, this, ref(consumer), _1, _2), token); }
+		{
+			_source->ReadToFunction(bind(&DataPacketer::Do, this, ref(consumer), _1, _2),
+									bind(&IPacketConsumer<EmptyType>::EndOfData, &consumer, not_using(_1)),
+									token);
+		}
 
 	private:
 		size_t Do(IPacketConsumer<EmptyType>& consumer, ConstByteData data, const ICancellationToken& token)
 		{
-			ConstByteData packet(data, 0, _packetSize);
-			return consumer.Process(Packet<EmptyType>(packet), token) ? _packetSize : 0;
+			const size_t packetSize = _packetSize ? *_packetSize : data.size();
+			ConstByteData packet(data, 0, packetSize);
+
+			return consumer.Process(Packet<EmptyType>(packet), token) ? packetSize : 0;
 		}
 	};
+	STINGRAYKIT_DECLARE_PTR(DataPacketer);
 
 
 	class DataDepacketer : public virtual IDataSource
@@ -52,25 +55,24 @@ namespace stingray
 		STINGRAYKIT_DECLARE_PTR(PacketSource);
 
 	private:
-		PacketSourcePtr		_lifeAssurance;
-		PacketSource&		_source;
-		const size_t 		_startOffset;
+		PacketSourcePtr		_source;
+		size_t				_startOffset;
 
 	public:
 		DataDepacketer(const PacketSourcePtr& source, size_t startOffset = 0)
-			:	_lifeAssurance(source),
-				_source(*source),
+			:	_source(source),
 				_startOffset(startOffset)
 		{ }
 
-		DataDepacketer(PacketSource& source, size_t startOffset = 0)
-			:	_lifeAssurance(null),
-				_source(source),
-				_startOffset(startOffset)
-		{ }
+		void SetStartOffset(size_t offset)
+		{ _startOffset = offset; }
 
 		virtual void Read(IDataConsumer& consumer, const ICancellationToken& token)
-		{ _source.ReadToFunction(bind(&DataDepacketer::Do, this, ref(consumer), _1, _2), token); }
+		{
+			_source->ReadToFunction(bind(&DataDepacketer::Do, this, ref(consumer), _1, _2),
+									bind(&IDataConsumer::EndOfData, &consumer, ref(token)),
+									token);
+		}
 
 	private:
 		bool Do(IDataConsumer& consumer, const Packet<EmptyType>& packet, const ICancellationToken& token)
@@ -78,9 +80,11 @@ namespace stingray
 			size_t offset = _startOffset;
 			while (token && offset < packet.GetSize())
 				offset += consumer.Process(ConstByteData(packet.GetData(), offset), token);
+
 			return true;
 		}
 	};
+	STINGRAYKIT_DECLARE_PTR(DataDepacketer);
 
 }
 
