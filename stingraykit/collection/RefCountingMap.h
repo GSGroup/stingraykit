52e929a88eb9bccb6feeaff7681f7ae330d03f01
Zavadovskiy Yan <yan.zavadovskiy@iksatprof.ru>
2017-04-11 16:38:40 +0300

    RefCountingMap: drop useless include, add really needed

--- stingraykit/collection/RefCountingMap.h
+++ stingraykit/collection/RefCountingMap.h
@@ -8,9 +8,11 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+#include <stingraykit/compare/comparers.h>
 #include <stingraykit/collection/iterator_base.h>
 #include <stingraykit/optional.h>
-#include <stingraykit/thread/Thread.h>
+
+#include <map>
 
 namespace stingray
 {
