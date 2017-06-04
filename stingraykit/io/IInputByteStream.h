23197821f926095ecd15dc5e9941da663ac01398
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2017-06-04 16:10:34 +0300

    IInputByteStream: add mirror for 'write-all' helpers

--- stingraykit/io/IInputByteStream.h
+++ stingraykit/io/IInputByteStream.h
@@ -9,6 +9,7 @@
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 #include <stingraykit/collection/ByteData.h>
+#include <stingraykit/string/ToString.h>
 #include <stingraykit/thread/DummyCancellationToken.h>
 
 namespace stingray
@@ -21,6 +22,28 @@ namespace stingray
 		virtual u64 Read(ByteData data, const ICancellationToken& token = DummyCancellationToken()) = 0;
 	};
 	STINGRAYKIT_DECLARE_PTR(IInputByteStream);
+
+
+	template < typename StreamType >
+	inline size_t ReadAll(StreamType& stream, ByteData data, const ICancellationToken& token = DummyCancellationToken())
+	{
+		size_t total = 0;
+		size_t readed = 0;
+		do {
+			readed = (size_t)stream.Read(ByteData(data, total), token);
+			total += readed;
+		} while (token && (total < data.size()) && readed != 0);
+
+		return total;
+	}
+
+
+	template < typename StreamType >
+	inline void CheckedReadAll(StreamType& stream, ByteData data, const ICancellationToken& token = DummyCancellationToken())
+	{
+		const size_t readed = ReadAll(stream, data, token);
+		STINGRAYKIT_CHECK(readed == data.size(), InputOutputException(StringBuilder() % "Readed only " % readed % " of " % data.size()));
+	}
 
 }
 
