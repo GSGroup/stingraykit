c98c4738e1ff188fd9e5a917abc366f8281160b2
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2017-08-08 05:02:13 +0300

    SortedMultiSet: remove unused header, update formatting

--- stingraykit/collection/SortedMultiSet.h
+++ stingraykit/collection/SortedMultiSet.h
@@ -13,7 +13,6 @@
 #include <stingraykit/collection/IEnumerable.h>
 #include <stingraykit/collection/IMultiSet.h>
 
-#include <algorithm>
 #include <set>
 
 namespace stingray
@@ -56,29 +55,33 @@ namespace stingray
 		mutable HolderWeakPtr	_itemsEnumeratorHolder;
 
 	public:
-		SortedMultiSet() : _items(make_shared<SetType>())
+		SortedMultiSet()
+			:	_items(make_shared<SetType>())
 		{ }
 
-		SortedMultiSet(const SortedMultiSet& other) : _items(make_shared<SetType>(*other._items))
+		SortedMultiSet(const SortedMultiSet& other)
+			:	_items(make_shared<SetType>(*other._items))
 		{ }
+
+		SortedMultiSet(shared_ptr<IEnumerator<T> > enumerator)
+			:	_items(make_shared<SetType>())
+		{
+			STINGRAYKIT_REQUIRE_NOT_NULL(enumerator);
+			Enumerable::ForEach(enumerator, bind(&SortedMultiSet::Add, this, _1));
+		}
+
+		SortedMultiSet(shared_ptr<IEnumerable<T> > enumerable)
+			:	_items(make_shared<SetType>())
+		{
+			STINGRAYKIT_REQUIRE_NOT_NULL(enumerable);
+			Enumerable::ForEach(enumerable, bind(&SortedMultiSet::Add, this, _1));
+		}
 
 		SortedMultiSet& operator = (const SortedMultiSet& other)
 		{
 			_items = make_shared<SetType>(*other._items);
 			_itemsEnumeratorHolder.reset();
 			return *this;
-		}
-
-		SortedMultiSet(shared_ptr<IEnumerator<T> > enumerator) : _items(make_shared<SetType>())
-		{
-			STINGRAYKIT_REQUIRE_NOT_NULL(enumerator);
-			Enumerable::ForEach(enumerator, bind(&SortedMultiSet::Add, this, _1));
-		}
-
-		SortedMultiSet(shared_ptr<IEnumerable<T> > enumerable) : _items(make_shared<SetType>())
-		{
-			STINGRAYKIT_REQUIRE_NOT_NULL(enumerable);
-			Enumerable::ForEach(enumerable, bind(&SortedMultiSet::Add, this, _1));
 		}
 
 		virtual shared_ptr<IEnumerator<ValueType> > GetEnumerator() const
