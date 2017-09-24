8eed54638ac7fed64ca901eebf918822519e9eca
Igor Zavorotkin <zavorotkin@spb.gs.ru>
2017-09-24 21:02:11 +0300

    UUID: export format

--- stingraykit/UUID.cpp
+++ stingraykit/UUID.cpp
@@ -14,10 +14,7 @@
 namespace stingray
 {
 
-	namespace
-	{
-		const std::string UuidFormat = "XXXX-XX-XX-XX-XXXXXX";
-	}
+	const std::string UUID::Format = "XXXX-XX-XX-XX-XXXXXX";
 
 
 	bool UUID::operator<(const UUID& other) const
@@ -26,20 +23,14 @@ namespace stingray
 	}
 
 
-	void UUID::GenerateImpl(UUID& uuid)
-	{
-		for (DataType::iterator it = uuid._data.begin(); it != uuid._data.end(); ++it)
-			*it = rand();
-	}
-
 	UUID UUID::FromString(const std::string& str)
 	{
-		STINGRAYKIT_CHECK(str.size() == static_cast<std::string::size_type>(2 * std::count(UuidFormat.begin(), UuidFormat.end(), 'X') + std::count(UuidFormat.begin(), UuidFormat.end(), '-')), "Invalid format");
+		STINGRAYKIT_CHECK(str.size() == static_cast<std::string::size_type>(2 * std::count(Format.begin(), Format.end(), 'X') + std::count(Format.begin(), Format.end(), '-')), "Invalid format");
 
 		UUID result;
 
 		DataType::iterator data_it = result._data.begin();
-		for (std::string::const_iterator format_it = UuidFormat.begin(), str_it = str.begin(); format_it != UuidFormat.end(); ++format_it)
+		for (std::string::const_iterator format_it = Format.begin(), str_it = str.begin(); format_it != Format.end(); ++format_it)
 		{
 			if (*format_it == 'X')
 				*data_it++ = FromHex<u8>(std::string((str_it++).base(), 2 * sizeof(u8)));
@@ -51,18 +42,26 @@ namespace stingray
 		return result;
 	}
 
+
 	std::string UUID::ToString() const
 	{
 		std::string result;
 
 		DataType::const_iterator data_it = _data.begin();
-		for (std::string::const_iterator format_it = UuidFormat.begin(); format_it != UuidFormat.end(); ++format_it)
+		for (std::string::const_iterator format_it = Format.begin(); format_it != Format.end(); ++format_it)
 			if (*format_it == 'X')
 				result += ToHex(*data_it++, 2 * sizeof(u8));
 			else
 				result += *format_it;
 
 		return result;
+	}
+
+
+	void UUID::GenerateImpl(UUID& uuid)
+	{
+		for (DataType::iterator it = uuid._data.begin(); it != uuid._data.end(); ++it)
+			*it = rand();
 	}
 
 }
--- stingraykit/UUID.h
+++ stingraykit/UUID.h
@@ -8,18 +8,21 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-
 #include <stingraykit/collection/array.h>
 #include <stingraykit/toolkit.h>
-
 
 namespace stingray
 {
 
-	struct UUID
+	class UUID
 	{
+	public:
+		static const std::string Format;
+
 	private:
 		typedef array<u8, 16> DataType;
+
+	private:
 		DataType _data;
 
 	public:
