f23afed2cc92aea3433ab11688ffae1b443c9b8d
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2017-10-24 22:33:51 +0300

    KeyValuePair: avoid some boilerplate

--- stingraykit/collection/KeyValuePair.h
+++ stingraykit/collection/KeyValuePair.h
@@ -8,7 +8,7 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/compare/comparers.h>
+#include <stingraykit/compare/MemberListComparer.h>
 #include <stingraykit/string/ToString.h>
 
 namespace stingray
@@ -41,12 +41,7 @@ namespace stingray
 		{ }
 
 		int Compare(const KeyValuePair& other) const
-		{
-			int r = comparers::Cmp()(Key, other.Key);
-			if (r != 0)
-				return r;
-			return comparers::Cmp()(Value, other.Value);
-		}
+		{ return CompareMembersCmp(&KeyValuePair::Key, &KeyValuePair::Value)(*this, other); }
 
 		KeyType GetKey() const			{ return Key; }
 		ValueType GetValue() const		{ return Value; }
