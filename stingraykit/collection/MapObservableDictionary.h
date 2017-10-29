1d4add9b6c8102301959f97fdd830e12906f47f4
Danil Dulin <danil.dulin@gs-labs.ru>
2017-10-29 18:16:53 +0300

    MapObservableDictionary: add support for custom underlying map

--- stingraykit/collection/MapObservableDictionary.h
+++ stingraykit/collection/MapObservableDictionary.h
@@ -19,16 +19,31 @@ namespace stingray
 	 * @{
 	 */
 
-	template < typename KeyType_, typename ValueType_, typename CompareType_ = comparers::Less >
+	template <
+			typename KeyType_,
+			typename ValueType_ ,
+			typename CompareType_ = comparers::Less,
+			template <class, class, class, class> class MapType_ = std::map,
+			typename AllocatorType_ = std::allocator<std::pair<const KeyType_, ValueType_> >
+			>
 	struct MapObservableDictionary
-		:	public ObservableDictionaryWrapper<MapDictionary<KeyType_, ValueType_, CompareType_> >
+		:	public ObservableDictionaryWrapper<MapDictionary<KeyType_, ValueType_, CompareType_, MapType_, AllocatorType_> >
 	{
-		typedef ObservableDictionaryWrapper<MapDictionary<KeyType_, ValueType_, CompareType_> > base;
+		typedef ObservableDictionaryWrapper<MapDictionary<KeyType_, ValueType_, CompareType_, MapType_, AllocatorType_> > base;
 
 		MapObservableDictionary() : base() { }
 		MapObservableDictionary(shared_ptr<IEnumerable<typename base::PairType> > enumerable) : base(enumerable) { }
 		MapObservableDictionary(shared_ptr<IEnumerator<typename base::PairType> > enumerator) : base(enumerator) { }
 	};
+
+	template <
+			typename KeyType,
+			typename ValueType,
+			typename CompareType = comparers::Less,
+			typename AllocatorType = typename flat_map<KeyType, ValueType, CompareType>::allocator_type
+			>
+	struct FlatMapObservableDictionary
+	{ typedef MapObservableDictionary<KeyType, ValueType, CompareType, flat_map, AllocatorType>		Type; };
 
 	/** @} */
 
