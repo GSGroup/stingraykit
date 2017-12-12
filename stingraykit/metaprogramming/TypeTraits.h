dcaffd0ce350d29a04201459b9f356c84ddf5613
Ivan Leonov <ivan.leonov@gs-labs.ru>
2017-12-12 10:08:49 +0300

    Code style fix

--- stingraykit/metaprogramming/TypeTraits.h
+++ stingraykit/metaprogramming/TypeTraits.h
@@ -14,54 +14,32 @@
 namespace stingray
 {
 
-	template < typename T >
-	struct IsReference { static const bool Value = false; };
-
-	template < typename T >
-	struct IsReference<T&> { static const bool Value = true; };
-
-	template < typename T >
-	struct IsConstReference { static const bool Value = false; };
-
-	template < typename T >
-	struct IsConstReference<const T&> { static const bool Value = true; };
-
-	template < typename T >
-	struct IsNonConstReference { static const bool Value = IsReference<T>::Value && !IsConstReference<T>::Value; };
-
-	template < typename T >
-	struct IsConst { static const bool Value = false; };
-
-	template < typename T >
-	struct IsConst<const T> { static const bool Value = true; };
-
-	template < typename T >
-	struct IsPointer { static const bool Value = false; };
-
-	template < typename T >
-	struct IsPointer<T*> { static const bool Value = true; };
-
-	template < typename T >
-	struct IsPointer<T* const> { static const bool Value = true; };
-
-	template < typename T >
-	struct IsArray { static const bool Value = false; };
-
-	template < typename T, unsigned N >
-	struct IsArray<T[N]> { static const bool Value = true; };
-
-	template < typename T, typename MemberPointerDetector = void >
-	struct IsClass
-	{ static const bool Value = false; };
-
-	template < typename T >
-	struct IsClass<T, typename ToVoid<int T::*>::ValueT >
-	{ static const bool Value = true; };
+	template < typename T > struct IsConst											{ static const bool Value = false; };
+	template < typename T > struct IsConst<const T>									{ static const bool Value = true; };
 
 
-	template <typename T>
-	struct StaticAssertCompleteType : CompileTimeAssert<sizeof(T) == sizeof(T)>
-	{ };
+	template < typename T > struct IsReference										{ static const bool Value = false; };
+	template < typename T > struct IsReference<T&>									{ static const bool Value = true; };
+
+	template < typename T > struct IsPointer										{ static const bool Value = false; };
+	template < typename T > struct IsPointer<T*>									{ static const bool Value = true; };
+	template < typename T > struct IsPointer<T* const>								{ static const bool Value = true; };
+
+	template < typename T > struct IsArray											{ static const bool Value = false; };
+	template < typename T, unsigned N > struct IsArray<T[N]>						{ static const bool Value = true; };
+
+
+	template < typename T > struct IsConstReference									{ static const bool Value = false; };
+	template < typename T > struct IsConstReference<const T&>						{ static const bool Value = true; };
+
+	template < typename T > struct IsNonConstReference								{ static const bool Value = IsReference<T>::Value && !IsConstReference<T>::Value; };
+
+
+	template < typename T, typename MemberPointerDetector = void > struct IsClass	{ static const bool Value = false; };
+	template < typename T > struct IsClass<T, typename ToVoid<int T::*>::ValueT>	{ static const bool Value = true; };
+
+
+	template < typename T > struct StaticAssertCompleteType : CompileTimeAssert<sizeof(T) == sizeof(T)> { };
 
 }
 
