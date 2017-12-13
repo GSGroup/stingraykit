7125674c4c8bfa3fde61207919aa41979a01050a
Ivan Leonov <ivan.leonov@gs-labs.ru>
2017-12-13 10:40:38 +0300

    Use IsComplete helper

--- stingraykit/metaprogramming/TypeRelationships.h
+++ stingraykit/metaprogramming/TypeRelationships.h
@@ -8,8 +8,9 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/metaprogramming/YesNo.h>
 #include <stingraykit/metaprogramming/IntegralConstant.h>
+#include <stingraykit/metaprogramming/TypeTraits.h>
+#include <stingraykit/metaprogramming/YesNo.h>
 
 namespace stingray
 {
@@ -27,7 +28,7 @@ namespace stingray
 			static NoType TestInheritance(...);
 
 		public:
-			static const bool Value = ( sizeof(TestInheritance((const Derived*)0)) == sizeof(YesType) ) && (sizeof(Derived) == sizeof(Derived));
+			static const bool Value = ( sizeof(TestInheritance((const Derived*)0)) == sizeof(YesType) ) && IsComplete<Derived>::Value;
 		};
 	}
 
