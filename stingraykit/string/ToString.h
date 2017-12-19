5a83b8e7fe8aea706f93dce267c4e63e8a606d3e
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2017-12-19 18:57:44 +0300

    ToString: cleanup headers, remove ugly forward declaration for std::map

--- stingraykit/string/ToString.h
+++ stingraykit/string/ToString.h
@@ -8,26 +8,14 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/Dummy.h>
-#include <stingraykit/Macro.h>
-#include <stingraykit/collection/CollectionBuilder.h>
 #include <stingraykit/collection/IEnumerable.h>
 #include <stingraykit/collection/RangeBase.h>
-#include <stingraykit/exception.h>
-#include <stingraykit/optional.h>
-#include <stingraykit/shared_ptr.h>
 #include <stingraykit/string/string_stream.h>
 
 #include <algorithm>
 #include <ctype.h>
 #include <limits>
-#include <string>
-
-namespace std
-{
-	template < class Key, class T, class Compare, class Allocator > class map;
-}
-
+#include <map>
 
 namespace stingray
 {
