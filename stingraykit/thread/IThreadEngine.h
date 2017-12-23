cc413679d54de2e5d9566e3b9259c6432b112fd5
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2017-12-23 23:34:14 +0300

    IThreadEngine: cleanup headers

--- stingraykit/thread/IThreadEngine.h
+++ stingraykit/thread/IThreadEngine.h
@@ -8,12 +8,10 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/ICreator.h>
 #include <stingraykit/diagnostics/ExternalAPIGuard.h>
-#include <stingraykit/exception.h>
-#include <stingraykit/shared_ptr.h>
 #include <stingraykit/string/ToString.h>
-#include <stingraykit/toolkit.h>
+#include <stingraykit/time/Time.h>
+#include <stingraykit/shared_ptr.h>
 
 namespace stingray
 {
