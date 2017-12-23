b51e9f8b554abe1950955142109d9b5a0172d47e
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2017-12-23 23:33:51 +0300

    PosixSemaphore: remove unneeded header

--- stingraykit/thread/posix/PosixSemaphore.h
+++ stingraykit/thread/posix/PosixSemaphore.h
@@ -8,15 +8,13 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+#include <stingraykit/time/Time.h>
+
 #include <pthread.h>
 #include <semaphore.h>
 
-#include <stingraykit/time/Time.h>
-#include <stingraykit/exception.h>
-
 namespace stingray {
 namespace posix {
-
 
 	class PosixSemaphore
 	{
@@ -38,10 +36,7 @@ namespace posix {
 		bool TimedWait(const Time& absTime) const;
 	};
 
-
-}
-}
-
+}}
 
 #endif
 
