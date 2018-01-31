04b776328f7e57fb8101e24cfc26a1d61e5ce11b
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2018-02-01 00:03:03 +0300

    ByteData: cleanup headers

--- stingraykit/collection/ByteData.h
+++ stingraykit/collection/ByteData.h
@@ -8,14 +8,11 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/Dummy.h>
-#include <stingraykit/TypeInfo.h>
 #include <stingraykit/collection/array.h>
 #include <stingraykit/collection/iterator_base.h>
 #include <stingraykit/shared_ptr.h>
 
 #include <algorithm>
-#include <vector>
 
 #define DETAIL_BYTEDATA_INDEX_CHECK(Arg1, Arg2) STINGRAYKIT_CHECK((Arg1) <= (Arg2), IndexOutOfRangeException(Arg1, Arg2))
 
