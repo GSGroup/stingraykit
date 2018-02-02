87c7945ea1263213ec01de0379493c66a4a45576
Igor Zavorotkin <zavorotkin@spb.gs.ru>
2018-02-03 02:15:28 +0300

    drop unused include

--- stingraykit/collection/DictionaryKeysSet.h
+++ stingraykit/collection/DictionaryKeysSet.h
@@ -8,7 +8,6 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/collection/EnumeratorWrapper.h>
 #include <stingraykit/collection/IObservableDictionary.h>
 #include <stingraykit/collection/IObservableSet.h>
 #include <stingraykit/signal/signals.h>
