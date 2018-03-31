1d91d8528c0eedb1066bc215ea884577f872779a
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2018-03-31 22:48:44 +0300

    Holder: use stingray's exception, cleanup headers

--- stingraykit/Holder.h
+++ stingraykit/Holder.h
@@ -8,9 +8,7 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/function/bind.h>
 #include <stingraykit/log/Logger.h>
-#include <stingraykit/shared_ptr.h>
 
 namespace stingray
 {
@@ -54,7 +52,7 @@ namespace stingray
 		}
 
 	private:
-		void Check() const { STINGRAYKIT_CHECK(_valid, std::logic_error("ScopedHolder is not valid!")); }
+		void Check() const { STINGRAYKIT_CHECK(_valid, LogicException("ScopedHolder is not valid!")); }
 		void Cleanup()
 		{
 			if (_valid)
@@ -88,7 +86,7 @@ namespace stingray
 		void Set()				{ Cleanup(); _valid = true; }
 
 	private:
-		void Check() const		{ STINGRAYKIT_CHECK(_valid, std::logic_error("ScopedHolder is not valid!")); }
+		void Check() const		{ STINGRAYKIT_CHECK(_valid, LogicException("ScopedHolder is not valid!")); }
 		void Cleanup() const	{ if (_valid) _cleanupFunc(); }
 	};
 
