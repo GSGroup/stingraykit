d0d59213f07a745fa44d5a1d56a62910a42c1374
Sergey Morozov <sergey.morozov@iksatprof.ru>
2018-05-14 18:03:19 +0300

    Introduced Decimal

---
+++ stingraykit/Decimal.cpp
@@ -0,0 +1,114 @@
+// Copyright (c) 2011 - 2018, GS Group, https://github.com/GSGroup
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+// provided that the above copyright notice and this permission notice appear in all copies.
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+// WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+#include <stingraykit/Decimal.h>
+#include <stingraykit/string/StringUtils.h>
+
+#include <cmath>
+
+namespace stingray
+{
+
+	namespace
+	{
+
+		const u16 BaseOfExponent = 10;
+
+		std::string RemoveUnsignificantZeros(const std::string& source)
+		{
+			std::string::const_reverse_iterator it = source.rbegin();
+			while (it != source.rend() && *it == '0')
+				++it;
+			return std::string(source.begin(), source.begin() + (source.rend() - it));
+		}
+
+		std::string AddSignificantZeros(const std::string& source, u16 exponent)
+		{
+			if (source.size() < exponent)
+				return std::string(exponent - source.size(), '0') + source;
+			return source;
+		}
+
+		s64 PowToExponent(const Decimal& decimal, u16 exponent)
+		{ return decimal.GetMantissa() * std::pow(BaseOfExponent, exponent - decimal.GetExponent()); }
+
+	}
+
+	Decimal Decimal::FromString(const std::string& str)
+	{
+		std::vector<std::string> fractions;
+		Copy(Split(str, "."), std::back_inserter(fractions));
+		STINGRAYKIT_CHECK_RANGE(fractions.size(), 1, 3);
+
+		const s64 integralPart = stingray::FromString<s64>(fractions[0]);
+
+		if (fractions.size() == 1)
+			return integralPart;
+
+		const std::string rawFractionalPart = RemoveUnsignificantZeros(fractions[1]);
+		const s64 fractionalPart = stingray::FromString<s64>(rawFractionalPart);
+
+		const u16 exponent = (u16)rawFractionalPart.size();
+		const s64 integralMantissa = integralPart * std::pow(BaseOfExponent, exponent);
+		const s64 fractionalMantissa = (integralPart >= 0) ? fractionalPart : -fractionalPart;
+		const int signFactor = (!fractions[0].empty() && fractions[0][0] == '-' && integralPart == 0) ? -1 : 1;
+
+		return Decimal((integralMantissa + fractionalMantissa) * signFactor, exponent);
+	}
+
+
+	std::string Decimal::ToString() const
+	{
+		const std::string value = AddSignificantZeros(stingray::ToString(_mantissa), _exponent);
+
+		std::string integralPart =  ExtractPrefix(value, value.size() - _exponent);
+		std::string fractionalPart  = RemoveUnsignificantZeros(RemovePrefix(value, integralPart));
+
+		if (integralPart.empty() || integralPart == "-")
+			integralPart.append("0");
+
+		StringBuilder builder = StringBuilder() % integralPart;
+
+		if (!fractionalPart.empty())
+			builder % "." % fractionalPart;
+		return builder;
+	}
+
+
+	bool Decimal::operator == (const Decimal& other) const
+	{
+		const u16 maxExponent = std::max(_exponent, other._exponent);
+		return PowToExponent(*this, maxExponent) == PowToExponent(other, maxExponent);
+	}
+
+
+	bool Decimal::operator < (const Decimal& other) const
+	{
+		const u16 maxExponent = std::max(_exponent, other._exponent);
+		return PowToExponent(*this, maxExponent) < PowToExponent(other, maxExponent);
+	}
+
+
+	Decimal& Decimal::operator += (const Decimal& other)
+	{
+		const u16 maxExponent = std::max(_exponent, other._exponent);
+		_mantissa = PowToExponent(*this, maxExponent) + PowToExponent(other, maxExponent);
+		_exponent = maxExponent;
+		return *this;
+	}
+
+
+	Decimal& Decimal::operator *= (const Decimal& other)
+	{
+		const u16 maxExponent = std::max(_exponent, other._exponent);
+		_mantissa = PowToExponent(*this, maxExponent) * PowToExponent(other, maxExponent);
+		_exponent = maxExponent + maxExponent;
+		return *this;
+	}
+
+}
---
+++ stingraykit/Decimal.h
@@ -0,0 +1,66 @@
+#ifndef STINGRAYKIT_DECIMAL_H
+#define STINGRAYKIT_DECIMAL_H
+
+// Copyright (c) 2011 - 2018, GS Group, https://github.com/GSGroup
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+// provided that the above copyright notice and this permission notice appear in all copies.
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+// WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+#include <stingraykit/Types.h>
+#include <stingraykit/toolkit.h>
+
+namespace stingray
+{
+
+	class Decimal
+	{
+	private:
+		s64			_mantissa;
+		u16			_exponent;
+
+	public:
+		Decimal(s64 mantissa = 0, u16 exponent = 0)
+			:	_mantissa(mantissa),
+				_exponent(exponent)
+		{ }
+
+		s64 GetMantissa() const
+		{ return _mantissa; }
+
+		u16 GetExponent() const
+		{ return _exponent; }
+
+		static Decimal FromString(const std::string& str);
+		std::string ToString() const;
+
+		bool operator == (const Decimal& other) const;
+		STINGRAYKIT_GENERATE_EQUALITY_OPERATORS_FROM_EQUAL(Decimal);
+
+		bool operator < (const Decimal& other) const;
+		STINGRAYKIT_GENERATE_RELATIONAL_OPERATORS_FROM_LESS(Decimal);
+
+		Decimal& operator += (const Decimal& other);
+		Decimal& operator *= (const Decimal& other);
+
+		Decimal operator - () const
+		{ return Decimal(-_mantissa, _exponent); }
+
+		Decimal& operator -= (const Decimal& other)
+		{ return *this += -other; }
+
+		Decimal operator + (const Decimal& other) const
+		{ Decimal res(*this); return res += other; }
+
+		Decimal operator - (const Decimal& other) const
+		{ Decimal res(*this); return res -= other; }
+
+		Decimal operator * (const Decimal& other) const
+		{ Decimal res(*this); return res *= other; }
+	};
+
+}
+
+#endif
