fabc5590fcc179c7e0a9ba8b03a3e6f60fe2f5d0
Igor Zavorotkin <zavorotkin@spb.gs.ru>
2018-10-01 19:29:03 +0300

    add all_of and none_of algorithms

--- stingraykit/algorithm.h
+++ stingraykit/algorithm.h
@@ -44,7 +44,7 @@ namespace stingray
 
 
 	template<class InputIterator, class UnaryPredicate>
-	bool any_of (InputIterator first, InputIterator last, UnaryPredicate pred)
+	bool any_of(InputIterator first, InputIterator last, UnaryPredicate pred)
 	{
 		while (first != last)
 		{
@@ -53,6 +53,32 @@ namespace stingray
 			++first;
 		}
 		return false;
+	}
+
+
+	template<class InputIterator, class UnaryPredicate>
+	bool all_of(InputIterator first, InputIterator last, UnaryPredicate pred)
+	{
+		while (first != last)
+		{
+			if (!pred(*first))
+				return false;
+			++first;
+		}
+		return true;
+	}
+
+
+	template<class InputIterator, class UnaryPredicate>
+	bool none_of(InputIterator first, InputIterator last, UnaryPredicate pred)
+	{
+		while (first != last)
+		{
+			if (pred(*first))
+				return false;
+			++first;
+		}
+		return true;
 	}
 
 
