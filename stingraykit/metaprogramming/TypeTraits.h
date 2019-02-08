5827ca959223e7b6fc132e44966c29bfc95b61db
Ivan Leonov <ivan.leonov@gs-labs.ru>
2019-02-08 14:58:01 +0300

    stingraykit.TypeTraits: Use casted -1 instead of inversed 0 for sign check
    	~T(0) - cause integral promotion and breaks the check for types that
    	sizeof lesser than int
    
    	thanks for Andrey Egorov

--- stingraykit/metaprogramming/TypeTraits.h
+++ stingraykit/metaprogramming/TypeTraits.h
@@ -71,10 +71,10 @@ namespace stingray
 	namespace Detail
 	{
 
-		template < typename T, bool = IsInt<T>::Value > struct IsSignedImpl			: integral_constant<bool, ~T(0) < T(0)> { };
+		template < typename T, bool = IsInt<T>::Value > struct IsSignedImpl			: integral_constant<bool, T(-1) < T(0)> { };
 		template < typename T > struct IsSignedImpl<T, false>						: FalseType { };
 
-		template < typename T, bool = IsInt<T>::Value > struct IsUnsignedImpl		: integral_constant<bool, T(0) <~ T(0)> { };
+		template < typename T, bool = IsInt<T>::Value > struct IsUnsignedImpl		: integral_constant<bool, T(0) < T(-1)> { };
 		template < typename T > struct IsUnsignedImpl<T, false>						: FalseType { };
 
 	}
