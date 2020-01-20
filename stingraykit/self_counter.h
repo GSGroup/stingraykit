bb88a3ae0b043371a163800ff8c6791309cd7316
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2020-01-20 17:51:53 +0300

    self_counter: update some code-style and formatting
    Drop redundant inline from implicitly inlining methods.

--- stingraykit/self_counter.h
+++ stingraykit/self_counter.h
@@ -8,17 +8,18 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+#include <stingraykit/core/NonCopyable.h>
+#include <stingraykit/thread/atomic/AtomicInt.h>
 #include <stingraykit/CheckedDelete.h>
 #include <stingraykit/Dummy.h>
-#include <stingraykit/core/NonCopyable.h>
 #include <stingraykit/exception.h>
 #include <stingraykit/safe_bool.h>
-#include <stingraykit/thread/atomic/AtomicInt.h>
 
 #define STINGRAYKIT_DECLARE_SELF_COUNT_PTR(type) typedef stingray::self_count_ptr< type > type##SelfCountPtr;
 
 namespace stingray
 {
+
 	namespace Detail
 	{
 		struct SelfCounterHelper
@@ -28,109 +29,125 @@ namespace stingray
 		};
 	}
 
+
 	struct static_cast_tag
-	{};
+	{ };
+
 
 	template < typename T >
 	class self_count_ptr : public safe_bool<self_count_ptr<T> >
 	{
-	private:
-		template<typename U> friend class self_count_ptr;
-		T*				_rawPtr;
+		template < typename U >
+		friend class self_count_ptr;
 
 	public:
 		typedef T ValueType;
 
-		explicit inline self_count_ptr(T* rawPtr = 0)
+	private:
+		T*				_rawPtr;
+
+	public:
+		explicit self_count_ptr(T* rawPtr = 0)
 			: _rawPtr(rawPtr)
-		{}
+		{ }
 
 		self_count_ptr(const NullPtrType&)
 			: _rawPtr()
-		{}
+		{ }
 
-		inline self_count_ptr(const self_count_ptr<T>& other)
+		self_count_ptr(const self_count_ptr<T>& other)
 			: _rawPtr(other._rawPtr)
-		{ if (_rawPtr) _rawPtr->add_ref(); }
+		{
+			if (_rawPtr)
+				_rawPtr->add_ref();
+		}
 
-		template<typename U>
-		inline self_count_ptr(const self_count_ptr<U>& other, typename EnableIf<IsInherited<U, T>::Value, Dummy>::ValueT* = 0)
+		template < typename U >
+		self_count_ptr(const self_count_ptr<U>& other, typename EnableIf<IsInherited<U, T>::Value, Dummy>::ValueT* = 0)
 			: _rawPtr(other._rawPtr)
-		{ if (_rawPtr) _rawPtr->add_ref(); }
+		{
+			if (_rawPtr)
+				_rawPtr->add_ref();
+		}
 
-		template<typename U>
-		inline self_count_ptr(const self_count_ptr<U>& other, static_cast_tag)
+		template < typename U >
+		self_count_ptr(const self_count_ptr<U>& other, static_cast_tag)
 			: _rawPtr(static_cast<T*>(other._rawPtr))
-		{ if (_rawPtr) _rawPtr->add_ref(); }
+		{
+			if (_rawPtr)
+				_rawPtr->add_ref();
+		}
 
-		inline ~self_count_ptr()
+		~self_count_ptr()
 		{
 			if (_rawPtr)
 				_rawPtr->release_ref();
 		}
 
-		inline self_count_ptr<T>& operator = (const self_count_ptr<T>& other)
+		self_count_ptr<T>& operator = (const self_count_ptr<T>& other)
 		{
 			self_count_ptr<T> tmp(other);
 			swap(tmp);
 			return *this;
 		}
 
-		inline bool operator == (T* ptr) const							{ return _rawPtr == ptr; }
-		inline bool operator != (T* ptr) const							{ return !(*this == ptr); }
-		inline bool operator == (const self_count_ptr<T>& other) const	{ return other == _rawPtr; }
-		inline bool operator != (const self_count_ptr<T>& other) const	{ return !(*this == other); }
-		inline bool boolean_test() const									{ return _rawPtr != 0; }
+		bool operator == (T* ptr) const								{ return _rawPtr == ptr; }
+		bool operator != (T* ptr) const								{ return !(*this == ptr); }
+		bool operator == (const self_count_ptr<T>& other) const		{ return other == _rawPtr; }
+		bool operator != (const self_count_ptr<T>& other) const		{ return !(*this == other); }
+		bool boolean_test() const									{ return _rawPtr != 0; }
 
-		inline void reset(T* ptr = 0)
+		void reset(T* ptr = 0)
 		{
 			self_count_ptr<T> tmp(ptr);
 			swap(tmp);
 		}
 
-		inline void swap(self_count_ptr<T>& other) { std::swap(_rawPtr, other._rawPtr); }
+		void swap(self_count_ptr<T>& other) { std::swap(_rawPtr, other._rawPtr); }
 
-		inline T* get() const				{ return _rawPtr; }
-		inline T* operator -> () const		{ check_ptr(); return _rawPtr; }
-		inline T& operator * () const		{ check_ptr(); return *_rawPtr; }
-		inline bool unique() const			{ return _rawPtr? _rawPtr->value() == 1: true; }
+		T* get() const					{ return _rawPtr; }
+		T* operator -> () const			{ check_ptr(); return _rawPtr; }
+		T& operator * () const			{ check_ptr(); return *_rawPtr; }
+		bool unique() const				{ return _rawPtr ? _rawPtr->value() == 1 : true; }
 
 		bool owner_before(const self_count_ptr& other) const
 		{ return get() < other.get(); }
 
 	private:
-		inline void check_ptr() const
+		void check_ptr() const
 		{ STINGRAYKIT_CHECK(_rawPtr, NullPointerException()); }
 	};
 
 
-	template<typename T>
+	template < typename T >
 	class self_counter : private NonCopyable
 	{
+	private:
 		mutable AtomicS32::Type	_value;
 
 	protected:
-		~self_counter() {}
+		~self_counter()
+		{ }
 
 	public:
 		self_counter() : _value(1)
-		{}
+		{ }
 
-		inline self_count_ptr<T> self_count_ptr_from_this()
+		self_count_ptr<T> self_count_ptr_from_this()
 		{
 			add_ref();
 			return self_count_ptr<T>(static_cast<T*>(this));
 		}
 
-		inline void add_ref() const
+		void add_ref() const
 		{
-			s32 count = AtomicS32::Inc(_value); (void)count;
+			const s32 count = AtomicS32::Inc(_value); (void)count;
 			STINGRAYKIT_DEBUG_ONLY(Detail::SelfCounterHelper::CheckAddRef(count));
 		}
 
-		inline void release_ref() const
+		void release_ref() const
 		{
-			s32 count = AtomicS32::Dec(_value);
+			const s32 count = AtomicS32::Dec(_value);
 			if (count == 0)
 			{
 				STINGRAYKIT_ANNOTATE_HAPPENS_AFTER(&_value);
@@ -142,7 +159,8 @@ namespace stingray
 			STINGRAYKIT_DEBUG_ONLY(Detail::SelfCounterHelper::CheckReleaseRef(count));
 		}
 
-		inline int value() const		{ return _value; }
+		int value() const
+		{ return _value; }
 	};
 
 
@@ -150,11 +168,14 @@ namespace stingray
 	struct ToPointer<self_count_ptr<T> >
 	{ typedef T* ValueT; };
 
-	template < typename T >
-	inline T* to_pointer(const self_count_ptr<T>& ptr) { return ptr.get(); }
 
 	template < typename T >
-	inline T* to_pointer(self_count_ptr<T>& ptr) { return ptr.get(); }
+	T* to_pointer(const self_count_ptr<T>& ptr)
+	{ return ptr.get(); }
+
+	template < typename T >
+	T* to_pointer(self_count_ptr<T>& ptr)
+	{ return ptr.get(); }
 
 
 #define DETAIL_MAKE_SELF_COUNT_PTR_TYPENAMES(Index_, UserArg_) STINGRAYKIT_COMMA_IF(Index_) typename STINGRAYKIT_CAT(Param, Index_)
