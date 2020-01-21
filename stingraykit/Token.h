86001d0bdbe8dfc7899fa832dfeace75a12ecaf9
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2020-01-21 18:12:51 +0300

    Token: implement move semantics support

--- stingraykit/Token.h
+++ stingraykit/Token.h
@@ -22,6 +22,9 @@ namespace stingray
 
 	class Token
 	{
+		STINGRAYKIT_DEFAULTCOPYABLE(Token);
+		STINGRAYKIT_DEFAULTMOVABLE(Token);
+
 	private:
 		self_count_ptr<IToken>	_token;
 
@@ -37,7 +40,13 @@ namespace stingray
 			: _token(token)
 		{ }
 
+		template < typename T >
+		Token(self_count_ptr<T>&& token)
+			: _token(std::move(token))
+		{ }
+
 		void Set(const Token& token)				{ _token = token._token; }
+		void Set(Token&& token)						{ _token = std::move(token._token); }
 		void Reset()								{ Set(null); }
 
 		bool IsInitialized() const					{ return _token.is_initialized(); }
@@ -46,8 +55,8 @@ namespace stingray
 
 
 	template < typename T, typename... Us >
-	Token MakeToken(const Us&... args)
-	{ return self_count_ptr<IToken>(new T(args...)); }
+	Token MakeToken(Us&&... args)
+	{ return self_count_ptr<IToken>(new T(std::forward<Us>(args)...)); }
 
 
 	template < >
@@ -68,6 +77,10 @@ namespace stingray
 				: _ptr(ptr), _token(token)
 			{ }
 
+			AttachTokenCustomDeleter(const shared_ptr<T>& ptr, Token&& token)
+				: _ptr(ptr), _token(std::move(token))
+			{ }
+
 			void operator () (T*)
 			{
 				_token.Reset();
@@ -83,6 +96,11 @@ namespace stingray
 
 
 	template < typename T >
+	shared_ptr<T> AttachToken(const shared_ptr<T>& ptr, Token&& token)
+	{ return shared_ptr<T>(ptr.get(), Detail::AttachTokenCustomDeleter<T>(ptr, std::move(token))); }
+
+
+	template < typename T >
 	class TokenAttacher
 	{
 	private:
@@ -95,6 +113,9 @@ namespace stingray
 
 		TokenAttacher& operator % (const Token& token)
 		{ _ptr = AttachToken(_ptr, token); return *this; }
+
+		TokenAttacher& operator % (Token&& token)
+		{ _ptr = AttachToken(_ptr, std::move(token)); return *this; }
 
 		operator shared_ptr<T> () const
 		{ return _ptr; }
@@ -115,9 +136,21 @@ namespace stingray
 			: _token(token)
 		{ }
 
+		TokenHolder(Token&& token)
+			: _token(std::move(token))
+		{ }
+
 		void Set(const Token& token_)
 		{
 			Token token(token_);
+
+			MutexLock l(_mutex);
+			std::swap(token, _token);
+		}
+
+		void Set(Token&& token_)
+		{
+			Token token(std::move(token_));
 
 			MutexLock l(_mutex);
 			std::swap(token, _token);
@@ -128,6 +161,9 @@ namespace stingray
 
 		TokenHolder& operator = (const Token& token)
 		{ Set(token); return *this; }
+
+		TokenHolder& operator = (Token&& token)
+		{ Set(std::move(token)); return *this; }
 	};
 
 
@@ -148,7 +184,10 @@ namespace stingray
 		bool Empty() const										{ return _tokens.empty(); }
 
 		void Add(const Token& token)							{ _tokens.push_back(token); }
+		void Add(Token&& token)									{ _tokens.push_back(std::move(token)); }
+
 		ThreadlessTokenPool& operator += (const Token& token)	{ Add(token); return *this; }
+		ThreadlessTokenPool& operator += (Token&& token)		{ Add(std::move(token)); return *this; }
 
 		void Release()											{ Tokens().swap(_tokens); }
 	};
@@ -171,7 +210,10 @@ namespace stingray
 		bool Empty() const							{ MutexLock l(_mutex); return _tokens.empty(); }
 
 		void Add(const Token& token)				{ MutexLock l(_mutex); _tokens.push_back(token); }
+		void Add(Token&& token)						{ MutexLock l(_mutex); _tokens.push_back(std::move(token)); }
+
 		TokenPool& operator += (const Token& token)	{ Add(token); return *this; }
+		TokenPool& operator += (Token&& token)		{ Add(std::move(token)); return *this; }
 
 		void Release()
 		{
@@ -202,8 +244,15 @@ namespace stingray
 				: _tokens(tokens), _key(key)
 			{ }
 
+			BracketsOperatorProxy(Tokens& tokens, Key&& key)
+				: _tokens(tokens), _key(std::move(key))
+			{ }
+
 			BracketsOperatorProxy& operator += (const Token& token)
 			{ _tokens.emplace(_key, token); return *this; }
+
+			BracketsOperatorProxy& operator += (Token&& token)
+			{ _tokens.emplace(_key, std::move(token)); return *this; }
 		};
 
 	private:
@@ -218,6 +267,9 @@ namespace stingray
 
 		BracketsOperatorProxy operator [] (const Key& key)
 		{ return BracketsOperatorProxy(_tokens, key); }
+
+		BracketsOperatorProxy operator [] (Key&& key)
+		{ return BracketsOperatorProxy(_tokens, std::move(key)); }
 
 		void release(const Key& key)
 		{ _tokens.erase(key); }
@@ -247,10 +299,21 @@ namespace stingray
 				: _mutex(mutex), _tokens(tokens), _key(key)
 			{ }
 
+			BracketsOperatorProxy(Mutex& mutex, Tokens& tokens, Key&& key)
+				: _mutex(mutex), _tokens(tokens), _key(std::move(key))
+			{ }
+
 			BracketsOperatorProxy& operator += (const Token& token)
 			{
 				MutexLock l(_mutex);
 				_tokens.emplace(_key, token);
+				return *this;
+			}
+
+			BracketsOperatorProxy& operator += (Token&& token)
+			{
+				MutexLock l(_mutex);
+				_tokens.emplace(_key, std::move(token));
 				return *this;
 			}
 		};
@@ -268,6 +331,9 @@ namespace stingray
 
 		BracketsOperatorProxy operator [] (const Key& key)
 		{ return BracketsOperatorProxy(_mutex, _tokens, key); }
+
+		BracketsOperatorProxy operator [] (Key&& key)
+		{ return BracketsOperatorProxy(_mutex, _tokens, std::move(key)); }
 
 		void release(const Key& key)
 		{
