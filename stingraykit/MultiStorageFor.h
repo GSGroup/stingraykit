f266fb8391b622e14c2efec37ecba0681a59c003
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2020-01-28 21:20:08 +0300

    MultiStorageFor: update code-style

--- stingraykit/MultiStorageFor.h
+++ stingraykit/MultiStorageFor.h
@@ -14,7 +14,7 @@
 namespace stingray
 {
 
-	template<typename TypeList>
+	template < typename TypeList >
 	struct MultiStorageFor
 	{
 		typedef typename MaxElement<typename TypeListTransform<TypeList, SizeOf>::ValueT, integral_constant_less>::ValueT		MaxSize;
@@ -24,28 +24,28 @@ namespace stingray
 
 		Storage _value;
 
-		template<typename T>
+		template < typename T >
 		void Ctor()											{ CheckCanContain<T>(); T* ptr = new(&_value) T(); (void)ptr; assert(ptr == &Ref<T>()); }
 
-		template <typename T, typename P1>
+		template < typename T, typename P1 >
 		void Ctor(const P1& p1)								{ CheckCanContain<T>(); T* ptr = new(&_value) T(p1); (void)ptr; assert(ptr == &Ref<T>()); }
 
-		template <typename T, typename P1, typename P2>
+		template < typename T, typename P1, typename P2 >
 		void Ctor(const P1& p1, const P2& p2)				{ CheckCanContain<T>(); T* ptr = new(&_value) T(p1, p2); (void)ptr; assert(ptr == &Ref<T>()); }
 
-		template <typename T, typename P1, typename P2, typename P3>
+		template < typename T, typename P1, typename P2, typename P3 >
 		void Ctor(const P1& p1, const P2& p2, const P3& p3)	{ CheckCanContain<T>(); T* ptr = new(&_value) T(p1, p2, p3); (void)ptr; assert(ptr == &Ref<T>()); }
 
-		template<typename T>
+		template < typename T >
 		void Dtor()											{ CheckCanContain<T>(); Ref<T>().~T(); }
 
-		template<typename T>
+		template < typename T >
 		T& Ref()											{ CheckCanContain<T>(); return *static_cast<T*>(static_cast<void*>(&_value)); }
 
-		template<typename T>
+		template < typename T >
 		const T& Ref() const								{ CheckCanContain<T>(); return *static_cast<const T*>(static_cast<const void*>(&_value)); }
 
-		template<typename T>
+		template < typename T >
 		void CheckCanContain() const						{ CompileTimeAssert<TypeListContains<TypeList, T>::Value> ERROR_TypeIsNotInList; (void)ERROR_TypeIsNotInList; }
 	};
 
