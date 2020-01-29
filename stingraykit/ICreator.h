c44088d1bbf98a05f815d2abe134359718c277b3
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2020-01-29 19:59:46 +0300

    ICreator: move to variadic templates

--- stingraykit/ICreator.h
+++ stingraykit/ICreator.h
@@ -9,6 +9,7 @@
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 
+#include <stingraykit/function/FunctorInvoker.h>
 #include <stingraykit/shared_ptr.h>
 
 
@@ -28,41 +29,25 @@ namespace stingray
 	};
 
 
-#define DETAIL_STINGRAYKIT_DECLARE_CONSTRUCTOR_CREATOR(N_, ParamTypesDecl_, ParamTypes_, ParamMembersDecl_, CtorParamsDecl_, CtorParamsInit_, CtorParams_) \
-	template < typename InterfaceType, typename ClassType, ParamTypesDecl_> \
-	class ConstructorCreator##N_ : public virtual ICreator<InterfaceType> \
-	{ \
-	private: \
-		ParamMembersDecl_ \
-	public: \
-		ConstructorCreator##N_(CtorParamsDecl_) : CtorParamsInit_ { } \
-		virtual shared_ptr<InterfaceType> Create() const { return make_shared_ptr<ClassType>(CtorParams_); } \
-	}; \
-	template < typename InterfaceType, typename ClassType, ParamTypesDecl_ > \
-	shared_ptr<ConstructorCreator##N_<InterfaceType, ClassType, ParamTypes_> > MakeConstructorCreator(CtorParamsDecl_) \
-	{ return make_shared_ptr<ConstructorCreator##N_<InterfaceType, ClassType, ParamTypes_> >(CtorParams_); }
+	template < typename InterfaceType, typename ClassType, typename... Ts >
+	class ConstructorCreator : public virtual ICreator<InterfaceType>
+	{
+		typedef typename TypeList<Ts...>::type ParamTypes;
 
-#define PTD(I_) typename T##I_
-#define PT(I_) T##I_
-#define PMD(I_) T##I_ arg##I_
-#define CPD(I_) const T##I_& arg##I_
-#define CPI(I_) arg##I_(arg##I_)
-#define CP(I_) arg##I_
+	private:
+		Tuple<ParamTypes>	_params;
 
-	DETAIL_STINGRAYKIT_DECLARE_CONSTRUCTOR_CREATOR(1, MK_PARAM(PTD(1)), MK_PARAM(PT(1)), MK_PARAM(PMD(1);), MK_PARAM(CPD(1)), MK_PARAM(CPI(1)), MK_PARAM(CP(1)));
-	DETAIL_STINGRAYKIT_DECLARE_CONSTRUCTOR_CREATOR(2, MK_PARAM(PTD(1), PTD(2)), MK_PARAM(PT(1), PT(2)), MK_PARAM(PMD(1); PMD(2);), MK_PARAM(CPD(1), CPD(2)), MK_PARAM(CPI(1), CPI(2)), MK_PARAM(CP(1), CP(2)));
-	DETAIL_STINGRAYKIT_DECLARE_CONSTRUCTOR_CREATOR(3, MK_PARAM(PTD(1), PTD(2), PTD(3)), MK_PARAM(PT(1), PT(2), PT(3)), MK_PARAM(PMD(1); PMD(2); PMD(3);), MK_PARAM(CPD(1), CPD(2), CPD(3)), MK_PARAM(CPI(1), CPI(2), CPI(3)), MK_PARAM(CP(1), CP(2), CP(3)));
-	DETAIL_STINGRAYKIT_DECLARE_CONSTRUCTOR_CREATOR(4, MK_PARAM(PTD(1), PTD(2), PTD(3), PTD(4)), MK_PARAM(PT(1), PT(2), PT(3), PT(4)), MK_PARAM(PMD(1); PMD(2); PMD(3); PMD(4);), MK_PARAM(CPD(1), CPD(2), CPD(3), CPD(4)), MK_PARAM(CPI(1), CPI(2), CPI(3), CPI(4)), MK_PARAM(CP(1), CP(2), CP(3), CP(4)));
-	DETAIL_STINGRAYKIT_DECLARE_CONSTRUCTOR_CREATOR(5, MK_PARAM(PTD(1), PTD(2), PTD(3), PTD(4), PTD(5)), MK_PARAM(PT(1), PT(2), PT(3), PT(4), PT(5)), MK_PARAM(PMD(1); PMD(2); PMD(3); PMD(4); PMD(5);), MK_PARAM(CPD(1), CPD(2), CPD(3), CPD(4), CPD(5)), MK_PARAM(CPI(1), CPI(2), CPI(3), CPI(4), CPI(5)), MK_PARAM(CP(1), CP(2), CP(3), CP(4), CP(5)));
+	public:
+		ConstructorCreator(const Ts&... args) : _params(args...) { }
 
-#undef CP
-#undef CPI
-#undef CPD
-#undef PMD
-#undef PT
-#undef PTD
+		virtual shared_ptr<InterfaceType> Create() const
+		{ return FunctorInvoker::Invoke(MakeShared<ClassType>(), _params); }
+	};
 
-#undef DETAIL_STINGRAYKIT_DECLARE_CONSTRUCTOR_CREATOR
+
+	template < typename InterfaceType, typename ClassType, typename... Ts >
+	shared_ptr<ConstructorCreator<InterfaceType, ClassType, Ts... > > MakeConstructorCreator(const Ts&... args)
+	{ return make_shared_ptr<ConstructorCreator<InterfaceType, ClassType, Ts...> >(args...); }
 
 
 	template < typename InterfaceType, typename ClassType >
@@ -71,10 +56,6 @@ namespace stingray
 		virtual shared_ptr<InterfaceType> Create() const
 		{ return make_shared_ptr<ClassType>(); }
 	};
-
-	template < typename InterfaceType, typename ClassType >
-	shared_ptr<DefaultConstructorCreator<InterfaceType, ClassType> > MakeConstructorCreator()
-	{ return make_shared_ptr<DefaultConstructorCreator<InterfaceType, ClassType> >(); }
 
 
 	namespace Detail
