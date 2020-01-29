fc5fd8ed3e8a6c4195527792c66bdaab197c9e03
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2020-01-29 21:41:35 +0300

    ForIf: move to variadic templates

--- stingraykit/metaprogramming/ForIf.h
+++ stingraykit/metaprogramming/ForIf.h
@@ -8,50 +8,29 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/Macro.h>
-
 #include <stddef.h>
 
 namespace stingray
 {
 
-#define TY typename
-
 	template < size_t Count, template <size_t> class FunctorClass, size_t Start = 0 >
 	struct ForIf
 	{
-		static bool Do()
+		template < typename... Ts >
+		static bool Do(const Ts&... args)
 		{
-			bool success = FunctorClass<Start>::Call();
-			return success && ForIf<Count - 1, FunctorClass, Start + 1>::Do();
+			const bool success = FunctorClass<Start>::Call(args...);
+			return success && ForIf<Count - 1, FunctorClass, Start + 1>::template Do<Ts...>(args...);
 		}
-
-#define DETAIL_STINGRAYKIT_DECLARE_FOR_IF_DO(TypesDecl_, TypesUsage_, ParamsDecl_, ParamsUsage_) \
-		template < TypesDecl_ > \
-		static bool Do(ParamsDecl_) \
-		{ \
-			bool success = FunctorClass<Start>::Call(ParamsUsage_); \
-			return success && ForIf<Count - 1, FunctorClass, Start + 1>::template Do<TypesUsage_>(ParamsUsage_); \
-		}
-
-		DETAIL_STINGRAYKIT_DECLARE_FOR_IF_DO(MK_PARAM(TY T1), MK_PARAM(T1), MK_PARAM(const T1& p1), MK_PARAM(p1));
-		DETAIL_STINGRAYKIT_DECLARE_FOR_IF_DO(MK_PARAM(TY T1, TY T2), MK_PARAM(T1, T2), MK_PARAM(const T1& p1, const T2& p2), MK_PARAM(p1, p2));
-		DETAIL_STINGRAYKIT_DECLARE_FOR_IF_DO(MK_PARAM(TY T1, TY T2, TY T3), MK_PARAM(T1, T2, T3), MK_PARAM(const T1& p1, const T2& p2, const T3& p3), MK_PARAM(p1, p2, p3));
-
-#undef DETAIL_STINGRAYKIT_DECLARE_FOR_IF_DO
 	};
 
 	template < template <size_t> class FunctorClass, size_t Start >
 	struct ForIf<0, FunctorClass, Start>
 	{
-		static bool Do() { return true; }
-
-		template <TY T1> static bool Do(const T1&) { return true; }
-		template <TY T1, TY T2> static bool Do(const T1&, const T2&) { return true; }
-		template <TY T1, TY T2, TY T3> static bool Do(const T1&, const T2&, const T3&) { return true; }
+		template < typename... Ts >
+		static bool Do(const Ts&...)
+		{ return true; }
 	};
-
-#undef TY
 
 }
 
