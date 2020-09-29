b7dfc04e7f09a0b33e41c5ce381b7c7f64db705a
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2020-09-30 01:30:03 +0300

    CMakeLists: GCC 6.x is minimal requirement now

--- stingraykit/core/Final.h
+++ stingraykit/core/Final.h
@@ -23,11 +23,9 @@ namespace stingray
 	template < typename T >
 	class Final
 	{
-#if defined __GNUC__ && __GNUG__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ > 6
 		friend T;
-#else
-	protected:
-#endif
+
+	private:
 		Final() { }
 		Final(const Final&) { }
 		~Final() { }
