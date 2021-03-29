f782c77dc89a08fb7df7b92372fa78036385c379
Igor Zavorotkin <zavorotkin@spb.gs.ru>
2021-03-29 03:59:40 +0300

    introduce SharedCircularBuffer and BufferedDataConsumer - trivial split up of BufferedDataConsumerBase

---
+++ stingraykit/io/BufferedDataConsumer.h
@@ -0,0 +1,98 @@
+// Copyright (c) 2011 - 2019, GS Group, https://github.com/GSGroup
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+// provided that the above copyright notice and this permission notice appear in all copies.
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+// WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef STINGRAYKIT_IO_BUFFEREDDATACONSUMER_H
+#define STINGRAYKIT_IO_BUFFEREDDATACONSUMER_H
+
+#include <stingraykit/io/IDataSource.h>
+#include <stingraykit/io/SharedCircularBuffer.h>
+#include <stingraykit/log/Logger.h>
+#include <stingraykit/signal/signals.h>
+
+#include <string.h>
+
+namespace stingray
+{
+
+	class BufferedDataConsumer : public virtual IDataConsumer
+	{
+	private:
+		static NamedLogger			s_logger;
+
+		SharedCircularBufferPtr		_buffer;
+
+		bool						_discardOnOverflow;
+		signal<void(size_t)>		_onOverflow;
+
+		const size_t				_inputPacketSize;
+		size_t						_requiredFreeSpace;
+
+		Mutex						_writeMutex;
+
+	public:
+		BufferedDataConsumer(const SharedCircularBufferPtr& buffer, bool discardOnOverflow, size_t inputPacketSize, size_t requiredFreeSpace = 0)
+			:	_buffer(STINGRAYKIT_REQUIRE_NOT_NULL(buffer)),
+				_discardOnOverflow(discardOnOverflow),
+				_inputPacketSize(inputPacketSize),
+				_requiredFreeSpace(requiredFreeSpace)
+		{
+			STINGRAYKIT_CHECK(inputPacketSize != 0, ArgumentException("inputPacketSize", inputPacketSize));
+			STINGRAYKIT_CHECK(_buffer->_buffer.GetTotalSize() % inputPacketSize == 0, "Buffer size is not a multiple of input packet size!");
+			STINGRAYKIT_CHECK(_buffer->_buffer.GetTotalSize() >= requiredFreeSpace, "Buffer size less then required free space!");
+		}
+
+		size_t Process(ConstByteData data, const ICancellationToken& token) override
+		{
+			if (data.size() % _inputPacketSize != 0)
+			{
+				s_logger.Error() << "Data size: " << data.size() << " is not a multiple of input packet size: " << _inputPacketSize;
+				return data.size();
+			}
+
+			MutexLock l1(_writeMutex); // we need this mutex because write can be called simultaneously from several threads
+			MutexLock l2(_buffer->_bufferMutex);
+			SharedCircularBuffer::WriteLock wl(*_buffer);
+
+			STINGRAYKIT_CHECK(!_buffer->_eod, InvalidOperationException("Already got EOD!"));
+			STINGRAYKIT_CHECK(!_buffer->_exception, InvalidOperationException("Already got exception!"));
+
+			BithreadCircularBuffer::Writer w = _buffer->_buffer.Write();
+			size_t packetized_size = w.size() / _inputPacketSize * _inputPacketSize;
+			if (packetized_size == 0 || _buffer->_buffer.GetFreeSize() < _requiredFreeSpace)
+			{
+				if (_discardOnOverflow)
+				{
+					_onOverflow(data.size());
+					return data.size();
+				}
+
+				_buffer->_bufferFull.Wait(_buffer->_bufferMutex, token);
+				return 0;
+			}
+
+			size_t write_size = std::min(data.size(), packetized_size);
+			{
+				MutexUnlock ul(_buffer->_bufferMutex);
+				::memcpy(w.data(), data.data(), write_size);
+			}
+
+			w.Push(write_size);
+			_buffer->_bufferEmpty.Broadcast();
+
+			return write_size;
+		}
+
+		void EndOfData(const ICancellationToken&) override
+		{ _buffer->SetEndOfData(); }
+
+		signal_connector<void(size_t)> OnOverflow() const
+		{ return _onOverflow.connector(); }
+	};
+
+}
+
+#endif
---
+++ stingraykit/io/SharedCircularBuffer.h
@@ -0,0 +1,121 @@
+#ifndef STINGRAYKIT_IO_SHAREDCIRCULARBUFFER_H
+#define STINGRAYKIT_IO_SHAREDCIRCULARBUFFER_H
+// Copyright (c) 2011 - 2019, GS Group, https://github.com/GSGroup
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+// provided that the above copyright notice and this permission notice appear in all copies.
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+// WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#include <stingraykit/io/BithreadCircularBuffer.h>
+
+#include <stingraykit/thread/ConditionVariable.h>
+#include <stingraykit/ExceptionPtr.h>
+
+namespace stingray
+{
+
+	class SharedCircularBuffer
+	{
+	public:
+		class ReadLock
+		{
+		private:
+			SharedCircularBuffer&	_parent;
+
+		public:
+			ReadLock(SharedCircularBuffer& parent) : _parent(parent)
+			{
+				STINGRAYKIT_CHECK(!_parent._activeRead, InvalidOperationException("Simultaneous Read()!"));
+				_parent._activeRead = true;
+			}
+
+			~ReadLock()
+			{ _parent._activeRead = false; }
+		};
+
+		class WriteLock
+		{
+		private:
+			SharedCircularBuffer&	_parent;
+
+		public:
+			WriteLock(SharedCircularBuffer& parent) : _parent(parent)
+			{ ++_parent._activeWrites; }
+
+			~WriteLock()
+			{ --_parent._activeWrites; }
+		};
+
+	public:
+		BithreadCircularBuffer	_buffer;
+		bool					_eod;
+		ExceptionPtr			_exception;
+
+		Mutex					_bufferMutex;
+		ConditionVariable		_bufferEmpty;
+		ConditionVariable		_bufferFull;
+
+	private:
+		bool					_activeRead;
+		size_t					_activeWrites;
+
+	public:
+		explicit SharedCircularBuffer(size_t size)
+			:	_buffer(size),
+				_eod(false),
+				_activeRead(false),
+				_activeWrites(0)
+		{ }
+
+		explicit SharedCircularBuffer(const BytesOwner& storage)
+			:	_buffer(storage),
+				_eod(false),
+				_activeRead(false),
+				_activeWrites(0)
+		{ }
+
+		size_t GetDataSize() const		{ MutexLock l(_bufferMutex); return _buffer.GetDataSize(); }
+		size_t GetFreeSize() const		{ MutexLock l(_bufferMutex); return _buffer.GetFreeSize(); }
+		size_t GetStorageSize() const	{ MutexLock l(_bufferMutex); return _buffer.GetTotalSize(); }
+
+		bool HasEndOfDataOrException() const	{ MutexLock l(_bufferMutex); return _eod || _exception; }
+
+		void SetEndOfData()
+		{
+			MutexLock l(_bufferMutex);
+
+			STINGRAYKIT_CHECK(!_exception, InvalidOperationException("Already got exception!"));
+
+			_eod = true;
+			_bufferEmpty.Broadcast();
+		}
+
+		void SetException(const std::exception& ex, const ICancellationToken& token)
+		{
+			MutexLock l(_bufferMutex);
+
+			STINGRAYKIT_CHECK(!_eod, InvalidOperationException("Already got EOD!"));
+
+			_exception = MakeExceptionPtr(ex);
+			_bufferEmpty.Broadcast();
+		}
+
+		void Clear()
+		{
+			MutexLock l(_bufferMutex);
+			STINGRAYKIT_CHECK(!_activeRead, InvalidOperationException("Simultaneous Read() and Clear()!"));
+			STINGRAYKIT_CHECK(_activeWrites == 0, InvalidOperationException("Simultaneous Process() and Clear()!"));
+
+			_buffer.Clear();
+			_eod = false;
+			_exception.reset();
+
+			_bufferFull.Broadcast();
+		}
+	};
+	STINGRAYKIT_DECLARE_PTR(SharedCircularBuffer);
+
+}
+
+#endif
