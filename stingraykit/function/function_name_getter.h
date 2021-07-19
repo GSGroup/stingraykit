f4495b2a1dafeadd5513fc3ffc912addc2c33e75
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2021-07-19 19:59:18 +0300

    function_name_getter: fix compatibility with final classes - get rid of nested method check

--- stingraykit/function/function_name_getter.h
+++ stingraykit/function/function_name_getter.h
@@ -24,43 +24,37 @@ namespace stingray
 	namespace Detail
 	{
 		std::string FuncPtrToString(intptr_t* ptrptr, size_t n);
+
+		struct function_name_getter
+		{
+			template < typename Func_ >
+			static std::string DoFallback(const Func_& func, typename EnableIf<function_type<Func_>::Type == FunctionType::RawFunctionPtr, int>::ValueT)
+			{ return "{ function: " + Detail::FuncPtrToString((intptr_t*)(&func), sizeof(func) / sizeof(intptr_t)) + " }"; }
+
+			template < typename Func_ >
+			static std::string DoFallback(const Func_& func, typename EnableIf<function_type<Func_>::Type == FunctionType::MethodPtr, int>::ValueT)
+			{ return "{ method: " + Detail::FuncPtrToString((intptr_t*)(&func), sizeof(func) / sizeof(intptr_t)) + " }"; }
+
+			template < typename Func_ >
+			static std::string DoFallback(const Func_& func, long)
+			{ return "{ functor: " + TypeInfo(func).GetName() + " }"; }
+
+		public:
+			template < typename Func_ >
+			static auto Do(const Func_& func, int)
+					-> decltype(func.get_name(), std::string())
+			{ return func.get_name(); }
+
+			template < typename Func_ >
+			static auto Do(const Func_& func, long)
+			{ return DoFallback(func, 0); }
+		};
 	}
-
-
-	STINGRAYKIT_DECLARE_METHOD_CHECK(get_name);
-
-
-	template < typename Func_, typename FunctionType::Enum FuncType_ = function_type<Func_>::Type, bool HasGetName = HasMethod_get_name<Func_>::Value >
-	struct function_name_getter
-	{
-		static std::string get(const Func_& func) { return "{ functor: " + TypeInfo(func).GetName() + " }"; }
-	};
-
-
-	template < typename Func_ >
-	struct function_name_getter<Func_, FunctionType::RawFunctionPtr, false>
-	{
-		static std::string get(const Func_& func) { return "{ function: " + Detail::FuncPtrToString((intptr_t*)(&func), sizeof(func) / sizeof(intptr_t)) + " }"; }
-	};
-
-
-	template < typename Func_ >
-	struct function_name_getter<Func_, FunctionType::MethodPtr, false>
-	{
-		static std::string get(const Func_& func) { return "{ method: " + Detail::FuncPtrToString((intptr_t*)(&func), sizeof(func) / sizeof(intptr_t)) + " }"; }
-	};
-
-
-	template < typename Func_, typename FunctionType::Enum FuncType_ >
-	struct function_name_getter<Func_, FuncType_, true>
-	{
-		static std::string get(const Func_& func) { return func.get_name(); }
-	};
 
 
 	template < typename Func_ >
 	std::string get_function_name(const Func_& func)
-	{ return function_name_getter<Func_>::get(func); }
+	{ return Detail::function_name_getter::Do(func, 0); }
 
 #endif
 
