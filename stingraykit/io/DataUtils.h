4993460d8f04fcf3bfd14be7b97b8b84e45fba2f
Igor Zavorotkin <zavorotkin@spb.gs.ru>
2021-12-01 01:29:28 +0300

    DataTaker: extract TakingConsumer

--- stingraykit/io/DataUtils.h
+++ stingraykit/io/DataUtils.h
@@ -8,38 +8,43 @@
 namespace stingray
 {
 
+	class TakingConsumer : public virtual IDataConsumer
+	{
+	private:
+		IDataConsumer&	_consumer;
+		size_t			_leftToTake;
+
+	public:
+		TakingConsumer(IDataConsumer& consumer, size_t count)
+			:	_consumer(consumer),
+				_leftToTake(count)
+		{ }
+
+		size_t Process(ConstByteData data, const ICancellationToken& token) override
+		{
+			const size_t size = std::min(_leftToTake, data.size());
+			if (!size)
+				return 0;
+
+			const size_t processed = _consumer.Process(ConstByteData(data, 0, size), token);
+			_leftToTake -= processed;
+
+			return processed;
+		}
+
+		void EndOfData(const ICancellationToken& token) override
+		{
+			_leftToTake = 0;
+			_consumer.EndOfData(token);
+		}
+
+		size_t GetLeftToTake() const
+		{ return _leftToTake; }
+	};
+
+
 	class DataTaker : public virtual IDataSource
 	{
-		class Consumer : public virtual IDataConsumer
-		{
-		private:
-			IDataConsumer&	_consumer;
-			size_t			_leftToTake;
-
-		public:
-			Consumer(IDataConsumer& consumer, size_t count) : _consumer(consumer), _leftToTake(count) { }
-
-			size_t Process(ConstByteData data, const ICancellationToken& token) override
-			{
-				const size_t size = std::min(data.size(), _leftToTake);
-				if (!size)
-					return 0;
-
-				const size_t processed = _consumer.Process(ConstByteData(data, 0, size), token);
-				_leftToTake -= processed;
-				return processed;
-			}
-
-			void EndOfData(const ICancellationToken& token) override
-			{
-				_leftToTake = 0;
-				_consumer.EndOfData(token);
-			}
-
-			size_t GetLeftToTake() const
-			{ return _leftToTake; }
-		};
-
 	private:
 		IDataSourcePtr	_source;
 		size_t			_leftToTake;
@@ -52,7 +57,7 @@ namespace stingray
 
 		void Read(IDataConsumer& consumer, const ICancellationToken& token) override
 		{
-			Consumer taker(consumer, _leftToTake);
+			TakingConsumer taker(consumer, _leftToTake);
 			while (token && taker.GetLeftToTake())
 				_source->Read(taker, token);
 			_leftToTake = taker.GetLeftToTake();
