17a42c6b4fb4cc9559e07ccc7f4e9a7c20fdbb59
Igor Zavorotkin <zavorotkin@spb.gs.ru>
2021-12-01 01:30:24 +0300

    DataTaker: allow to specify whether to discard data after taking or report not processed

--- stingraykit/io/DataUtils.h
+++ stingraykit/io/DataUtils.h
@@ -13,18 +13,20 @@ namespace stingray
 	private:
 		IDataConsumer&	_consumer;
 		size_t			_leftToTake;
+		bool			_discardRest;
 
 	public:
-		TakingConsumer(IDataConsumer& consumer, size_t count)
+		TakingConsumer(IDataConsumer& consumer, size_t count, bool discardRest = false)
 			:	_consumer(consumer),
-				_leftToTake(count)
+				_leftToTake(count),
+				_discardRest(discardRest)
 		{ }
 
 		size_t Process(ConstByteData data, const ICancellationToken& token) override
 		{
 			const size_t size = std::min(_leftToTake, data.size());
 			if (!size)
-				return 0;
+				return _discardRest ? data.size() : 0;
 
 			const size_t processed = _consumer.Process(ConstByteData(data, 0, size), token);
 			_leftToTake -= processed;
@@ -48,16 +50,18 @@ namespace stingray
 	private:
 		IDataSourcePtr	_source;
 		size_t			_leftToTake;
+		bool			_discardRest;
 
 	public:
-		DataTaker(const IDataSourcePtr& source, size_t count)
+		DataTaker(const IDataSourcePtr& source, size_t count, bool discardRest = false)
 			:	_source(source),
-				_leftToTake(count)
+				_leftToTake(count),
+				_discardRest(discardRest)
 		{ }
 
 		void Read(IDataConsumer& consumer, const ICancellationToken& token) override
 		{
-			TakingConsumer taker(consumer, _leftToTake);
+			TakingConsumer taker(consumer, _leftToTake, _discardRest);
 			while (token && taker.GetLeftToTake())
 				_source->Read(taker, token);
 			_leftToTake = taker.GetLeftToTake();
