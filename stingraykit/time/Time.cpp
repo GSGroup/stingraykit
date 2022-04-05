eabe5e757f0f066bcc1a53329f76075fb4c18146
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2022-04-05 19:52:12 +0300

    Time: invert DaysTo result
    So it will return number of days that must be added to this to get endTime.
    
    I.e. make TODAY.DaysTo(TOMORROW)==1 instead of TODAY.DaysTo(TOMORROW)==-1

--- stingraykit/time/Time.cpp
+++ stingraykit/time/Time.cpp
@@ -350,7 +350,7 @@ namespace stingray
 
 
 	int Time::DaysTo(const Time& endTime) const
-	{ return (Time::FromBrokenDownTime(BreakDown().GetDayStart()) - Time::FromBrokenDownTime(endTime.BreakDown().GetDayStart())) / TimeDuration::Day(); }
+	{ return (Time::FromBrokenDownTime(endTime.BreakDown().GetDayStart()) - Time::FromBrokenDownTime(BreakDown().GetDayStart())) / TimeDuration::Day(); }
 
 
 	void Time::Serialize(ObjectOStream& ar) const
