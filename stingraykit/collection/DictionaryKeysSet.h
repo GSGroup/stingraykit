f0425eee2c2da30870d702c1113b8dd3c30b16b3
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2022-05-21 23:59:19 +0300

    DictionaryKeysSet: get rid of duplicated code

--- stingraykit/collection/DictionaryKeysSet.h
+++ stingraykit/collection/DictionaryKeysSet.h
@@ -16,15 +16,15 @@
 namespace stingray
 {
 
-	template < typename KeyType_, typename ValueType_ >
+	template < typename KeyType_, typename ValueType_, typename DictionaryType_ = IReadonlyDictionary<KeyType_, ValueType_> >
 	class DictionaryKeysSet : public virtual IReadonlySet<KeyType_>
 	{
-		using DictionaryType = IReadonlyDictionary<KeyType_, ValueType_>;
+		using DictionaryType = DictionaryType_;
 		STINGRAYKIT_DECLARE_PTR(DictionaryType);
 
 		using ValueType = KeyType_;
 
-	private:
+	protected:
 		const DictionaryTypePtr		_dict;
 
 	public:
@@ -56,8 +56,10 @@ namespace stingray
 
 
 	template < typename KeyType_, typename ValueType_ >
-	class ObservableDictionaryKeysSet : public virtual IReadonlyObservableSet<KeyType_>
+	class ObservableDictionaryKeysSet : public DictionaryKeysSet<KeyType_, ValueType_, IReadonlyObservableDictionary<KeyType_, ValueType_>>, public virtual IReadonlyObservableSet<KeyType_>
 	{
+		using base = DictionaryKeysSet<KeyType_, ValueType_, IReadonlyObservableDictionary<KeyType_, ValueType_>>;
+
 		using SetType = IReadonlyObservableSet<KeyType_>;
 		using OnChangedSignature = typename SetType::OnChangedSignature;
 
@@ -66,56 +68,32 @@ namespace stingray
 		using DictionaryType = IReadonlyObservableDictionary<KeyType_, ValueType_>;
 		STINGRAYKIT_DECLARE_PTR(DictionaryType);
 
-		using ValueType = KeyType_;
-
 	private:
-		const DictionaryTypePtr								_dict;
 		signal<OnChangedSignature, ExternalMutexPointer>	_onChanged;
 		const Token											_connection;
 
 	public:
 		explicit ObservableDictionaryKeysSet(const DictionaryTypePtr& dict)
-			:	_dict(STINGRAYKIT_REQUIRE_NOT_NULL(dict)),
-				_onChanged(ExternalMutexPointer(shared_ptr<const Mutex>(_dict, &_dict->GetSyncRoot())), Bind(&ObservableDictionaryKeysSet::OnChangedPopulator, this, _1)),
-				_connection(_dict->OnChanged().connect(Bind(&ObservableDictionaryKeysSet::InvokeOnChanged, this, _1, _2), false))
+			:	base(dict),
+				_onChanged(ExternalMutexPointer(shared_ptr<const Mutex>(base::_dict, &base::_dict->GetSyncRoot())), Bind(&ObservableDictionaryKeysSet::OnChangedPopulator, this, _1)),
+				_connection(base::_dict->OnChanged().connect(Bind(&ObservableDictionaryKeysSet::InvokeOnChanged, this, _1, _2), false))
 		{ }
-
-		shared_ptr<IEnumerator<ValueType>> GetEnumerator() const override
-		{ return KeysEnumerator(_dict->GetEnumerator()); }
-
-		shared_ptr<IEnumerable<ValueType>> Reverse() const override
-		{ return KeysEnumerable(_dict->Reverse()); }
-
-		size_t GetCount() const override
-		{ return _dict->GetCount(); }
-
-		bool IsEmpty() const override
-		{ return _dict->IsEmpty(); }
-
-		bool Contains(const ValueType& value) const override
-		{ return _dict->ContainsKey(value); }
-
-		shared_ptr<IEnumerator<ValueType>> Find(const ValueType& value) const override
-		{ return KeysEnumerator(_dict->Find(value)); }
-
-		shared_ptr<IEnumerator<ValueType>> ReverseFind(const ValueType& value) const override
-		{ return KeysEnumerator(_dict->ReverseFind(value)); }
 
 		signal_connector<OnChangedSignature> OnChanged() const override
 		{ return _onChanged.connector(); }
 
 		const Mutex& GetSyncRoot() const override
-		{ return _dict->GetSyncRoot(); }
+		{ return base::_dict->GetSyncRoot(); }
 
 	private:
-		void InvokeOnChanged(CollectionOp op, const ValueType& val)
+		void InvokeOnChanged(CollectionOp op, const KeyType_& val)
 		{
 			if (op != CollectionOp::Updated)
 				_onChanged(op, val);
 		}
 
 		void OnChangedPopulator(const function<OnChangedSignature> slot) const
-		{ _dict->OnChanged().SendCurrentState(Bind(slot, _1, _2)); }
+		{ base::_dict->OnChanged().SendCurrentState(Bind(slot, _1, _2)); }
 	};
 
 
