070d1060b22ac5bae9e806e26980e451616f2901
Andrey Alyushenko <andrey.alyushenko@gs-labs.ru>
2022-06-14 20:22:32 +0300

    Range: fix headers

--- stingraykit/collection/Range.h
+++ stingraykit/collection/Range.h
@@ -8,14 +8,14 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#include <stingraykit/RefStorage.h>
 #include <stingraykit/collection/RangeBase.h>
 #include <stingraykit/collection/ToRange.h>
 #include <stingraykit/collection/Transformers.h>
-#include <stingraykit/function/FunctorInvoker.h>
 #include <stingraykit/dynamic_caster.h>
+#include <stingraykit/function/FunctorInvoker.h>
 #include <stingraykit/math.h>
 #include <stingraykit/optional.h>
+#include <stingraykit/RefStorage.h>
 
 namespace stingray
 {
