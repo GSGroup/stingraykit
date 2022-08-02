5d5a33abce29731b31a420b7f5838910f8292423
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2022-08-02 21:14:07 +0300

    unittests: add some tests for integer overflow exception

---
+++ unittests/ExceptionTest.cpp
@@ -0,0 +1,120 @@
+#include <stingraykit/exception.h>
+
+#include <gtest/gtest.h>
+
+using namespace stingray;
+
+
+#if 0
+TEST(ExceptionTest, MustBeUncompilable)
+{
+	STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s8)0, (u8)0);
+	STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s16)0, (u16)0);
+	STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s32)0, (u32)0);
+	STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s64)0, (u64)0);
+
+	STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u8)0, (u16)0);
+	STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u16)0, (u32)0);
+	STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u32)0, (u64)0);
+}
+#endif
+
+
+TEST(ExceptionTest, IntegerOverflow)
+{
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(0, 0));
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(0, 1));
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(0, std::numeric_limits<int>::max()));
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(1, std::numeric_limits<int>::max() - 1));
+
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(1, std::numeric_limits<int>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(0u, 0u));
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(0u, 1u));
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(0u, std::numeric_limits<unsigned>::max()));
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(1u, std::numeric_limits<unsigned>::max() - 1));
+
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(1u, std::numeric_limits<unsigned>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s8)0, std::numeric_limits<s8>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s8)1, std::numeric_limits<s8>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u8)(std::numeric_limits<u8>::max() / 2 + 1), std::numeric_limits<s8>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u8)(std::numeric_limits<u8>::max() / 2 + 2), std::numeric_limits<s8>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s16)(std::numeric_limits<s16>::max() - (s16)std::numeric_limits<s8>::max() * 2 - 1), std::numeric_limits<u8>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s16)(std::numeric_limits<s16>::max() - (s16)std::numeric_limits<s8>::max() * 2), std::numeric_limits<u8>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s16)0, std::numeric_limits<s16>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s16)1, std::numeric_limits<s16>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u16)(std::numeric_limits<u16>::max() / 2 + 1), std::numeric_limits<s16>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u16)(std::numeric_limits<u16>::max() / 2 + 2), std::numeric_limits<s16>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s32)(std::numeric_limits<s32>::max() - (s32)std::numeric_limits<s16>::max() * 2 - 1), std::numeric_limits<u16>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s32)(std::numeric_limits<s32>::max() - (s32)std::numeric_limits<s16>::max() * 2), std::numeric_limits<u16>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s32)0, std::numeric_limits<s32>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s32)1, std::numeric_limits<s32>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u32)(std::numeric_limits<u32>::max() / 2 + 1), std::numeric_limits<s32>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u32)(std::numeric_limits<u32>::max() / 2 + 2), std::numeric_limits<s32>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s64)(std::numeric_limits<s64>::max() - (s64)std::numeric_limits<s32>::max() * 2 - 1), std::numeric_limits<u32>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s64)(std::numeric_limits<s64>::max() - (s64)std::numeric_limits<s32>::max() * 2), std::numeric_limits<u32>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s64)0, std::numeric_limits<s64>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s64)1, std::numeric_limits<s64>::max()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u64)(std::numeric_limits<u64>::max() / 2 + 1), std::numeric_limits<s64>::max()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u64)(std::numeric_limits<u64>::max() / 2 + 2), std::numeric_limits<s64>::max()), IntegerOverflowException);
+}
+
+
+TEST(ExceptionTest, IntegerUnderflow)
+{
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(0, -1));
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(0, std::numeric_limits<int>::min()));
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(-1, std::numeric_limits<int>::min() + 1));
+
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(-1, std::numeric_limits<int>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(1u, -1));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(1u, -2), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(std::numeric_limits<unsigned>::max() / 2u + 1u, std::numeric_limits<int>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW(std::numeric_limits<unsigned>::max() / 2u, std::numeric_limits<int>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s8)0, std::numeric_limits<s8>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s8)-1, std::numeric_limits<s8>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u8)(std::numeric_limits<u8>::max() / 2 + 1), std::numeric_limits<s8>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u8)(std::numeric_limits<u8>::max() / 2), std::numeric_limits<s8>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s16)(std::numeric_limits<s16>::min() - std::numeric_limits<s8>::min()), std::numeric_limits<s8>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s16)(std::numeric_limits<s16>::min() - std::numeric_limits<s8>::min() - 1), std::numeric_limits<s8>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s16)0, std::numeric_limits<s16>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s16)-1, std::numeric_limits<s16>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u16)(std::numeric_limits<u16>::max() / 2 + 1), std::numeric_limits<s16>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u16)(std::numeric_limits<u16>::max() / 2), std::numeric_limits<s16>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s32)(std::numeric_limits<s32>::min() - std::numeric_limits<s16>::min()), std::numeric_limits<s16>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s32)(std::numeric_limits<s32>::min() - std::numeric_limits<s16>::min() - 1), std::numeric_limits<s16>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s32)0, std::numeric_limits<s32>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s32)-1, std::numeric_limits<s32>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u32)(std::numeric_limits<u32>::max() / 2 + 1), std::numeric_limits<s32>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u32)(std::numeric_limits<u32>::max() / 2), std::numeric_limits<s32>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s64)(std::numeric_limits<s64>::min() - std::numeric_limits<s32>::min()), std::numeric_limits<s32>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s64)(std::numeric_limits<s64>::min() - std::numeric_limits<s32>::min() - 1), std::numeric_limits<s32>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s64)0, std::numeric_limits<s64>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((s64)-1, std::numeric_limits<s64>::min()), IntegerOverflowException);
+
+	ASSERT_NO_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u64)(std::numeric_limits<u64>::max() / 2 + 1), std::numeric_limits<s64>::min()));
+	ASSERT_THROW(STINGRAYKIT_CHECK_INTEGER_OVERFLOW((u64)(std::numeric_limits<u64>::max() / 2), std::numeric_limits<s64>::min()), IntegerOverflowException);
+}
