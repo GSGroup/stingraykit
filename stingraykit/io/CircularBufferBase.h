13cf6501941891c19b363ed0ac6f957df75c9522
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2022-10-10 20:38:58 +0300

    stingraykit: update license plates

--- stingraykit/io/CircularBufferBase.h
+++ stingraykit/io/CircularBufferBase.h
@@ -1,7 +1,7 @@
 #ifndef STINGRAYKIT_IO_CIRCULARBUFFERBASE_H
 #define STINGRAYKIT_IO_CIRCULARBUFFERBASE_H
 
-// Copyright (c) 2011 - 2019, GS Group, https://github.com/GSGroup
+// Copyright (c) 2011 - 2022, GS Group, https://github.com/GSGroup
 // Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
 // provided that the above copyright notice and this permission notice appear in all copies.
 // THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
