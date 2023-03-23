23d2829a7195b5649941805df4ebb39c775740b3
Zavadovskiy Yan <yan.zavadovskiy@gs-labs.ru>
2023-03-23 23:37:47 +0300

    Rect: add full set of comparison operators

--- stingraykit/Rect.h
+++ stingraykit/Rect.h
@@ -8,6 +8,7 @@
 // IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 // WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+#include <stingraykit/compare/MemberListComparer.h>
 #include <stingraykit/string/ToString.h>
 
 namespace stingray
@@ -23,8 +24,9 @@ namespace stingray
 
 		bool Valid() const										{ return Width > 0 && Height > 0; }
 
-		bool operator == (const Size& other) const				{ return Width == other.Width && Height == other.Height; }
-		STINGRAYKIT_GENERATE_EQUALITY_OPERATORS_FROM_EQUAL(Size);
+		bool operator < (const Size& other) const
+		{ return CompareMembersLess(&Size::Width, &Size::Height)(*this, other); }
+		STINGRAYKIT_GENERATE_COMPARISON_OPERATORS_FROM_LESS(Size);
 
 		Size operator + (const Size& other) const				{ return Size(Width + other.Width, Height + other.Height); }
 		Size operator - (const Size& other) const				{ return Size(Width - other.Width, Height - other.Height); }
@@ -75,8 +77,9 @@ namespace stingray
 
 		bool Valid() const										{ return WidthScale > 0 && HeightScale > 0; }
 
-		bool operator == (const SizeScale& other) const			{ return WidthScale == other.WidthScale && HeightScale == other.HeightScale; }
-		STINGRAYKIT_GENERATE_EQUALITY_OPERATORS_FROM_EQUAL(SizeScale);
+		bool operator < (const SizeScale& other) const
+		{ return CompareMembersLess(&SizeScale::WidthScale, &SizeScale::HeightScale)(*this, other); }
+		STINGRAYKIT_GENERATE_COMPARISON_OPERATORS_FROM_LESS(SizeScale);
 
 		std::string ToString() const							{ return StringBuilder() % WidthScale % ":" % HeightScale; }
 
@@ -105,8 +108,9 @@ namespace stingray
 		BasicPosition() : X(0), Y(0) { }
 		BasicPosition(ValueType x, ValueType y) : X(x), Y(y) { }
 
-		bool operator == (const BasicPosition& other) const		{ return X == other.X && Y == other.Y; }
-		STINGRAYKIT_GENERATE_EQUALITY_OPERATORS_FROM_EQUAL(BasicPosition);
+		bool operator < (const BasicPosition& other) const
+		{ return CompareMembersLess(&BasicPosition::X, &BasicPosition::Y)(*this, other); }
+		STINGRAYKIT_GENERATE_COMPARISON_OPERATORS_FROM_LESS(BasicPosition);
 
 		std::string ToString() const							{ return StringBuilder() % "[" % X % ", " % Y % "]"; }
 
@@ -152,8 +156,9 @@ namespace stingray
 		BasicPosition<ValueType> GetTopLeft() const					{ return BasicPosition<ValueType>(X1, Y1); }
 		BasicPosition<ValueType> GetRightBottom() const				{ return BasicPosition<ValueType>(X2, Y2); }
 
-		bool operator == (const BasicRect& other) const				{ return GetTopLeft() == other.GetTopLeft() && GetRightBottom() == other.GetRightBottom(); }
-		STINGRAYKIT_GENERATE_EQUALITY_OPERATORS_FROM_EQUAL(BasicRect);
+		bool operator < (const BasicRect& other) const
+		{ return CompareMembersLess(&BasicRect::X1, &BasicRect::Y1, &BasicRect::X2, &BasicRect::Y2)(*this, other); }
+		STINGRAYKIT_GENERATE_COMPARISON_OPERATORS_FROM_LESS(BasicRect);
 
 		BasicRect Move(const BasicPosition<ValueType>& pos) const	{ return BasicRect(X1 + pos.X, Y1 + pos.Y, X2 + pos.X, Y2 + pos.Y); }
 		BasicRect Move(ValueType dx, ValueType dy) const			{ return BasicRect(X1 + dx, Y1 + dy, X2 + dx, Y2 + dy); }
