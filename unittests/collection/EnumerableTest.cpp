a92f326877bbd25267c034d0febfe679d1f1e365
Andrey Alyushenko <andrey.alyushenko@gs-labs.ru>
2023-09-29 19:54:49 +0300

    EnumerableTest: test contains

--- unittests/collection/EnumerableTest.cpp
+++ unittests/collection/EnumerableTest.cpp
@@ -23,6 +23,21 @@ namespace
 			break;
 	}
 
+	class ValueHolder
+	{
+	private:
+		int		_value;
+
+	public:
+		ValueHolder(int value) : _value(value) { }
+
+		int GetValue() const { return _value; }
+		const int& GetValueRef() const { return _value; }
+
+		bool operator == (const ValueHolder& other) const { return _value == other._value; }
+	};
+	STINGRAYKIT_DECLARE_PTR(ValueHolder);
+
 }
 
 
@@ -42,5 +57,36 @@ TEST(EnumerableTest, ForBasedLoop)
 			ASSERT_EQ(j++, i);
 
 		ASSERT_EQ(j, 5);
+	}
+}
+
+
+TEST(EnumerableTest, Contains)
+{
+	{
+		EnumerableBuilder<int> b;
+		b % 0 % 1 % 2 % 3 % 4;
+
+		ASSERT_TRUE(Enumerable::Contains(b.Get(), 2));
+		ASSERT_FALSE(Enumerable::Contains(b.Get(), 5));
+	}
+
+	{
+		EnumerableBuilder<ValueHolderPtr> b;
+		b % make_shared_ptr<ValueHolder>(0) % make_shared_ptr<ValueHolder>(1) % make_shared_ptr<ValueHolder>(2) % make_shared_ptr<ValueHolder>(3) % make_shared_ptr<ValueHolder>(4);
+
+		ASSERT_TRUE(Enumerable::Contains(b.Get() | Transform(&ValueHolder::GetValue), 2));
+		ASSERT_FALSE(Enumerable::Contains(b.Get() | Transform(&ValueHolder::GetValue), 5));
+
+		ASSERT_TRUE(Enumerable::Contains(b.Get() | Transform(&ValueHolder::GetValueRef), 2));
+		ASSERT_FALSE(Enumerable::Contains(b.Get() | Transform(&ValueHolder::GetValueRef), 5));
+	}
+
+	{
+		EnumerableBuilder<ValueHolder> b;
+		b % ValueHolder(0) % ValueHolder(1) % ValueHolder(2) % ValueHolder(3) % ValueHolder(4);
+
+		ASSERT_TRUE(Enumerable::Contains(b.Get(), 2));
+		ASSERT_FALSE(Enumerable::Contains(b.Get(), 5));
 	}
 }
