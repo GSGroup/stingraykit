1dd456e9c79ba5eb4e90d446093b1dff927a829b
Sergey Morozov <sergey.morozov@gs-labs.ru>
2023-12-12 12:27:37 +0300

    MultiTokenBuilder introduced

---
+++ stingraykit/MultiToken.h
@@ -0,0 +1,71 @@
+#ifndef STINGRAYKIT_MULTITOKEN_H
+#define STINGRAYKIT_MULTITOKEN_H
+
+#include <stingraykit/ObjectToken.h>
+#include <stingraykit/unique_ptr.h>
+
+#include <deque>
+
+namespace stingray
+{
+
+	template < bool IsForwardDestructionOrder >
+	class MultiTokenBuilderBase
+	{
+		class MultiToken
+		{
+			STINGRAYKIT_NONCOPYABLE(MultiToken);
+
+		private:
+			using Tokens = std::deque<Token>;
+
+		private:
+			Tokens		_tokens;
+
+		public:
+			MultiToken() { }
+
+			~MultiToken()
+			{
+				while (!_tokens.empty())
+				{
+					if (IsForwardDestructionOrder)
+						_tokens.pop_front();
+					else
+						_tokens.pop_back();
+				}
+			}
+
+			void Add(const Token& token)
+			{ _tokens.push_back(token); }
+
+			void Add(Token&& token)
+			{ _tokens.push_back(std::move(token)); }
+		};
+		STINGRAYKIT_DECLARE_UNIQ_PTR(MultiToken);
+
+	private:
+		MultiTokenUniqPtr		_token;
+
+	public:
+		MultiTokenBuilderBase()
+			:	_token(make_unique_ptr<MultiToken>())
+		{ }
+
+		MultiTokenBuilderBase& operator % (const Token& token)
+		{ STINGRAYKIT_REQUIRE_NOT_NULL(_token)->Add(token); return *this; }
+
+		MultiTokenBuilderBase& operator % (Token&& token)
+		{ STINGRAYKIT_REQUIRE_NOT_NULL(_token)->Add(std::move(token)); return *this; }
+
+		operator Token ()
+		{ return MakeObjectToken(MultiTokenUniqPtr(std::move(STINGRAYKIT_REQUIRE_NOT_NULL(_token)))); }
+	};
+
+
+	using ForwardMultiTokenBuilder = MultiTokenBuilderBase<true>;
+	using ReverseMultiTokenBuilder = MultiTokenBuilderBase<false>;
+
+}
+
+#endif
